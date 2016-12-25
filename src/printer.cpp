3b2a71085151f66f63ca6af3d9ad6ba1b2e5bbc4
Alastair Robertson <alastair@ajor.co.uk>
2016-12-25 23:00:23 +0000

    Printer: Put each case in switch statements on a single line

--- src/printer.cpp
+++ src/printer.cpp
@@ -23,57 +23,23 @@ void Printer::visit(Binop &binop)
   std::string indent(depth_, ' ');
   std::string opstr;
   switch (binop.op) {
-    case ebpf::bpftrace::Parser::token::EQ:
-      opstr = "==";
-      break;
-    case ebpf::bpftrace::Parser::token::NE:
-      opstr = "!=";
-      break;
-    case ebpf::bpftrace::Parser::token::LE:
-      opstr = "<=";
-      break;
-    case ebpf::bpftrace::Parser::token::GE:
-      opstr = ">=";
-      break;
-    case ebpf::bpftrace::Parser::token::LT:
-      opstr = "<";
-      break;
-    case ebpf::bpftrace::Parser::token::GT:
-      opstr = ">";
-      break;
-    case ebpf::bpftrace::Parser::token::LAND:
-      opstr = "&&";
-      break;
-    case ebpf::bpftrace::Parser::token::LOR:
-      opstr = "||";
-      break;
-    case ebpf::bpftrace::Parser::token::PLUS:
-      opstr = "+";
-      break;
-    case ebpf::bpftrace::Parser::token::MINUS:
-      opstr = "-";
-      break;
-    case ebpf::bpftrace::Parser::token::MUL:
-      opstr = "*";
-      break;
-    case ebpf::bpftrace::Parser::token::DIV:
-      opstr = "/";
-      break;
-    case ebpf::bpftrace::Parser::token::MOD:
-      opstr = "%";
-      break;
-    case ebpf::bpftrace::Parser::token::BAND:
-      opstr = "&";
-      break;
-    case ebpf::bpftrace::Parser::token::BOR:
-      opstr = "|";
-      break;
-    case ebpf::bpftrace::Parser::token::BXOR:
-      opstr = "^";
-      break;
-    default:
-      opstr = "???";
-      break;
+    case ebpf::bpftrace::Parser::token::EQ:    opstr = "=="; break;
+    case ebpf::bpftrace::Parser::token::NE:    opstr = "!="; break;
+    case ebpf::bpftrace::Parser::token::LE:    opstr = "<="; break;
+    case ebpf::bpftrace::Parser::token::GE:    opstr = ">="; break;
+    case ebpf::bpftrace::Parser::token::LT:    opstr = "<"; break;
+    case ebpf::bpftrace::Parser::token::GT:    opstr = ">"; break;
+    case ebpf::bpftrace::Parser::token::LAND:  opstr = "&&"; break;
+    case ebpf::bpftrace::Parser::token::LOR:   opstr = "||"; break;
+    case ebpf::bpftrace::Parser::token::PLUS:  opstr = "+"; break;
+    case ebpf::bpftrace::Parser::token::MINUS: opstr = "-"; break;
+    case ebpf::bpftrace::Parser::token::MUL:   opstr = "*"; break;
+    case ebpf::bpftrace::Parser::token::DIV:   opstr = "/"; break;
+    case ebpf::bpftrace::Parser::token::MOD:   opstr = "%"; break;
+    case ebpf::bpftrace::Parser::token::BAND:  opstr = "&"; break;
+    case ebpf::bpftrace::Parser::token::BOR:   opstr = "|"; break;
+    case ebpf::bpftrace::Parser::token::BXOR:  opstr = "^"; break;
+    default: opstr = "???"; break;
   }
 
   out_ << indent << opstr << std::endl;
@@ -84,15 +50,9 @@ void Printer::visit(Unop &unop)
   std::string indent(depth_, ' ');
   std::string opstr;
   switch (unop.op) {
-    case ebpf::bpftrace::Parser::token::LNOT:
-      opstr = "!";
-      break;
-    case ebpf::bpftrace::Parser::token::BNOT:
-      opstr = "~";
-      break;
-    default:
-      opstr = "???";
-      break;
+    case ebpf::bpftrace::Parser::token::LNOT: opstr = "!"; break;
+    case ebpf::bpftrace::Parser::token::BNOT: opstr = "~"; break;
+    default: opstr = "???"; break;
   }
 
   out_ << indent << opstr << std::endl;
