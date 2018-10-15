160fc7aa6e0eb62cc22bd56900d7fad74716ef10
Alastair Robertson <alastair@ajor.co.uk>
2018-10-15 22:01:27 +0100

    Set up multi-environment unit testing
    Supports:
    - LLVM 5 (static build)
    - LLVM 5
    - LLVM 6
    - LLVM 7

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,14 @@
 set -e
+
+STATIC_LINKING=${STATIC_LINKING:-OFF}
+RUN_TESTS=${RUN_TESTS:-1}
+
 mkdir -p "$1"
 cd "$1"
-cmake -DCMAKE_BUILD_TYPE="$2" -DSTATIC_LINKING:BOOL=ON ../
+cmake -DCMAKE_BUILD_TYPE="$2" -DSTATIC_LINKING:BOOL=$STATIC_LINKING ../
 shift 2
 make "$@"
+
+if [ $RUN_TESTS = 1 ]; then
+  ./tests/bpftrace_test $TEST_ARGS
+fi
