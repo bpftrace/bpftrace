ed4bd6094901f552d85e45a75fc37f36f006f37d
Alex Birch <Birch-san@users.noreply.github.com>
2018-11-01 20:43:45 +0000

    simplify for now

--- src/ast/codegen_llvm.cpp
+++ src/ast/codegen_llvm.cpp
@@ -341,14 +341,14 @@ void CodegenLLVM::visit(Call &call)
     // expr_ = b_.CreateIntCast(expr_, b_.getInt64Ty(), false); // promote int to 64-bit
     
     // b_.CreateMemSet(buf, b_.getInt8(0), b_.CreateAdd(strlen, b_.getInt64(1)), 1);
-    b_.CreateMemSet(buf, b_.getInt8(0), call.type.size+1, 1);
+    b_.CreateMemSet(buf, b_.getInt8(0), call.type.size, 1);
     call.vargs->front()->accept(*this);
     b_.CreateProbeReadStr(buf, call.type.size, expr_);
     
     // call.vargs->at(1)->accept(*this);
     // expr_ = b_.CreateIntCast(expr_, b_.getInt64Ty(), false); // promote int to 64-bit
     
-    b_.CreateStore(b_.getInt8(0), b_.CreateGEP(buf, {b_.getInt64(0), b_.getInt8(call.type.size)}));
+    // b_.CreateStore(b_.getInt8(0), b_.CreateGEP(buf, {b_.getInt64(0), b_.getInt8(call.type.size)}));
     expr_ = buf;
   }
   else if (call.func == "kaddr")
