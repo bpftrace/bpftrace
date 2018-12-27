278fd33a86dd15a866c282575c74772ef78c3eb7
williangaspar <williangaspar360@gmail.com>
2018-12-26 23:19:06 -0200

    fixes: https://github.com/iovisor/bpftrace/issues/35

--- tests/README.md
+++ tests/README.md
@@ -1,7 +1,17 @@
 # BPFtrace Tests
 
-Tests can be run with the `bpftrace_test` executable.
+There are two test suites in the project.
+
+## Unit tests
+
+These tests can be run with the `bpftrace_test` executable.
 
 The code generation tests are based on the output of LLVM 5, so may give errors if run with different version. They can be excluded by running:
 
 `bpftrace_test --gtest_filter=-codegen*`
+
+## Runtime tests
+
+  Runtime tests will call the bpftrace executable.
+  * Run: `sudo make runtime-tests` inside your build folder
+  * By default, runtime-tests will look for the executable in the build folder. You can set a value to the environment variable `BPFTRACE_RUNTIME_TEST_EXECUTABLE` to customize it
---
+++ tests/main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import os
+from os import environ
+import time
+from datetime import timedelta
+
+from utils import Utils
+from parser import TestParser
+
+
+def main():
+    test_suite = TestParser.read_all()
+
+    start_time = time.time()
+
+    total_tests = 0
+    total_fail = 0
+
+    for fname, tests in test_suite:
+        print('Test file: ' + fname + '\n')
+        for test in tests:
+            success = Utils.run_test(test)
+            total_tests += 1
+            if not success:
+                total_fail += 1
+        print('--------------------------------\n')
+
+    elapsed = time.time() - start_time
+    print(str(total_tests) + ' tests [fail ' + str(total_fail) + ']')
+    print('Done in ' + str(timedelta(seconds=elapsed)) )
+
+
+if __name__ == "__main__":
+    main()
