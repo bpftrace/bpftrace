1ffd798b8e68801f507c1687b847db06a17c3fb9
Daniel Xu <dxu@dxuuu.xyz>
2019-01-17 18:02:41 -0800

    Fix alpine docker build
    This patch fixes the docker builds (`./build.sh` and
    `./build-debug.sh`).
    
    Two main things were broken:
    * Alpine was missing a bcc-devel package. The temporary fix is to clone
      bcc, build, and install into the container
    * Static linking against bcc was broken. I've made some cmake config
      changes to support this.

--- docker/build.sh
+++ docker/build.sh
@@ -5,6 +5,14 @@ set -e
 STATIC_LINKING=${STATIC_LINKING:-OFF}
 RUN_TESTS=${RUN_TESTS:-1}
 
+# Install bcc
+cd /bcc/build
+cp src/cc/libbcc.a /usr/local/lib64/libbcc.a
+cp src/cc/libbcc-loader-static.a /usr/local/lib64/libbcc-loader-static.a
+cp src/cc/libbpf.a /usr/local/lib64/libbpf.a
+cd /
+
+# Build bpftrace
 mkdir -p "$1"
 cd "$1"
 cmake -DCMAKE_BUILD_TYPE="$2" -DSTATIC_LINKING:BOOL=$STATIC_LINKING ../
