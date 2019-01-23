bd7b7832b2a9d41ee52d6b35f131467ba61c0e30
Daniel Xu <dxu@dxuuu.xyz>
2019-01-23 14:43:45 -0800

    Address review

--- docker/build.sh
+++ docker/build.sh
@@ -5,13 +5,6 @@ set -e
 STATIC_LINKING=${STATIC_LINKING:-OFF}
 RUN_TESTS=${RUN_TESTS:-1}
 
-# Install bcc
-cd /bcc/build
-cp src/cc/libbcc.a /usr/local/lib64/libbcc.a
-cp src/cc/libbcc-loader-static.a /usr/local/lib64/libbcc-loader-static.a
-cp src/cc/libbpf.a /usr/local/lib64/libbpf.a
-cd /
-
 # Build bpftrace
 mkdir -p "$1"
 cd "$1"
