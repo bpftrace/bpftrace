e4cbe480ee8ebaabf0a67dff2ebc45e53f6cdddd
Augusto Caringi <acaringi@redhat.com>
2019-02-14 21:46:34 +0100

    Fix new build warning msg after c++17 was enabled
    warning: ignoring returned value of type ‘llvm::Error’, declared with
    attribute nodiscard [-Wunused-result]
         CompileLayer.emitAndFinalize(K);

--- src/bpforc.h
+++ src/bpforc.h
@@ -96,7 +96,7 @@ public:
 
   void compileModule(std::unique_ptr<Module> M) {
     auto K = addModule(move(M));
-    CompileLayer.emitAndFinalize(K);
+    cantFail(CompileLayer.emitAndFinalize(K));
   }
 
   VModuleKey addModule(std::unique_ptr<Module> M) {
