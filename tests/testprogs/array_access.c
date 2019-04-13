c268ffab1abc3d13c36ff5ba1ccfc4c6cd617946
Dale Hamel <dale.hamel@srvthe.net>
2019-04-13 12:03:27 -0400

    Add runtime test for array access

---
+++ tests/testprogs/array_access.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+typedef struct MY_STRUCT{
+  int x[4];
+
+}mystruct;
+
+void test_struct(mystruct *s) { }
+
+int main(int argc, char **argv)
+{
+  mystruct s;
+  s.x[0] = 1;
+  test_struct(&s);
+}
