5cce614ca757d0d22919f1a4dc025388b6989450
Dale Hamel <dale.hamel@srvthe.net>
2019-04-13 17:41:24 -0400

    More usdt runtime test cases

--- tests/testprogs/usdt_test.c
+++ tests/testprogs/usdt_test.c
@@ -5,17 +5,17 @@
 
 static long
 myclock() {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    DTRACE_PROBE1(tracetest, testprobe, tv.tv_sec);
-    DTRACE_PROBE1(tracetest, testprobe2, tv.tv_sec);
-    DTRACE_PROBE1(tracetest2, testprobe2, tv.tv_sec);
-    return tv.tv_sec;
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  DTRACE_PROBE1(tracetest, testprobe, tv.tv_sec);
+  DTRACE_PROBE1(tracetest, testprobe2, tv.tv_sec);
+  DTRACE_PROBE1(tracetest2, testprobe2, tv.tv_sec);
+  return tv.tv_sec;
 }
 
 int
 main(int argc, char **argv) {
-    usleep(2000000);
+  usleep(1000000);
     myclock();
     return 0;
 }
