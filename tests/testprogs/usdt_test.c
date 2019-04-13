1b21a2567592b7239d1725d5c23ba3ab238f4a31
Dale Hamel <dale.hamel@srvthe.net>
2019-04-13 17:55:54 -0400

    Speed up usdt tests and cover just args

--- tests/testprogs/usdt_test.c
+++ tests/testprogs/usdt_test.c
@@ -7,15 +7,15 @@ static long
 myclock() {
   struct timeval tv;
   gettimeofday(&tv, NULL);
-  DTRACE_PROBE1(tracetest, testprobe, tv.tv_sec);
-  DTRACE_PROBE1(tracetest, testprobe2, tv.tv_sec);
-  DTRACE_PROBE1(tracetest2, testprobe2, tv.tv_sec);
+  DTRACE_PROBE2(tracetest,  testprobe,  tv.tv_sec, "Hello world");
+  DTRACE_PROBE2(tracetest,  testprobe2, tv.tv_sec, "Hello world2");
+  DTRACE_PROBE2(tracetest2, testprobe2, tv.tv_sec, "Hello world3");
   return tv.tv_sec;
 }
 
 int
 main(int argc, char **argv) {
-  usleep(1000000);
+  usleep(500000);
     myclock();
     return 0;
 }
