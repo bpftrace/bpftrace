8f9c9cf0197ef1650f15b2f977a23b54688530ad
bas smit <bas@baslab.org>
2019-05-12 22:57:06 +0200

    Script to check kernel requirements
    This adds a simple script that checks whether the kernel was
    compiled with all options that bpftrace needs. All missing features will
    be reported to the user.
    
    As light weight distros used for CI usually leave out `bash` this script
    is written to work with `sh`.
    
    Closes #622

---
+++ scripts/check_kernel_features.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# Report missing kernel features
+#
+# Usage: ./check_kernel_features.sh
+
+set -e
+set -u
+
+err=0
+config=''
+
+# Find kernel config
+for c in "/boot/config-$(uname -r)" "/boot/config" "/proc/config.gz"; do
+    if [ -f "$c" ]; then
+        config="$c"
+        break
+    fi
+done
+
+if [ -z "$config" ]; then
+    echo "Could not find kernel config" >&2
+    exit 1
+fi
+
+# Check feature
+check_opt() {
+    if ! zgrep -qE "^${1}[[:space:]]*=[[:space:]]*[y|Y]" "$config"; then
+        err=1
+        echo "Required option ${1} not set" >&2
+    fi
+}
+
+check_opt 'CONFIG_BPF'
+check_opt 'CONFIG_BPF_EVENTS'
+check_opt 'CONFIG_BPF_JIT'
+check_opt 'CONFIG_BPF_SYSCALL'
+check_opt 'CONFIG_FTRACE_SYSCALLS'
+check_opt 'CONFIG_HAVE_EBPF_JIT'
+
+# Status report
+if [ $err -eq 0 ]; then
+    echo "All required features present!"
+else
+    echo "Missing required features"
+fi
+
+exit $err
