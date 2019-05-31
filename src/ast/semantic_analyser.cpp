176eb0740415539c76e96fc53a341f264e7a1a57
Jay Kamat <jaygkamat@gmail.com>
2019-05-31 09:27:41 -0700

    Fix typo in type mismatch error message
    Closes #641

--- src/ast/semantic_analyser.cpp
+++ src/ast/semantic_analyser.cpp
@@ -661,7 +661,7 @@ void SemanticAnalyser::visit(Binop &binop)
       // allow integer to cast pointer comparisons (eg, ptr != 0):
       !(lhs == Type::cast && rhs == Type::integer) &&
       !(lhs == Type::integer && rhs == Type::cast)) {
-      err_ << "TYpe mismatch for '" << opstr(binop) << "': ";
+      err_ << "Type mismatch for '" << opstr(binop) << "': ";
       err_ << "comparing '" << lhs << "' ";
       err_ << "with '" << rhs << "'" << std::endl;
     }
