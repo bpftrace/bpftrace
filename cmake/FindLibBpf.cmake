8e0800ce1bb9a6ff2fc8d3257409f0573155f340
Jiri Olsa <jolsa@kernel.org>
2019-06-06 12:48:32 +0200

    Add support to check for libbpf package
    It will be detected on usual place, pluys user can specify
    non standard libbpf place (like /opt/libbpf/ below) with:
    
      $ cmake ... -DLIBBPF_INCLUDE_DIRS=/opt/libbpf/include \
                  -DLIBBPF_LIBRARIES=/opt/libbpf/lib64/libbpf.so
    
    Adding detection of btf_dump__new symbol, which is needed
    in following changes. If found the LIBBPF_BTF_DUMP_FOUND
    variable is set to TRUE.

---
+++ cmake/FindLibBpf.cmake
@@ -0,0 +1,57 @@
+# - Try to find libbpf
+# Once done this will define
+#
+#  LIBBPF_FOUND - system has libbpf
+#  LIBBPF_INCLUDE_DIRS - the libbpf include directory
+#  LIBBPF_LIBRARIES - Link these to use libbpf
+#  LIBBPF_DEFINITIONS - Compiler switches required for using libbpf
+
+#if (LIBBPF_LIBRARIES AND LIBBPF_INCLUDE_DIRS)
+#  set (LibBpf_FIND_QUIETLY TRUE)
+#endif (LIBBPF_LIBRARIES AND LIBBPF_INCLUDE_DIRS)
+
+find_path (LIBBPF_INCLUDE_DIRS
+  NAMES
+    bpf/bpf.h
+    bpf/btf.h
+    bpf/libbpf.h
+  PATHS
+    /usr/include
+    /usr/local/include
+    /opt/local/include
+    /sw/include
+    ENV CPATH)
+
+find_library (LIBBPF_LIBRARIES
+  NAMES
+    bpf
+  PATHS
+    /usr/lib
+    /usr/local/lib
+    /opt/local/lib
+    /sw/lib
+    ENV LIBRARY_PATH
+    ENV LD_LIBRARY_PATH)
+
+include (FindPackageHandleStandardArgs)
+
+# handle the QUIETLY and REQUIRED arguments and set LIBBPF_FOUND to TRUE if all listed variables are TRUE
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibBpf "Please install the libbpf development package"
+  LIBBPF_LIBRARIES
+  LIBBPF_INCLUDE_DIRS)
+
+mark_as_advanced(LIBBPF_INCLUDE_DIRS LIBBPF_LIBRARIES)
+
+# We need btf_dump support, set LIBBPF_BTF_DUMP_FOUND
+# when it's found.
+if (LIBBPF_FOUND)
+  include(CheckSymbolExists)
+  # adding also elf for static build check
+  SET(CMAKE_REQUIRED_LIBRARIES ${LIBBPF_LIBRARIES} elf)
+  # libbpf quirk, needs upstream fix
+  SET(CMAKE_REQUIRED_DEFINITIONS -include stdbool.h)
+  check_symbol_exists(btf_dump__new "${LIBBPF_INCLUDE_DIRS}/bpf/btf.h" HAVE_BTF_DUMP)
+  if (HAVE_BTF_DUMP)
+    set(LIBBPF_BTF_DUMP_FOUND TRUE)
+  endif()
+endif()
