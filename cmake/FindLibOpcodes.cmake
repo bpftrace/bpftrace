fbb251804a5e295b5dd52665f90378c2866ee18e
Jiri Olsa <jolsa@kernel.org>
2019-06-06 12:48:32 +0200

    Add support to check for libbfd/libopcodes libraries
    We need both libbfd and libopcodes to use the binutils
    disassembler later on. Adding the cmake detection of
    these, mostly copied/stolen from already present cmake
    Find* modules (FindLibBcc/FindLibElf)

---
+++ cmake/FindLibOpcodes.cmake
@@ -0,0 +1,43 @@
+# - Try to find libbfd
+# Once done this will define
+#
+#  LIBOPCODES_FOUND - system has libbfd
+#  LIBOPCODES_INCLUDE_DIRS - the libbfd include directory
+#  LIBOPCODES_LIBRARIES - Link these to use libbfd
+#  LIBOPCODES_DEFINITIONS - Compiler switches required for using libbfd
+
+#if (LIBOPCODES_LIBRARIES AND LIBOPCODES_INCLUDE_DIRS)
+#  set (LibBpf_FIND_QUIETLY TRUE)
+#endif (LIBOPCODES_LIBRARIES AND LIBOPCODES_INCLUDE_DIRS)
+
+find_path (LIBOPCODES_INCLUDE_DIRS
+  NAMES
+    dis-asm.h
+  PATHS
+    /usr/include
+    /usr/local/include
+    /opt/local/include
+    /sw/include
+    ENV CPATH)
+
+find_library (LIBOPCODES_LIBRARIES
+  NAMES
+    opcodes
+  PATHS
+    /lib
+    /usr/lib
+    /usr/local/lib
+    /opt/local/lib
+    /sw/lib
+    ENV LIBRARY_PATH
+    ENV LD_LIBRARY_PATH)
+
+include (FindPackageHandleStandardArgs)
+
+
+# handle the QUIETLY and REQUIRED arguments and set LIBOPCODES_FOUND to TRUE if all listed variables are TRUE
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibOpcodes "Please install the libopcodes development package"
+  LIBOPCODES_LIBRARIES
+  LIBOPCODES_INCLUDE_DIRS)
+
+mark_as_advanced(LIBOPCODES_INCLUDE_DIRS LIBOPCODES_LIBRARIES)
