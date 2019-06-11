c71bf9e623e13edae2b4381fa4203fdfd0349dfc
Matheus Marchini <mat@mmarchini.me>
2019-06-11 16:44:32 -0700

    WIP unify CI and ./build.sh

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
+
+set -x
 set -e
-pushd docker
-docker build -t bpftrace-builder-alpine -f Dockerfile.alpine .
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DOCKER_BASE="${DOCKER_BASE:-alpine}"
+
+BUILD_FLAGS=""
+IMAGE_NAME="bpftrace-builder-$DOCKER_BASE"
+
+if [ "$LLVM_VERSION" != "" ]; then
+  BUILD_FLAGS="$BUILD_FLAGS --build-arg LLVM_VERSION=$LLVM_VERSION"
+  IMAGE_NAME="${IMAGE_NAME}-llvm-$LLVM_VERSION"
+fi;
+
+if [ $DOCKER_FORCE_REBUILD = 1 ]; then
+  BUILD_FLAGS="$BUILD_FLAGS --no-cache"
+fi
+
+pushd $DIR/docker
+docker build $BUILD_FLAGS -t $IMAGE_NAME -f Dockerfile.$DOCKER_BASE .
 popd
--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+set -x
 set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+pushd $DIR
+BUILD_TYPE="${BUILD_TYPE:-release}"
+
 ./build-docker-image.sh
-./build-release.sh "$@"
+./build-${BUILD_TYPE}.sh "$@"
+popd
