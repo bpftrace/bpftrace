e73822a26687e3def9b775fb6c80c24f6fd1f06e
Jiri Olsa <jolsa@kernel.org>
2019-07-04 14:20:44 +0200

    Add BfdDisasm class
    As a IDisasm interface implementation by using the
    binutils disassembler. It will be compiled in, if
    the libbfd and libopcodes libraries are detected
    during the build.

---
+++ src/bfd-disasm.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "disasm.h"
+
+namespace bpftrace {
+
+class BfdDisasm : public IDisasm
+{
+public:
+  BfdDisasm(std::string &path);
+  ~BfdDisasm();
+
+  AlignState is_aligned(uint64_t offset, uint64_t pc);
+
+private:
+  int fd_ = -1;
+  uint64_t size;
+};
+
+} // namespace bpftrace
