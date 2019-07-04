aef85f88406b21abf1069357b0e98b008fcef28c
Jason Keene <jasonkeene@gmail.com>
2019-07-03 18:53:10 -0600

    Add support for go symbol names to uaddr

--- src/ast/semantic_analyser.h
+++ src/ast/semantic_analyser.h
@@ -61,7 +61,7 @@ private:
   bool check_nargs(const Call &call, size_t expected_nargs);
   bool check_varargs(const Call &call, size_t min_nargs, size_t max_nargs);
   bool check_arg(const Call &call, Type type, int arg_num, bool want_literal=false);
-  bool check_alpha_numeric(const Call &call, int arg_num);
+  bool check_symbol(const Call &call, int arg_num);
 
   void check_stack_call(Call &call, Type type);
 
