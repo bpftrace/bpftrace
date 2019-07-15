8c8ccc96202dcc874929bfa5fa613b3e761a48aa
Daniel Xu <dxu@dxuuu.xyz>
2019-07-15 14:50:53 -0700

    Fix unused variable warnings

--- tests/testprogs/uprobe_test.c
+++ tests/testprogs/uprobe_test.c
@@ -5,7 +5,7 @@ int function1()
   return 0;
 }
 
-int main(int argc, char **argv) {
+int main(int argc __attribute__((unused)), char **argv __attribute__((unused))) {
   while (1) {
     function1();
   }
--- tests/testprogs/usdt_test.c
+++ tests/testprogs/usdt_test.c
@@ -18,7 +18,7 @@ myclock() {
 }
 
 int
-main(int argc, char **argv) {
+main(int argc, char **argv __attribute__((unused))) {
   if (argc > 1)
   // If we don't have Systemtap headers, we should skip USDT tests. Returning 1 can be used as validation in the REQUIRE
 #ifndef HAVE_SYSTEMTAP_SYS_SDT_H
