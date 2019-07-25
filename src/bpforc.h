03c44c848e633ea180406838862007c8e7713c86
Matheus Marchini <mat@mmarchini.me>
2019-07-25 14:08:22 -0700

    bpforc: be more strict wrt failed compilation

--- src/bpforc.h
+++ src/bpforc.h
@@ -61,7 +61,7 @@ public:
   void compileModule(std::unique_ptr<Module> M)
   {
     auto mod = addModule(move(M));
-    CompileLayer.emitAndFinalize(mod);
+    cantFail(CompileLayer.emitAndFinalize(mod));
   }
 
   ModuleHandle addModule(std::unique_ptr<Module> M) {
