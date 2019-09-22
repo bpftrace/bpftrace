39781607284f258fbf2afae2e3a0e2fb262cb8ee
bas smit <bas@baslab.org>
2019-09-23 00:24:50 +0200

    Store the BPF map type in the map object
    Throughout the codebase the `type` (integer, count etc) is used to make
    assumptions about the BPF map type used, instead of having access to the
    real type. This leads to bugs, an example:
    
    in `map.cpp`
    ```
      if ((type.type == Type::hist || type.type == Type::lhist) &&
          (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 6, 0)))
          map_type_ = BPF_MAP_TYPE_PERCPU_HASH;
      else
        map_type_ = BPF_MAP_TYPE_HASH;
    ```
    
    in the `zero_map` function
    
    ```
      if ( ... map.type_.type == Type::lhist ... )
        value_size *= ncpus_;
    ```
    
    The `ncpu` scaling is only valid for a `PERCPU` type as those have a
    value for each CPU. On a kernel < 4.6.0 this will likely cause an issue.
    
    This commit makes the type the map was created with available in the map
    object. That way the actual map type can be used instead of guessing
    based on value type.

--- src/map.cpp
+++ src/map.cpp
@@ -36,26 +36,25 @@ Map::Map(const std::string &name, const SizedType &type, const MapKey &key, int 
   if (key_size == 0)
     key_size = 8;
 
-  enum bpf_map_type map_type;
   if ((type.type == Type::hist || type.type == Type::lhist || type.type == Type::count ||
       type.type == Type::sum || type.type == Type::min || type.type == Type::max ||
       type.type == Type::avg || type.type == Type::stats) &&
       (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 6, 0)))
   {
-      map_type = BPF_MAP_TYPE_PERCPU_HASH;
+      map_type_ = BPF_MAP_TYPE_PERCPU_HASH;
   }
   else if (type.type == Type::join)
   {
-    map_type = BPF_MAP_TYPE_PERCPU_ARRAY;
+    map_type_ = BPF_MAP_TYPE_PERCPU_ARRAY;
     max_entries = 1;
     key_size = 4;
   }
   else
-    map_type = BPF_MAP_TYPE_HASH;
+    map_type_ = BPF_MAP_TYPE_HASH;
 
   int value_size = type.size;
   int flags = 0;
-  mapfd_ = create_map(map_type, name.c_str(), key_size, value_size, max_entries, flags);
+  mapfd_ = create_map(map_type_, name.c_str(), key_size, value_size, max_entries, flags);
   if (mapfd_ < 0)
   {
     std::cerr << "Error creating map: '" << name_ << "': " << strerror(errno)
@@ -94,6 +93,7 @@ Map::Map(const SizedType &type) {
 Map::Map(enum bpf_map_type map_type)
 {
   int key_size, value_size, max_entries, flags;
+  map_type_ = map_type;
 
   std::string name;
 #ifdef DEBUG
