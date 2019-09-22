35af52c4e3670c42b6e086f6dd69f9318d72f0e1
bas smit <bas@baslab.org>
2019-09-23 01:06:16 +0200

    Fix maptype bugs
    The `ncpus_` scaling is only valid when the map is of type PER_CPU. This
    isn't always the case as PER_CPU isn't used on kernels < 4.6.0.
    
    Because 0 initialized vectors were used this didn't lead to bugs (afaik)
    but it is still good to fix it.
    
    This commit changes that by looking at the type with which the map was
    created, instead of guessing it based on the type of the content.

--- src/bpftrace.h
+++ src/bpftrace.h
@@ -184,9 +184,9 @@ private:
   int print_map_stats(IMap &map);
   int print_hist(const std::vector<uint64_t> &values, uint32_t div) const;
   int print_lhist(const std::vector<uint64_t> &values, int min, int max, int step) const;
-  template <typename T> static T reduce_value(const std::vector<uint8_t> &value, int ncpus);
-  static int64_t min_value(const std::vector<uint8_t> &value, int ncpus);
-  static uint64_t max_value(const std::vector<uint8_t> &value, int ncpus);
+  template <typename T> static T reduce_value(const std::vector<uint8_t> &value, int nvalues);
+  static int64_t min_value(const std::vector<uint8_t> &value, int nvalues);
+  static uint64_t max_value(const std::vector<uint8_t> &value, int nvalues);
   static uint64_t read_address_from_output(std::string output);
   std::vector<uint8_t> find_empty_key(IMap &map, size_t size) const;
   static int spawn_child(const std::vector<std::string>& args, int *notify_trace_start_pipe_fd);
