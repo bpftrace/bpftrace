2a361cc90046be5cb73f594e3e50a138479c3dfa
Adam Jensen <acjensen@gmail.com>
2019-10-30 14:30:59 -0400

    Support wildcards in probe path (#879)
    Fixes #688. One case that remains tricky is usdt:/my/app/* because path includes leading and trailing colons in the lexer. It works if you provide a probe name, though. Maybe this is fine for now.
    
    I'm not sure whether all the tests are needed or wanted, but they were helpful for testing, so I left them in.

--- src/ast/semantic_analyser.cpp
+++ src/ast/semantic_analyser.cpp
@@ -1128,7 +1128,11 @@ void SemanticAnalyser::visit(AttachPoint &ap)
       err_ << "uprobes should have a target" << std::endl;
     if (ap.func == "")
       err_ << "uprobes should be attached to a function" << std::endl;
-    ap.target = resolve_binary_path(ap.target);
+
+    auto paths = resolve_binary_path(ap.target);
+    if (paths.size() > 1)
+      err_ << "path '" << ap.target << "' must refer to a unique binary but matched " << paths.size() << std::endl;
+    ap.target = paths.front();
     struct stat s;
     if (stat(ap.target.c_str(), &s) != 0)
       err_ << "failed to stat uprobe target file " << ap.target << ": "
@@ -1138,20 +1142,22 @@ void SemanticAnalyser::visit(AttachPoint &ap)
     if (ap.func == "")
       err_ << "usdt probe must have a target function or wildcard" << std::endl;
 
-    usdt_probe_list probes;
+    if (ap.target != "") {
+      auto paths = resolve_binary_path(ap.target);
+      if (paths.size() > 1)
+        err_ << "path '" << ap.target << "' must refer to a unique binary but matched " << paths.size() << std::endl;
+      ap.target = paths.front();
+      struct stat s;
+      if (stat(ap.target.c_str(), &s) != 0)
+        err_ << "usdt target file " << ap.target << " does not exist" << std::endl;
+    }
+
     if (bpftrace_.pid_ > 0) {
        USDTHelper::probes_for_pid(bpftrace_.pid_);
     } else if (ap.target != "") {
        USDTHelper::probes_for_path(ap.target);
     } else {
       err_ << "usdt probe must specify at least path or pid to probe" << std::endl;
-    }
-
-    if (ap.target != "") {
-      ap.target = resolve_binary_path(ap.target);
-      struct stat s;
-      if (stat(ap.target.c_str(), &s) != 0)
-        err_ << "usdt target file " << ap.target << " does not exist" << std::endl;
     }
   }
   else if (ap.provider == "tracepoint") {
--- src/list.cpp
+++ src/list.cpp
@@ -159,7 +159,13 @@ void list_probes(const BPFtrace &bpftrace, const std::string &search_input)
       show_all = executable.find(":") == std::string::npos;
       executable = executable.substr(0, executable.find(":"));
 
-      absolute_exe = resolve_binary_path(executable);
+      auto paths = resolve_binary_path(executable);
+      if (paths.size() > 1)
+      {
+        std::cerr << "path '" << executable << "' must refer to a unique binary but matched " << paths.size() << std::endl;
+        return;
+      }
+      absolute_exe = paths.front();
       struct stat s;
       if (stat(absolute_exe.c_str(), &s) != 0) {
         std::cerr << "uprobe target " << executable << " does not exist" << std::endl;
@@ -175,7 +181,7 @@ void list_probes(const BPFtrace &bpftrace, const std::string &search_input)
       std::string line;
       while (std::getline(*symbol_stream, line))
       {
-        std::string probe = "uprobe:" + executable + ":" + line;
+        std::string probe = "uprobe:" + absolute_exe + ":" + line;
         if (show_all || search.empty() || !search_probe(probe, re))
           std::cout << probe << std::endl;
       }
@@ -194,7 +200,13 @@ void list_probes(const BPFtrace &bpftrace, const std::string &search_input)
     std::string usdt_path = search.substr(search.find(":")+1, search.size());
     usdt_path_list = usdt_path.find(":") == std::string::npos;
     usdt_path = usdt_path.substr(0, usdt_path.find(":"));
-    usdt_probes = USDTHelper::probes_for_path(usdt_path);
+    auto paths = resolve_binary_path(usdt_path);
+    if (paths.size() > 1)
+    {
+      std::cerr << "path '" << usdt_path << "' must refer to a unique binary but matched " << paths.size() << std::endl;
+      return;
+    }
+    usdt_probes = USDTHelper::probes_for_path(paths.front());
   }
 
   for (auto const& usdt_probe : usdt_probes)
