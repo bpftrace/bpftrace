49cd031b9dae035ddde4d65bc66072f06f95bf8d
bas smit <bas@baslab.org>
2019-11-12 20:55:26 +0100

    Mark 'override_return' as unsafe
    A badly inserted override has the possibility to break your system. One
    such example is `kprobe:do_openat { override_return(-1); }` which will
    prevent your whole system from opening files.

--- src/utils.h
+++ src/utils.h
@@ -97,6 +97,7 @@ static std::vector<DeprecatedName> DEPRECATED_LIST =
 static std::vector<std::string> UNSAFE_BUILTIN_FUNCS = {
   "system",
   "signal",
+  "override_return",
 };
 
 bool get_uint64_env_var(const ::std::string &str, uint64_t &dest);
