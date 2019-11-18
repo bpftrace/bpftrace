8822cc20c3bb58b01d2787defe3205cae531b5f5
Daniel Xu <dxu@dxuuu.xyz>
2019-11-18 13:55:32 -0800

    Implement bitfield support
    Bitfields are a pretty commonly used C feature (especially in kernel
    land). Having bpftrace support would make bpftrace easier to use for
    kernel developers.
    
    Example script:
    ```
    
    kprobe:ip_send_check{
      printf("version is %x, ihl is %x\n", ((struct iphdr*)arg0)->version, ((struct iphdr*)arg0)->ihl);
    }
    ```
    
    `struct iphdr*` is defined as:
    ```
    struct iphdr {
    	__u8	ihl:4,
    		version:4;
            ...
    ```
    
    Before, we'd get the following output:
    ```
    Attaching 1 probe...
    version is 45, ihl is 45
    version is 45, ihl is 45
    version is 45, ihl is 45
    ...
    ```
    
    Now we get:
    ```
    Attaching 1 probe...
    version is 4, ihl is 5
    version is 4, ihl is 5
    version is 4, ihl is 5
    version is 4, ihl is 5
    ...
    ```
    
    This closes #969 .

--- src/struct.h
+++ src/struct.h
@@ -5,9 +5,24 @@
 
 namespace bpftrace {
 
+struct Bitfield {
+  bool operator==(const Bitfield &other) const;
+  bool operator!=(const Bitfield &other) const;
+
+  // Read `read_bytes` bytes starting from this field's offset
+  size_t read_bytes;
+  // Then rshift the resulting value by `access_rshift` to get field value
+  size_t access_rshift;
+  // Then logical AND `mask` to mask out everything but this bitfield
+  size_t mask;
+};
+
 struct Field {
   SizedType type;
   int offset;
+
+  bool is_bitfield;
+  Bitfield bitfield;
 };
 
 using FieldsMap = std::map<std::string, Field>;
