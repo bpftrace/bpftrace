bdbc76d204134107cec876b12c8bc390febd4ee1
Daniel Xu <dxu@dxuuu.xyz>
2019-11-19 17:21:32 -0800

    codegen: fix rhs type check for binop
    Stolen from @mmarchini.
    
    This closes #634 .

--- src/ast/semantic_analyser.cpp
+++ src/ast/semantic_analyser.cpp
@@ -855,12 +855,12 @@ void SemanticAnalyser::visit(Binop &binop)
         }
       }
     }
-    else if (lhs != Type::integer
+    else if (!(lhs == Type::integer && rhs == Type::integer)
              && binop.op != Parser::token::EQ
              && binop.op != Parser::token::NE) {
       buf << "The " << opstr(binop)
-          << " operator can not be used on expressions of type " << lhs
-          << std::endl;
+          << " operator can not be used on expressions of types " << lhs
+          << ", " << rhs << std::endl;
       bpftrace_.error(err_, binop.loc, buf.str());
     }
   }
