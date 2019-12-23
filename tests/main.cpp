ddaa482aed644f3f9bc5cab9386a0bdb01ca9834
Alastair Robertson <alastair@ajor.co.uk>
2019-12-23 22:13:28 +0000

    Make ASSERTs in helper functions fail the parent testcase

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,7 +1,19 @@
 #include "gtest/gtest.h"
 
+class ThrowListener : public testing::EmptyTestEventListener
+{
+  void OnTestPartResult(const testing::TestPartResult& result) override
+  {
+    if (result.type() == testing::TestPartResult::kFatalFailure)
+    {
+      throw testing::AssertionException(result);
+    }
+  }
+};
+
 int main(int argc, char *argv[])
 {
   ::testing::InitGoogleTest(&argc, argv);
+  ::testing::UnitTest::GetInstance()->listeners().Append(new ThrowListener);
   return RUN_ALL_TESTS();
 }
