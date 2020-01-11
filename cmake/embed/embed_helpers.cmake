ddf012caf97cb865e5f22a70f8d31188aafd2d04
Dale Hamel <dale.hamel@srvthe.net>
2020-01-11 12:03:14 -0500

    Fix debian libclang only linking

--- cmake/embed/embed_helpers.cmake
+++ cmake/embed/embed_helpers.cmake
@@ -2,6 +2,19 @@ include(ExternalProject)
 include(ProcessorCount)
 include(embed_patches)
 
+# Workaround to remove dynamic libs from library dependencies
+function(unlink_transitive_dependency targets dep_to_remove)
+  foreach(tgt ${targets})
+    get_target_property(int_link_libs ${tgt} INTERFACE_LINK_LIBRARIES)
+
+    if(NOT(int_link_libs MATCHES "-NOTFOUND$"))
+      list(REMOVE_ITEM int_link_libs "${dep_to_remove}")
+      set_target_properties(${tgt} PROPERTIES
+        INTERFACE_LINK_LIBRARIES "${int_link_libs}")
+    endif()
+  endforeach(tgt ${targets})
+endfunction(unlink_transitive_dependency)
+
 # Detect the distribution bpftrace is being built on
 function(detect_host_os os_id)
   file(STRINGS "/etc/os-release" HOST_OS_INFO)
