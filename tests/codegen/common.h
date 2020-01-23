b950baf7bebd0b43c02167878ad51238e0fc94fe
bas smit <bas@baslab.org>
2020-01-23 23:24:09 +0100

    TEST HACKS

--- tests/codegen/common.h
+++ tests/codegen/common.h
@@ -14,6 +14,15 @@
 #include "semantic_analyser.h"
 #include "tracepoint_format_parser.h"
 
+#include <cstdio>
+#include <sys/prctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <fcntl.h>
+
 namespace bpftrace {
 namespace test {
 namespace codegen {
@@ -24,6 +33,20 @@ target datalayout = "e-m:e-p:64:64-i64:64-n32:64-S128"
 target triple = "bpf-pc-linux"
 
 )HEAD";
+
+
+static int llvm_validate_ir(const std::string &data)
+{
+  // TODO: Remove hardcode
+  char cmd[] = "/usr/bin/llvm-as-7 -o /dev/null";
+  FILE *pipe = popen(cmd, "w");
+  if (!pipe) {
+    return 1;
+  }
+
+  fputs(data.c_str(), pipe);
+  return pclose(pipe);
+}
 
 static void test(
     BPFtrace &bpftrace,
@@ -50,7 +73,12 @@ static void test(
   codegen.compile(DebugLevel::kDebug, out);
 
   std::string full_expected_output = header + expected_output;
-  EXPECT_EQ(full_expected_output, out.str()) << "the following program failed: '" << input << "'";
+  /* EXPECT_EQ(full_expected_output, out.str()) << "the following program failed: '" << input << "'"; */
+
+  EXPECT_EQ(llvm_validate_ir(out.str()), 0) << "Validation of LLVM IR failed for:" << std::endl
+                                            << input << std::endl
+                                            << "invalid IR: " << std::endl << out.str();
+
 }
 
 static void test(
