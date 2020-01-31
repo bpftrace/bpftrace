b272a5189dc2a7e0d2863e29cecb477405c39840
Dale Hamel <dale.hamel@srvthe.net>
2020-01-30 21:09:48 -0500

    Android cross compiler functions

--- cmake/embed/embed_helpers.cmake
+++ cmake/embed/embed_helpers.cmake
@@ -1,6 +1,7 @@
 include(ExternalProject)
 include(ProcessorCount)
 include(embed_patches)
+include(embed_platforms)
 
 # Workaround to remove dynamic libs from library dependencies
 function(unlink_transitive_dependency targets dep_to_remove)
@@ -66,14 +67,90 @@ function(get_target_triple out)
   elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Android")
     set(vendor "android")
     set(os "linux")
-    message(AUTHOR_WARNING "Android build not yet fully implemented.")
-  else()
-    message(AUTHOR_WARNING "The target system ${CMAKE_SYSTEM_NAME} isn't supported")
   endif()
   set(triple "${arch}-${vendor}-${os}")
   set(${out} ${triple} PARENT_SCOPE)
   message(STATUS "Detected target triple: ${triple}")
 endfunction()
+
+# If an external dependency doesn't use cmake, it cannot use the toolchain file
+# To substitute for this, the toolchain is exported via standard env vars
+function(get_toolchain_exports out)
+
+  get_target_triple(TARGET_TRIPLE)
+  if(NOT ${TARGET_TRIPLE} MATCHES android)
+    message(FATAL_ERROR "No cross toolchain exists for non-android platform yet")
+  endif()
+
+  set(API_NUM "${ANDROID_NATIVE_API_LEVEL}")
+  set(arch ${CMAKE_SYSTEM_PROCESSOR})
+
+  if("${arch}" MATCHES "armv7")
+    # FIXME use env var for toolchain home
+    set(CROSS_EXPORTS
+        "export TOOLCHAIN=/opt/android-ndk/toolchains/llvm/prebuilt/linux-x86_64 && \
+        export AR=$TOOLCHAIN/bin/arm-linux-androideabi-ar && \
+        export AS=$TOOLCHAIN/bin/arm-linux-androideabi-as && \
+        export CC=$TOOLCHAIN/bin/armv7a-linux-androideabi${API_NUM}-clang && \
+        export CXX=$TOOLCHAIN/bin/armv7a-linux-androideabi${API_NUM}-clang++ && \
+        export LD=$TOOLCHAIN/bin/arm-linux-androideabi-ld && \
+        export RANLIB=$TOOLCHAIN/bin/arm-linux-androideabi-ranlib && \
+        export STRIP=$TOOLCHAIN/bin/arm-linux-androideabi-strip"
+        )
+  elseif("${arch}" MATCHES "x86_64")
+    set(CROSS_EXPORTS
+        "export TOOLCHAIN=/opt/android-ndk/toolchains/llvm/prebuilt/linux-x86_64 && \
+        export AR=$TOOLCHAIN/bin/x86_64-linux-android-ar && \
+        export AS=$TOOLCHAIN/bin/x86_64-linux-android-as && \
+        export CC=$TOOLCHAIN/bin/x86_64-linux-android${API_NUM}-clang && \
+        export CXX=$TOOLCHAIN/bin/x86_64-linux-android${API_NUM}-clang++ && \
+        export LD=$TOOLCHAIN/bin/x86_64-linux-android-ld && \
+        export RANLIB=$TOOLCHAIN/bin/x86_64-linux-android-ranlib && \
+        export STRIP=$TOOLCHAIN/bin/x86_64-linux-android-strip"
+        )
+  elseif("${arch}" MATCHES "aarch64")
+    set(CROSS_EXPORTS
+        "export TOOLCHAIN=/opt/android-ndk/toolchains/llvm/prebuilt/linux-x86_64 && \
+        export AR=$TOOLCHAIN/bin/aarch64-linux-android-ar && \
+        export AS=$TOOLCHAIN/bin/aarch64-linux-android-as && \
+        export CC=$TOOLCHAIN/bin/aarch64-linux-android${API_NUM}-clang && \
+        export CXX=$TOOLCHAIN/bin/aarch64-linux-android${API_NUM}-clang++ && \
+        export LD=$TOOLCHAIN/bin/aarch64-linux-android-ld && \
+        export RANLIB=$TOOLCHAIN/bin/aarch64-linux-android-ranlib && \
+        export STRIP=$TOOLCHAIN/bin/aarch64-linux-android-strip"
+        )
+  else()
+    message(FATAL_ERROR "${arch} doesn't match any supported android ABI")
+  endif()
+
+  set(${out} "${CROSS_EXPORTS}" PARENT_SCOPE)
+endfunction(get_toolchain_exports out)
+
+function(get_android_cross_tuple out)
+  set(arch ${CMAKE_SYSTEM_PROCESSOR})
+  if("${arch}" MATCHES "armv7")
+    set(${out} "armv7a-linux-androideabi" PARENT_SCOPE)
+  elseif("${arch}" MATCHES "x86_64")
+    set(${out} "x86_64-linux-android" PARENT_SCOPE)
+  elseif("${arch}" MATCHES "aarch64")
+    set(${out} "aarch64-linux-android" PARENT_SCOPE)
+  else()
+    message(FATAL_ERROR "${arch} doesn't match any supported android ABI")
+  endif()
+endfunction(get_android_cross_tuple out)
+
+function(get_android_wordsize out)
+  set(arch ${CMAKE_SYSTEM_PROCESSOR})
+  if("${arch}" MATCHES "armv7")
+    set(${out} 32 PARENT_SCOPE)
+  elseif("${arch}" MATCHES "x86_64")
+    set(${out} 64 PARENT_SCOPE)
+  elseif("${arch}" MATCHES "aarch64")
+    set(${out} 64 PARENT_SCOPE)
+  else()
+    message(FATAL_ERROR "${arch} doesn't match any supported android ABI")
+  endif()
+endfunction(get_android_wordsize out)
 
 function(fix_llvm_linkflags targetProperty propertyValue)
   set_target_properties(${target_property} PROPERTIES
@@ -99,9 +176,13 @@ function(fetch_patches patchName patchPath patchURL patchChecksum stripLevel)
     # Can add to this if ladder to support additional patch formats, tar
     # probably catches quit a lot...
     if(patchName MATCHES .*tar.*)
+      if(NOT ${stripLevel})
+        set(stripLevel 0)
+      endif()
+      message("Extracting ${patchName} to ${patchPath}, strip level ${stripLevel}")
       execute_process(COMMAND tar -xpf ${patchPath}/${patchName} --strip-components=${stripLevel} -C ${patchPath})
     else()
-      message("Patch ${patchName} doesn't appear to a tar achive, assuming it is a plaintext patch")
+      message(WARNING "Patch ${patchName} doesn't appear to a tar achive, assuming it is a plaintext patch")
     endif()
   endif()
-endfunction(fetch_patches patchName patchPatch patchURL patchChecksum)
+endfunction(fetch_patches patchName patchPatch patchURL patchChecksum stripLevel)
