bpftrace/bpftrace

commit 76770de39e33b3971062248d8ba105c2abb24101

Report kernel instruction limit
This makes use of the verifier log to find the instruction limit of the
kernel.

The log message is the similar between all kernel versions I've tested
(centos 7, fedora 31 and ubuntu 19.10) which makes extracting the limit
quite easy, which is also why I left any error handling out.

```
$ bpftrace --info
Kernel helpers
  get_current_cgroup_id: yes
  send_signal: yes
  override_return: no

Kernel features
  Instruction limit: 1000000
  Loop support: yes
```

src/bpffeature.cpp
@@ -84,12 +84,58 @@ static bool detect_override_return(void)
       "test_override_return", BPF_PROG_TYPE_KPROBE, insns, sizeof(insns));
 }
 
+static int detect_instruction_limit(void)
+{
+  struct bpf_insn insns[] = {
+    BPF_LD_IMM64(BPF_REG_0, 0),
+    BPF_EXIT_INSN(),
+  };
+
+  constexpr int log_size = 4096;
+  char logbuf[log_size] = {};
+  int loglevel = 1;
+  int ret = 0;
+#ifdef HAVE_BCC_PROG_LOAD
+  ret = bcc_prog_load(BPF_PROG_TYPE_KPROBE,
+                      "ins_count",
+                      insns,
+                      sizeof(insns),
+                      "GPL",
+                      0,
+                      loglevel,
+                      logbuf,
+                      log_size);
+#else
+  ret = bpf_prog_load(BPF_PROG_TYPE_KPROBE,
+                      "ins_count",
+                      insns,
+                      sizeof(insns),
+                      "GPL",
+                      0,
+                      loglevel,
+                      logbuf,
+                      log_size);
+#endif
+  if (ret >= 0)
+    close(ret);
+
+  // Extract limit from the verifier log:
+  // processed 2 insns (limit 131072), stack depth 0
+  std::string log(logbuf, log_size);
+  std::size_t line_start = log.find("processed 2 insns");
+  std::size_t begin = log.find("limit", line_start) + /* "limit " = 6*/ 6;
+  std::size_t end = log.find(")", begin);
+  std::string cnt = log.substr(begin, end - begin);
+  return std::stoi(cnt);
+}
+
 BPFfeature::BPFfeature(void)
 {
   has_loop_ = detect_loop();
   has_signal_ = detect_signal();
   has_get_current_cgroup_id_ = detect_get_current_cgroup_id();
   has_override_return_ = detect_override_return();
+  insns_limit_ = detect_instruction_limit();
 }
 
 std::string BPFfeature::report(void)
@@ -104,6 +150,7 @@ std::string BPFfeature::report(void)
       << std::endl
       << std::endl
       << "Kernel features" << std::endl
+      << "  Instruction limit: " << std::to_string(insns_limit_) << std::endl
       << "  Loop support: " << to_str(has_loop()) << std::endl
       << std::endl;
   return buf.str();

src/bpffeature.h
@@ -24,10 +24,15 @@ public:
   {
     return has_override_return_;
   };
+  bool instruction_limit(void)
+  {
+    return insns_limit_;
+  };
   std::string report(void);
 
 protected:
   bool has_loop_;
+  int insns_limit_;
 
   /* Helpers */
   bool has_signal_;

commit b727f0340a3f7f254ba5c4c63645d711180b8760

Allow running tests as non-root (again)
Commit 76770de39e ("Report kernel instruction limit") broke non-root
tests again. It was crashing non-root tests with:

    bpf: Failed to load program: Operation not permitted

    unknown file: Failure
    C++ exception with description "stoi" thrown in the test body.

This commit gracefully handles errors.

src/bpffeature.cpp
@@ -95,28 +95,36 @@ static int detect_instruction_limit(void)
   char logbuf[log_size] = {};
   int loglevel = 1;
   int ret = 0;
+  {
+    // Don't want to spam unit tests with failure messages
+    StderrSilencer silencer;
+    silencer.silence();
+
 #ifdef HAVE_BCC_PROG_LOAD
-  ret = bcc_prog_load(BPF_PROG_TYPE_KPROBE,
-                      "ins_count",
-                      insns,
-                      sizeof(insns),
-                      "GPL",
-                      0,
-                      loglevel,
-                      logbuf,
-                      log_size);
+    ret = bcc_prog_load(BPF_PROG_TYPE_KPROBE,
+                        "ins_count",
+                        insns,
+                        sizeof(insns),
+                        "GPL",
+                        0,
+                        loglevel,
+                        logbuf,
+                        log_size);
 #else
-  ret = bpf_prog_load(BPF_PROG_TYPE_KPROBE,
-                      "ins_count",
-                      insns,
-                      sizeof(insns),
-                      "GPL",
-                      0,
-                      loglevel,
-                      logbuf,
-                      log_size);
+    ret = bpf_prog_load(BPF_PROG_TYPE_KPROBE,
+                        "ins_count",
+                        insns,
+                        sizeof(insns),
+                        "GPL",
+                        0,
+                        loglevel,
+                        logbuf,
+                        log_size);
 #endif
-  if (ret >= 0)
+  }
+  if (ret < 0)
+    return 0;
+  else
     close(ret);
 
   // Extract limit from the verifier log:
@@ -150,7 +158,8 @@ std::string BPFfeature::report(void)
       << std::endl
       << std::endl
       << "Kernel features" << std::endl
-      << "  Instruction limit: " << std::to_string(insns_limit_) << std::endl
+      << "  Instruction limit: "
+      << (insns_limit_ ? std::to_string(insns_limit_) : "?") << std::endl
       << "  Loop support: " << to_str(has_loop()) << std::endl
       << std::endl;
   return buf.str();