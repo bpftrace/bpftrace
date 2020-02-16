e3d3f03366a6f979bc2defeaceb321e786dba523
bas smit <bas@baslab.org>
2020-02-16 20:33:23 +0100

    bpffeature: detect supported map types
    In some cases, kernel lockdown being the main one, certain required map
    types (like PERF_EVENT_ARRAY) are blocked which make it impossible for
    bpftrace to function. By adding detection to bpffeature we can easily
    test for cases like this and abort in the appropriate place.

--- src/bpffeature.h
+++ src/bpffeature.h
@@ -17,11 +17,26 @@ public:
   bool has_helper_get_current_cgroup_id();
   bool has_helper_send_signal();
 
+  bool has_map_array();
+  bool has_map_hash();
+  bool has_map_percpu_array();
+  bool has_map_percpu_hash();
+  bool has_map_stack_trace();
+  bool has_map_perf_event_array();
+
   std::string report(void);
 
 protected:
   std::unique_ptr<bool> has_loop_;
   std::unique_ptr<int> insns_limit_;
+
+  /* Map types */
+  std::unique_ptr<bool> map_hash_;
+  std::unique_ptr<bool> map_percpu_hash_;
+  std::unique_ptr<bool> map_array_;
+  std::unique_ptr<bool> map_percpu_array_;
+  std::unique_ptr<bool> map_stack_trace_;
+  std::unique_ptr<bool> map_perf_event_array_;
 
   /* Helpers */
   std::unique_ptr<bool> has_send_signal_;
