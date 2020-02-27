4f49c7c4c3808f2ad67e50924a7bc9f7a0c0b144
Daniel Xu <dxu@dxuuu.xyz>
2020-02-27 10:04:11 -0800

    Use host network when building docker image
    I was getting these errors before:
    ```
    fetch http://dl-cdn.alpinelinux.org/alpine/v3.8/main/x86_64/APKINDEX.tar.gz
    ERROR: http://dl-cdn.alpinelinux.org/alpine/v3.8/main: temporary error (try again later)
    WARNING: Ignoring APKINDEX.adfa7ceb.tar.gz: No such file or directory
    fetch http://dl-cdn.alpinelinux.org/alpine/v3.8/community/x86_64/APKINDEX.tar.gz
    ERROR: http://dl-cdn.alpinelinux.org/alpine/v3.8/community: temporary error (try again later)
    WARNING: Ignoring APKINDEX.efaa1f73.tar.gz: No such file or directory
    ERROR: unsatisfiable constraints:
    ```
    
    Adding this flag seemed to fix the issue.

--- build-debug.sh
+++ build-debug.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-docker run --rm -it -u $(id -u):$(id -g) -v $(pwd):$(pwd) -e STATIC_LINKING=ON -e STATIC_LIBC=ON -e RUN_TESTS=0 bpftrace-builder-alpine "$(pwd)/build-debug" Debug "$@"
+docker run --network host --rm -it -u $(id -u):$(id -g) -v $(pwd):$(pwd) -e STATIC_LINKING=ON -e STATIC_LIBC=ON -e RUN_TESTS=0 bpftrace-builder-alpine "$(pwd)/build-debug" Debug "$@"
