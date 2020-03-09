bpftrace/bpftrace

commit 4ad2bf195f6bdf92721ff0a7c92f1bc30eaecd44

Remove type qualifiers from a cast_type
Clang parser does not remove type qualifiers ("const", "volatile, and
"restrict" keywords) from a cast_type of Type::cast, and this results in
a casting error. For example:

```
% sudo ./src/bpftrace  -e "kprobe:vfs_open { @ = ((struct file*)arg1)->f_cred->usage; }"
stdin:1:23-54: ERROR: Unknown struct/union: 'const struct cred'
kprobe:vfs_open { @ = ((struct file*)arg1)->f_cred->usage; }
```

Remove type qualifiers to fix this. libclang C API does not provide a
direct way to remove type qualifiers, so  remove "const" / "volatile" /
"restrict" keywords from a string obtained by `clang_getTypeSpelling()`.

In addition to this, fix BTF::type_of to remove type qualifiers from a
btf_type. Otherwise, BTF::type_of fails to get a struct name (it'll
return "(anon)").

src/clang_parser.cpp
@@ -1,5 +1,6 @@
 #include <cstring>
 #include <iostream>
+#include <regex>
 
 #include "llvm/Config/llvm-config.h"
 
@@ -201,10 +202,25 @@ static bool translateMacro(CXCursor cursor, std::string &name, std::string &valu
   return value.length() != 0;
 }
 
+static std::string remove_qualifiers(std::string &&typestr)
+{
+  // libclang prints "const" keyword first
+  // https://github.com/llvm-mirror/clang/blob/65acf43270ea2894dffa0d0b292b92402f80c8cb/lib/AST/TypePrinter.cpp#L137-L157
+  return std::regex_replace(typestr,
+                            std::regex("^(const volatile\\s+)|^(const\\s+)|"
+                                       "^(volatile\\s+)|\\*(\\s*restrict)$"),
+                            "");
+}
+
+static std::string get_unqualified_type_name(CXType clang_type)
+{
+  return remove_qualifiers(get_clang_string(clang_getTypeSpelling(clang_type)));
+}
+
 static SizedType get_sized_type(CXType clang_type)
 {
   auto size = clang_Type_getSizeOf(clang_type);
-  auto typestr = get_clang_string(clang_getTypeSpelling(clang_type));
+  auto typestr = get_unqualified_type_name(clang_type);
 
   switch (clang_type.kind)
   {
@@ -233,7 +249,7 @@ static SizedType get_sized_type(CXType clang_type)
       SizedType type;
       if (pointee_type.kind == CXType_Record)
       {
-        auto pointee_typestr = get_clang_string(clang_getTypeSpelling(pointee_type));
+        auto pointee_typestr = get_unqualified_type_name(pointee_type);
         type = SizedType(Type::cast, sizeof(uintptr_t), pointee_typestr);
       }
       else
@@ -357,7 +373,7 @@ bool ClangParser::visit_children(CXCursor &cursor, BPFtrace &bpftrace)
 
           auto named_parent = get_named_parent(c);
           auto ptype = clang_getCanonicalType(clang_getCursorType(named_parent));
-          auto ptypestr = get_clang_string(clang_getTypeSpelling(ptype));
+          auto ptypestr = get_unqualified_type_name(ptype);
           auto ptypesize = clang_Type_getSizeOf(ptype);
 
           auto ident = get_clang_string(clang_getCursorSpelling(c));

tests/clang_parser.cpp
@@ -709,6 +709,22 @@ TEST(clang_parser, struct_typedef)
   EXPECT_EQ(structs["max_align_t"].fields["__clang_max_align_nonce2"].offset, 16);
 }
 
+TEST(clang_parser, struct_qualifiers)
+{
+  BPFtrace bpftrace;
+  parse("struct a {int a} struct b { volatile const struct a* restrict a; "
+        "const struct a a2; };",
+        bpftrace);
+
+  StructMap &structs = bpftrace.structs_;
+  EXPECT_EQ(structs["struct b"].size, 16);
+  EXPECT_EQ(structs["struct b"].fields.size(), 2U);
+  EXPECT_EQ(structs["struct b"].fields["a"].type.type, Type::cast);
+  EXPECT_EQ(structs["struct b"].fields["a"].type.cast_type, "struct a");
+  EXPECT_EQ(structs["struct b"].fields["a2"].type.type, Type::cast);
+  EXPECT_EQ(structs["struct b"].fields["a2"].type.cast_type, "struct a");
+}
+
 } // namespace clang_parser
 } // namespace test
 } // namespace bpftrace

src/btf.cpp
@@ -288,8 +288,12 @@ std::string BTF::type_of(const btf_type *type, const std::string &field)
     if (!f)
       break;
 
-    // Get rid of all the pointers on the way to the actual type.
-    while (BTF_INFO_KIND(f->info) == BTF_KIND_PTR) {
+    // Get rid of all the pointers and qualifiers on the way to the actual type.
+    while (BTF_INFO_KIND(f->info) == BTF_KIND_PTR ||
+           BTF_INFO_KIND(f->info) == BTF_KIND_CONST ||
+           BTF_INFO_KIND(f->info) == BTF_KIND_VOLATILE ||
+           BTF_INFO_KIND(f->info) == BTF_KIND_RESTRICT)
+    {
       f = btf__type_by_id(btf, f->type);
     }
 

tests/data/btf_data.h
@@ -20,7 +20,7 @@
 //
 //  struct Foo3 {
 //    struct Foo1 *foo1;
-//    struct Foo2 *foo2;
+//    const volatile struct Foo2 * restrict foo2;
 //  };
 
 // generated by:
@@ -31,8 +31,8 @@
 //  % llvm-objcopy --dump-section .BTF=a.btf a.o
 //  % hexdump -ve '13/1 "0x%02x, " "\n"' a.btf
 unsigned char btf_data[] = {
-  0x9f, 0xeb, 0x01, 0x00, 0x18, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xfc,
-  0x00, 0x00, 0x00, 0xfc, 0x00, 0x00, 0x00, 0x36, 0x00, 0x00, 0x00, 0x01, 0x00,
+  0x9f, 0xeb, 0x01, 0x00, 0x18, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20,
+  0x01, 0x00, 0x00, 0x20, 0x01, 0x00, 0x00, 0x36, 0x00, 0x00, 0x00, 0x01, 0x00,
   0x00, 0x00, 0x02, 0x00, 0x00, 0x04, 0x10, 0x00, 0x00, 0x00, 0x06, 0x00, 0x00,
   0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0b, 0x00, 0x00, 0x00,
   0x07, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -44,18 +44,21 @@ unsigned char btf_data[] = {
   0x00, 0x00, 0x20, 0x00, 0x00, 0x01, 0x1f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
   0x01, 0x01, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x01, 0x24, 0x00, 0x00, 0x00,
   0x00, 0x00, 0x00, 0x01, 0x08, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x01, 0x00,
-  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x08, 0x00, 0x00, 0x00, 0x2d, 0x00,
-  0x00, 0x00, 0x02, 0x00, 0x00, 0x04, 0x18, 0x00, 0x00, 0x00, 0x15, 0x00, 0x00,
-  0x00, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-  0x09, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02,
-  0x00, 0x00, 0x05, 0x10, 0x00, 0x00, 0x00, 0x32, 0x00, 0x00, 0x00, 0x03, 0x00,
-  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0a, 0x00, 0x00,
-  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x04,
-  0x01, 0x00, 0x00, 0x00, 0x34, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x00,
-  0x00, 0x00, 0x00, 0x00, 0x46, 0x6f, 0x6f, 0x33, 0x00, 0x66, 0x6f, 0x6f, 0x31,
-  0x00, 0x66, 0x6f, 0x6f, 0x32, 0x00, 0x46, 0x6f, 0x6f, 0x31, 0x00, 0x61, 0x00,
-  0x62, 0x00, 0x63, 0x00, 0x69, 0x6e, 0x74, 0x00, 0x63, 0x68, 0x61, 0x72, 0x00,
-  0x6c, 0x6f, 0x6e, 0x67, 0x20, 0x69, 0x6e, 0x74, 0x00, 0x46, 0x6f, 0x6f, 0x32,
-  0x00, 0x66, 0x00, 0x67, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x09, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x0a, 0x0a, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x09, 0x0b, 0x00, 0x00, 0x00, 0x2d, 0x00, 0x00, 0x00, 0x02,
+  0x00, 0x00, 0x04, 0x18, 0x00, 0x00, 0x00, 0x15, 0x00, 0x00, 0x00, 0x04, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0c, 0x00, 0x00,
+  0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x05,
+  0x10, 0x00, 0x00, 0x00, 0x32, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0d, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x04, 0x01, 0x00, 0x00,
+  0x00, 0x34, 0x00, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x46, 0x6f, 0x6f, 0x33, 0x00, 0x66, 0x6f, 0x6f, 0x31, 0x00, 0x66, 0x6f,
+  0x6f, 0x32, 0x00, 0x46, 0x6f, 0x6f, 0x31, 0x00, 0x61, 0x00, 0x62, 0x00, 0x63,
+  0x00, 0x69, 0x6e, 0x74, 0x00, 0x63, 0x68, 0x61, 0x72, 0x00, 0x6c, 0x6f, 0x6e,
+  0x67, 0x20, 0x69, 0x6e, 0x74, 0x00, 0x46, 0x6f, 0x6f, 0x32, 0x00, 0x66, 0x00,
+  0x67, 0x00,
 };
 unsigned int btf_data_len = sizeof(btf_data) / sizeof(btf_data[0]);

commit 7df9216962aae08ecd991b2e5e54e30728b6b9c0

Avoid calling "slow" regex constructor
On my ubuntu VM it took ~17 seconds before `tcpconnect.bt` was compiled,
most of the time spent in the newly added `remove)qualifiers`:

```
Time spent in remove_qualifiers: 17502 ms
Time spent in remove_qualifiers: 16662 ms
```

Which is mostly caused by the regex constructor being called 27k times.

With this patch the average of 10 runs is down to: 575ms

This fixes a regression introduced by #1177

src/clang_parser.cpp
@@ -206,10 +206,9 @@ static std::string remove_qualifiers(std::string &&typestr)
 {
   // libclang prints "const" keyword first
   // https://github.com/llvm-mirror/clang/blob/65acf43270ea2894dffa0d0b292b92402f80c8cb/lib/AST/TypePrinter.cpp#L137-L157
-  return std::regex_replace(typestr,
-                            std::regex("^(const volatile\\s+)|^(const\\s+)|"
-                                       "^(volatile\\s+)|\\*(\\s*restrict)$"),
-                            "");
+  static std::regex re("^(const volatile\\s+)|^(const\\s+)|"
+                       "^(volatile\\s+)|\\*(\\s*restrict)$");
+  return std::regex_replace(typestr, re, "");
 }
 
 static std::string get_unqualified_type_name(CXType clang_type)