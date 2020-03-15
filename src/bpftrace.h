8008c5a9282b16e9ee8a2e3ea244fd312486a89c
Chris Hunt <chrahunt@gmail.com>
2020-03-15 19:08:24 -0400

    Remove leftover print_lhist declaration
    The implementation was removed in 1fc0c1f7, but not the declaration.

--- src/bpftrace.h
+++ src/bpftrace.h
@@ -214,10 +214,6 @@ private:
   int print_map_lhist(IMap &map);
   int print_map_stats(IMap &map);
   int print_hist(const std::vector<uint64_t> &values, uint32_t div) const;
-  int print_lhist(const std::vector<uint64_t> &values,
-                  int min,
-                  int max,
-                  int step) const;
   template <typename T>
   static T reduce_value(const std::vector<uint8_t> &value, int nvalues);
   static int64_t min_value(const std::vector<uint8_t> &value, int nvalues);
