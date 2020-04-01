f70ee7ed8cf8db25c4d425cde7957e0d747820c5
Jiri Olsa <jolsa@kernel.org>
2020-04-01 20:35:51 +0200

    Resolve kfunc arguments in semantic analyser
    Resolving function arguments for kfunc/kretfunc attach points
    and storing them in map 'attach point name' -> arguments.
    
    Using that map to provide types for args and retval builtins
    under kfunc/kretfunc attach points.
    
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- src/ast/semantic_analyser.h
+++ src/ast/semantic_analyser.h
@@ -83,6 +83,7 @@ private:
   std::map<std::string, SizedType> map_val_;
   std::map<std::string, MapKey> map_key_;
   std::map<std::string, ExpressionList> map_args_;
+  std::map<std::string, SizedType> ap_args_;
   std::unordered_set<StackType> needs_stackid_maps_;
   bool needs_join_map_ = false;
   bool needs_elapsed_map_ = false;
