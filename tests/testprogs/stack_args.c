c90cec22f20b05e3a25d9ea9ab1ca1be87066d77
Sumanth Korikkar <sumanthk@linux.ibm.com>
2020-04-06 09:48:56 +0200

    bpftrace: Add architecture specific testcase support
    - Add ARCH support for the test tool
    - Fix architecture specific testcases
    
    Signed-off-by: Sumanth Korikkar <sumanthk@linux.ibm.com>

--- tests/testprogs/stack_args.c
+++ tests/testprogs/stack_args.c
@@ -1,10 +1,21 @@
 #define ARG(x) int (x) __attribute((unused))
 
 // Declare enough args that some are placed on the stack
-void too_many_args(ARG(a), ARG(b), ARG(c), ARG(d), ARG(e), ARG(f), ARG(g), ARG(h)) {}
+void too_many_args(ARG(a),
+                   ARG(b),
+                   ARG(c),
+                   ARG(d),
+                   ARG(e),
+                   ARG(f),
+                   ARG(g),
+                   ARG(h),
+                   ARG(i),
+                   ARG(j))
+{
+}
 
 int main(void)
 {
-  too_many_args(0, 1, 2, 4, 8, 16, 32, 64);
+  too_many_args(0, 1, 2, 4, 8, 16, 32, 64, 128, 256);
   return 0;
 }
