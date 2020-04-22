6d9bc52cbcc199c3a090482560a0f03662c97893
bas smit <bas@baslab.org>
2020-04-23 00:47:51 +0200

    Support vagrant in tools-parsing-test
    Quick work around. Using the cmake source dir is still a better long
    term solution.

--- tests/tools-parsing-test.sh
+++ tests/tools-parsing-test.sh
@@ -13,8 +13,23 @@ BPFTRACE_EXECUTABLE=${BPFTRACE_EXECUTABLE:-$DIR/../src/bpftrace};
 
 EXIT_STATUS=0;
 
-# TODO(mmarchini) get path from cmake
-for f in $(ls ../../tools/*.bt); do
+TOOLDIR=""
+
+function tooldir() {
+    for dir in "../../tools" "/vagrant/tools"; do
+        if [[ -d "$dir" ]]; then
+            TOOLDIR="$dir"
+            return
+        fi
+    done
+
+    >&2 echo "Tool dir not found"
+    exit 1
+}
+
+tooldir
+
+for f in "$TOOLDIR"/*.bt; do
   if $BPFTRACE_EXECUTABLE --unsafe -d $f 2>/dev/null >/dev/null; then
     echo "$f    passed"
   else
