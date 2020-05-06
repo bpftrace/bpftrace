3584aec0502394594ef170c785138abf149d6964
Sumanth Korikkar <sumanthk@linux.ibm.com>
2020-05-06 11:21:09 -0500

    bpftrace: Fix usdt reads in various architecture
    In USDT tracing, bpftrace uses bcc library function
    bcc_usdt_get_argument to retrieve the arg passed to the DTRACE_PROBE().
    The value present in the arg string is "gprs[0-15]" in s390. However it
    is named as "r[0-15]" in bpftrace. This leads to offset for register
    "gprs[0-15]" not known error.
    
    Fix this by creating an alias of each register. aarch64 already does it.
    x86 does have the same convention in bpftrace and bcc.
    
    Signed-off-by: Sumanth Korikkar <sumanthk@linux.ibm.com>

--- src/arch/s390.cpp
+++ src/arch/s390.cpp
@@ -2,8 +2,9 @@
 
 #include <algorithm>
 #include <array>
+#include <set>
+#include <vector>
 
-#define REQ_REGISTERS 19
 #define ARG_REGISTERS 5
 // For s390x, r2-r6 registers are used as function arguments, then the extra
 // arguments can be found starting at sp+160
@@ -13,28 +14,28 @@ namespace bpftrace {
 namespace arch {
 
 // clang-format off
-static std::array<std::string, REQ_REGISTERS> registers = {
+static std::vector<std::set<std::string>> registers = {
   // Breakpoint event address
-  "arg",
-  "pswmask",
+  { "arg" },
+  { "pswmask" },
   // Instruction address
-  "pswaddr",
-  "r0",
-  "r1",
-  "r2",
-  "r3",
-  "r4",
-  "r5",
-  "r6",
-  "r7",
-  "r8",
-  "r9",
-  "r10",
-  "r11",
-  "r12",
-  "r13",
-  "r14",
-  "r15",
+  { "pswaddr" },
+  { "r0", "gprs[0]" },
+  { "r1", "gprs[1]" },
+  { "r2", "gprs[2]" },
+  { "r3", "gprs[3]" },
+  { "r4", "gprs[4]" },
+  { "r5", "gprs[5]" },
+  { "r6", "gprs[6]" },
+  { "r7", "gprs[7]" },
+  { "r8", "gprs[8]" },
+  { "r9", "gprs[9]" },
+  { "r10", "gprs[10]" },
+  { "r11", "gprs[11]" },
+  { "r12", "gprs[12]" },
+  { "r13", "gprs[13]" },
+  { "r14", "gprs[14]" },
+  { "r15", "gprs[15]" }
 };
 
 static std::array<std::string, ARG_REGISTERS> arg_registers = {
@@ -48,10 +49,12 @@ static std::array<std::string, ARG_REGISTERS> arg_registers = {
 
 int offset(std::string reg_name)
 {
-  auto it = find(registers.begin(), registers.end(), reg_name);
-  if (it == registers.end())
-    return -1;
-  return distance(registers.begin(), it);
+  for (unsigned int i = 0; i < registers.size(); i++)
+  {
+    if (registers[i].count(reg_name))
+      return i;
+  }
+  return -1;
 }
 
 int max_arg()
