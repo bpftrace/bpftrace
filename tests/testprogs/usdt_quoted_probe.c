28ebc99ea637a05dbc574c9f25fae04db609325d
Daniel Xu <dxu@dxuuu.xyz>
2020-05-26 14:53:09 -0700

    tests: Fix embedded build tests
    Forgot to add checks for <sys/sdt.h> headers.

--- tests/testprogs/usdt_quoted_probe.c
+++ tests/testprogs/usdt_quoted_probe.c
@@ -6,8 +6,17 @@
 #define DTRACE_PROBE1(a, b, c) (void)0
 #endif
 
-int main()
+int main(int argc, char **argv __attribute__((unused)))
 {
+  if (argc > 1)
+  // If we don't have Systemtap headers, we should skip USDT tests. Returning 1
+  // can be used as validation in the REQUIRE
+#ifndef HAVE_SYSTEMTAP_SYS_SDT_H
+    return 1;
+#else
+    return 0;
+#endif
+
   int a = 1;
   // For some reason some compilers think `a` is unused
   (void)a;
