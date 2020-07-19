e039b9e00507b246afc7434435ee8a4cb72b42eb
bas smit <bas@baslab.org>
2020-07-19 16:00:37 +0200

    testing: add script to compare BPF between versions
    This uses the ELF output option to compare the generated BPF between two
    versions.

--- scripts/compare_tool_elf.sh
+++ scripts/compare_tool_elf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Compare the IR generated for the shipped
+# Compare the ELF generated for the shipped
 # tools between two bpftrace builds
 #
 
@@ -8,31 +8,23 @@ set -o pipefail
 set -e
 set -u
 
-if [[ "$#" -ne 3 ]]; then
+if [[ "$#" -ne 4 ]]; then
   echo "Compare IR generated between two bpftrace builds"
   echo ""
   echo "USAGE:"
-  echo "$(basename $0) <bpftrace_A> <bpftrace_B> <tooldir>"
+  echo "$(basename $0) <bpftrace_A> <bpftrace_B> <objdump> <tooldir>"
   echo ""
   echo "EXAMPLE:"
-  echo "$(basename $0) bpftrace bpftrace_master /vagrant/tools"
+  echo "$(basename $0) bpftrace_master bpftrace llvm-objdump-7 /vagrant/tools"
   echo ""
   exit 1
 fi
 
-TOOLDIR=$3
 BPF_A=$(command -v "$1") || ( echo "ERROR: $1 not found"; exit 1 )
 BPF_B=$(command -v "$2") || ( echo "ERROR: $2 not found"; exit 1 )
+OBJDUMP=$(command -v "$3") || (echo "ERROR: $3 not found"; exit 1 )
+TOOLDIR=$4
 [[ -d "$TOOLDIR" ]] || (echo "tooldir does not appear to be a directory: ${TOOLDIR}"; exit 1)
-
-# Set to 1 to only compare result after opt
-AFTER_OPT=0
-
-if [ $AFTER_OPT -eq 1 ]; then
-  FLAGS="-d"
-else
-  FLAGS="-dd"
-fi
 
 TMPDIR=$(mktemp -d)
 [[ $? -ne 0 || -z $TMPDIR ]] && (echo "Failed to create tmp dir"; exit 10)
@@ -45,17 +37,13 @@ function hash() {
     sha1sum "${1}" | awk '{print $1}'
 }
 
-function fix_timestamp() {
-    cat $@ | awk '/(add|sub) i64 %get_ns/ { $NF = ""} {print}'
-}
-
 echo "Using version $($BPF_A -V) and $($BPF_B -V)"
 
 for script in ${TOOLDIR}/*.bt; do
     s=$(basename ${script/.bt/})
     echo "Checking $s"
-    2>&1 $BPF_A "$FLAGS" "$script" | fix_timestamp > "a_${s}"
-    2>&1 $BPF_B "$FLAGS" "$script" | fix_timestamp > "b_${s}"
+    2>&1 $BPF_A --emit-elf "a_${s}" "$script" >/dev/null
+    2>&1 $BPF_B --emit-elf "b_${s}" "$script" >/dev/null
     if [ $? -ne 0 ]; then
         echo "###############################"
         echo "bpftrace failed on script: ${s}"
@@ -65,7 +53,7 @@ for script in ${TOOLDIR}/*.bt; do
     if [[ $(hash "a_${s}") != $(hash "b_${s}")  ]]; then
         echo "###############################"
         echo "Change detected for script: ${s}"
-        diff -b -u "a_${s}" "b_${s}"
+        diff -u <($OBJDUMP "a_${s}") <($OBJDUMP "b_${s}")
     fi
 done
 
