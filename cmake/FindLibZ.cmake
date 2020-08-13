ffa1a805133b6f554f16674272e0b0ab0a0bd053
Viktor Malik <viktor.malik@gmail.com>
2020-08-13 07:36:54 +0200

    Separate CMake package for finding libz
    Move finding libz into a separate file FindLibZ.cmake. This is useful
    since libz will be needed from multiple places.

---
+++ cmake/FindLibZ.cmake
@@ -0,0 +1,27 @@
+# - Try to find libz
+# Once done this will define
+#
+#  LIBZ_FOUND - system has libz
+#  LIBZ_LIBRARIES - Link these to use libz
+
+find_library(LIBZ_LIBRARIES
+  NAMES
+    libz.a
+  PATHS
+    /lib
+    /usr/lib
+    /usr/local/lib
+    /opt/local/lib
+    /usr/lib/x86_64-linux-gnu/
+    /sw/lib
+    ENV LIBRARY_PATH
+    ENV LD_LIBRARY_PATH)
+
+include (FindPackageHandleStandardArgs)
+
+
+# handle the QUIETLY and REQUIRED arguments and set LIBZ_FOUND to TRUE if all listed variables are TRUE
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibZ "Please install the libz package"
+  LIBZ_LIBRARIES)
+
+mark_as_advanced(LIBZ_LIBRARIES)
