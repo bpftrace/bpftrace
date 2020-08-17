6ea0b2e0173cbfda6c701693a0f27db5426963c7
bas smit <bas@baslab.org>
2020-08-17 23:15:10 +0200

    cmake: detect map batch operators

--- cmake/FindLibBpf.cmake
+++ cmake/FindLibBpf.cmake
@@ -55,6 +55,8 @@ if (LIBBPF_FOUND)
     set(LIBBPF_BTF_DUMP_FOUND TRUE)
   endif()
   check_symbol_exists(btf_dump__emit_type_decl "${LIBBPF_INCLUDE_DIRS}/bpf/btf.h" HAVE_LIBBPF_BTF_DUMP_EMIT_TYPE_DECL)
+
+  check_symbol_exists(bpf_map_lookup_batch "${LIBBPF_INCLUDE_DIRS}/bpf/bpf.h" HAVE_LIBBPF_MAP_BATCH)
   SET(CMAKE_REQUIRED_DEFINITIONS)
   SET(CMAKE_REQUIRED_LIBRARIES)
 endif()
