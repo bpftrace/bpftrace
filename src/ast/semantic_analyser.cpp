4d1c4d300b155171f77d76e77f0ebef26fb4041f
bas smit <bas@baslab.org>
2020-09-11 23:15:31 +0200

    Integer dereference is always `uint64`

--- src/ast/semantic_analyser.cpp
+++ src/ast/semantic_analyser.cpp
@@ -1450,7 +1450,7 @@ void SemanticAnalyser::visit(Unop &unop)
     }
     else if (type.IsIntTy())
     {
-      unop.type = CreateInteger(8 * type.size, type.IsSigned());
+      unop.type = CreateUInt64();
     }
   }
   else if (unop.op == Parser::token::LNOT) {
