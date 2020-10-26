2aa294b12568978eca1251100e149e830b23c097
Jeffle Xu <jefflexu@linux.alibaba.com>
2020-10-26 22:19:11 +0800

    tests: Add architecture specific testcase support
    commit 48d405133160 ("bpftrace: Add architecture specific testcase
    support") added architecture specific support for runtime tests.
    Still there are architecture specific testcases in semantic_analyser,
    where 'ip' register is hard coded in semantic_analyser.call_reg and
    semantic_analyser.builtin_functions.
    
    Besides registers such as 'ip' and 'bp' are also hard codes in some
    codegen tests.
    
    These testcases are all c/c++ files and thus we need conditional compiling.
    
    Signed-off-by: Jeffle Xu <jefflexu@linux.alibaba.com>

--- tests/codegen/intptrcast_assign_var.cpp
+++ tests/codegen/intptrcast_assign_var.cpp
@@ -4,10 +4,12 @@ namespace bpftrace {
 namespace test {
 namespace codegen {
 
+#ifdef ARCH_X86_64
 TEST(codegen, intptrcast_assign_var)
 {
   test("kretprobe:f { @=*(int8*)(reg(\"bp\")-1) }", NAME);
 }
+#endif
 
 } // namespace codegen
 } // namespace test
--- tests/codegen/intptrcast_call.cpp
+++ tests/codegen/intptrcast_call.cpp
@@ -4,11 +4,13 @@ namespace bpftrace {
 namespace test {
 namespace codegen {
 
+#ifdef ARCH_X86_64
 TEST(codegen, intptrcast_call)
 {
   // Casting should work inside a call
   test("kretprobe:f { @=sum(*(int8*)(reg(\"bp\")-1)) }", NAME);
 }
+#endif
 
 } // namespace codegen
 } // namespace test
