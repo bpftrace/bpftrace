97db09994e3768e01297b2500918a1237bc5f93b
Daniel Xu <dxu@dxuuu.xyz>
2020-11-05 10:33:29 -0800

    runtime-tests: Print bpftrace --info before running

--- tests/runtime-tests.sh
+++ tests/runtime-tests.sh
@@ -12,4 +12,9 @@ pushd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
 BPFTRACE_RUNTIME_TEST_EXECUTABLE=${BPFTRACE_RUNTIME_TEST_EXECUTABLE:-../src/};
 export BPFTRACE_RUNTIME_TEST_EXECUTABLE;
 
+echo "===================="
+echo "bpftrace --info:"
+echo "===================="
+"${BPFTRACE_RUNTIME_TEST_EXECUTABLE}/bpftrace" --info
+
 python3 runtime/engine/main.py $@
