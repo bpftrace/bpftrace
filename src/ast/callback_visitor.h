d9ec01cda9f73aad126bcbfff7e0212bf20c0d42
Masanori Misono <m.misono760@gmail.com>
2020-11-09 08:27:48 +0900

    Add CallbackVisitor
    This visitor calls a callback function on each node.

---
+++ src/ast/callback_visitor.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "ast.h"
+#include <functional>
+
+namespace bpftrace {
+namespace ast {
+
+using callback = std::function<void(Node *)>;
+
+class CallbackVisitor : public Visitor
+{
+public:
+  explicit CallbackVisitor(callback func) : func_(func)
+  {
+  }
+
+  void visit(Integer &integer) override;
+  void visit(PositionalParameter &param) override;
+  void visit(String &string) override;
+  void visit(StackMode &mode) override;
+  void visit(Identifier &identifier) override;
+  void visit(Builtin &builtin) override;
+  void visit(Call &call) override;
+  void visit(Map &map) override;
+  void visit(Variable &var) override;
+  void visit(Binop &binop) override;
+  void visit(Unop &unop) override;
+  void visit(Ternary &ternary) override;
+  void visit(FieldAccess &acc) override;
+  void visit(ArrayAccess &arr) override;
+  void visit(Cast &cast) override;
+  void visit(Tuple &tuple) override;
+  void visit(ExprStatement &expr) override;
+  void visit(AssignMapStatement &assignment) override;
+  void visit(AssignVarStatement &assignment) override;
+  void visit(If &if_block) override;
+  void visit(Unroll &unroll) override;
+  void visit(While &while_block) override;
+  void visit(Jump &jump) override;
+  void visit(Predicate &pred) override;
+  void visit(AttachPoint &ap) override;
+  void visit(Probe &probe) override;
+  void visit(Program &program) override;
+
+private:
+  callback func_;
+};
+
+} // namespace ast
+} // namespace bpftrace
