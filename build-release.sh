3aa61a18e0207ec59d6c360301bab1194fe51203
Feng Sun <loyou85@gmail.com>
2021-02-04 17:54:57 +0800

    Add ALLOW_UNSAFE_PROBE option for docker build
    Signed-off-by: Feng Sun <loyou85@gmail.com>

--- build-release.sh
+++ build-release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-docker run --network host --rm -it -u $(id -u):$(id -g) -v $(pwd):$(pwd) -e STATIC_LINKING=ON -e STATIC_LIBC=ON -e RUN_TESTS=0 bpftrace-builder-alpine "$(pwd)/build-release" Release "$@"
+docker run --network host --rm -it -u $(id -u):$(id -g) -v $(pwd):$(pwd) -e STATIC_LINKING=ON -e STATIC_LIBC=ON -e ALLOW_UNSAFE_PROBE=OFF -e RUN_TESTS=0 bpftrace-builder-alpine "$(pwd)/build-release" Release "$@"
