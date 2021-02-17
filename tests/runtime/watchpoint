bpftrace/bpftrace

commit 290dadeabc63d43ee08bcc89ab9cbd404a4768fc

watchpoint: Permit specifying function+function_arg XOR absolute address
Memory watchpoint will support either specifying an absolute address or
a function and the associated function argument as the address. The user
may only specify one or the other.

Note we only support x86 and arm for now. It should be trivial to add
other archs later (just implement `arch::invalid_watchpoint_modes()`).

src/arch/arch.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace bpftrace {
 namespace arch {
@@ -13,6 +14,8 @@ int pc_offset();
 int sp_offset();
 int arg_stack_offset();
 std::string name();
+// Each string is lexicographically sorted by character
+std::vector<std::string> invalid_watchpoint_modes();
 
 } // namespace arch
 } // namespace bpftrace

src/arch/aarch64.cpp
@@ -147,5 +147,15 @@ std::string name()
   return std::string("aarch64");
 }
 
+std::vector<std::string> invalid_watchpoint_modes()
+{
+  // See arch/arm/kernel/hw_breakpoint.c:arch_build_bp_info in kernel source
+  return std::vector<std::string>{
+    "rx",
+    "wx",
+    "rwx",
+  };
+}
+
 } // namespace arch
 } // namespace bpftrace

src/arch/ppc64.cpp
@@ -127,5 +127,11 @@ std::string name()
 #endif // __BYTE_ORDER__
 }
 
+std::vector<std::string> invalid_watchpoint_modes()
+{
+  throw std::runtime_error(
+      "Watchpoints are not supported on this architecture");
+}
+
 } // namespace arch
 } // namespace bpftrace

src/arch/s390.cpp
@@ -92,5 +92,11 @@ std::string name()
   return std::string("s390x");
 }
 
+std::vector<std::string> invalid_watchpoint_modes()
+{
+  throw std::runtime_error(
+      "Watchpoints are not supported on this architecture");
+}
+
 } // namespace arch
 } // namespace bpftrace

src/arch/x86_64.cpp
@@ -87,5 +87,16 @@ std::string name()
   return std::string("x86_64");
 }
 
+std::vector<std::string> invalid_watchpoint_modes()
+{
+  // See intel developer manual, Volume 3, section 17.2.4
+  return std::vector<std::string>{
+    "r",
+    "rx",
+    "wx",
+    "rwx",
+  };
+}
+
 } // namespace arch
 } // namespace bpftrace

src/ast/semantic_analyser.cpp
@@ -2394,7 +2394,16 @@ void SemanticAnalyser::visit(AttachPoint &ap)
       LOG(ERROR, ap.loc, err_) << "software count should be a positive integer";
   }
   else if (ap.provider == "watchpoint") {
-    if (!ap.address)
+    if (ap.func.size())
+    {
+      if (bpftrace_.pid() <= 0 && !has_child_)
+        LOG(ERROR, ap.loc, err_) << "-p PID or -c CMD required for watchpoint";
+
+      if (ap.address > static_cast<uint64_t>(arch::max_arg()))
+        LOG(ERROR, ap.loc, err_)
+            << arch::name() << " doesn't support arg" << ap.address;
+    }
+    else if (!ap.address)
       LOG(ERROR, ap.loc, err_)
           << "watchpoint must be attached to a non-zero address";
     if (ap.len != 1 && ap.len != 2 && ap.len != 4 && ap.len != 8)
@@ -2413,8 +2422,11 @@ void SemanticAnalyser::visit(AttachPoint &ap)
       if (ap.mode[i - 1] == ap.mode[i])
         LOG(ERROR, ap.loc, err_) << "watchpoint modes may not be duplicated";
     }
-    if (ap.mode == "rx" || ap.mode == "wx" || ap.mode == "rwx")
-      LOG(ERROR, ap.loc, err_) << "watchpoint modes (rx, wx, rwx) not allowed";
+    const auto invalid_modes = arch::invalid_watchpoint_modes();
+    if (std::any_of(invalid_modes.cbegin(),
+                    invalid_modes.cend(),
+                    [&](const auto &mode) { return mode == ap.mode; }))
+      LOG(ERROR, ap.loc, err_) << "invalid watchpoint mode: " << ap.mode;
   }
   else if (ap.provider == "hardware") {
     if (ap.target == "")

tests/mocks.h
@@ -1,9 +1,11 @@
 #pragma once
 
+#include "gmock/gmock.h"
+
 #include "bpffeature.h"
 #include "bpftrace.h"
 #include "child.h"
-#include "gmock/gmock.h"
+#include "procmon.h"
 
 namespace bpftrace {
 namespace test {
@@ -95,6 +97,25 @@ public:
   {
     (void)pause;
   };
+};
+
+class MockProcMon : public ProcMonBase
+{
+public:
+  MockProcMon(pid_t pid)
+  {
+    pid_ = pid;
+  }
+
+  ~MockProcMon() override = default;
+
+  bool is_alive(void) override
+  {
+    if (pid_ > 0)
+      return true;
+    else
+      return false;
+  }
 };
 
 } // namespace test

tests/semantic_analyser.cpp
@@ -1228,16 +1228,49 @@ TEST(semantic_analyser, tracepoint)
   test("tracepoint:category:event { 1 }", 0);
 }
 
-TEST(semantic_analyser, watchpoint)
+#if defined(ARCH_X86_64) || defined(ARCH_AARCH64)
+TEST(semantic_analyser, watchpoint_invalid_modes)
 {
-  test("watchpoint:0x1234:8:rw { 1 }", 0);
-  test("watchpoint:0x1234:9:rw { 1 }", 1);
-  test("watchpoint:0x1234:8:rwx { 1 }", 1);
-  test("watchpoint:0x1234:8:rx { 1 }", 1);
-  test("watchpoint:0x1234:8:b { 1 }", 1);
-  test("watchpoint:0x1234:8:rww { 1 }", 1);
-  test("watchpoint:0x0:8:rww { 1 }", 1);
+  auto bpftrace = get_mock_bpftrace();
+  bpftrace->procmon_ = std::make_unique<MockProcMon>(123);
+
+#ifdef ARCH_X86_64
+  test(*bpftrace, "watchpoint:0x1234:8:r { 1 }", 1);
+#elif ARCH_AARCH64
+  test(*bpftrace, "watchpoint:0x1234:8:r { 1 }", 0);
+#endif
+  test(*bpftrace, "watchpoint:0x1234:8:rx { 1 }", 1);
+  test(*bpftrace, "watchpoint:0x1234:8:wx { 1 }", 1);
+  test(*bpftrace, "watchpoint:0x1234:8:xw { 1 }", 1);
+  test(*bpftrace, "watchpoint:0x1234:8:rwx { 1 }", 1);
+  test(*bpftrace, "watchpoint:0x1234:8:xx { 1 }", 1);
+  test(*bpftrace, "watchpoint:0x1234:8:b { 1 }", 1);
 }
+
+TEST(semantic_analyser, watchpoint_absolute)
+{
+  auto bpftrace = get_mock_bpftrace();
+  bpftrace->procmon_ = std::make_unique<MockProcMon>(123);
+
+  test(*bpftrace, "watchpoint:0x1234:8:rw { 1 }", 0);
+  test(*bpftrace, "watchpoint:0x1234:9:rw { 1 }", 1);
+  test(*bpftrace, "watchpoint:0x0:8:rw { 1 }", 1);
+}
+
+TEST(semantic_analyser, watchpoint_function)
+{
+  auto bpftrace = get_mock_bpftrace();
+  bpftrace->procmon_ = std::make_unique<MockProcMon>(123);
+
+  test(*bpftrace, "watchpoint:func1+arg2:8:rw { 1 }", 0);
+  test(*bpftrace, "w:func1+arg2:8:rw { 1 }", 0);
+  test(*bpftrace, "w:func1.one_two+arg2:8:rw { 1 }", 0);
+  test(*bpftrace, "watchpoint:func1+arg99999:8:rw { 1 }", 1);
+
+  bpftrace->procmon_ = std::make_unique<MockProcMon>(0);
+  test(*bpftrace, "watchpoint:func1+arg2:8:rw { 1 }", 1);
+}
+#endif // if defined(ARCH_X86_64) || defined(ARCH_AARCH64)
 
 TEST(semantic_analyser, args_builtin_wrong_use)
 {

commit 5e43e2f4ed78229ebfd909234501badd930533e4

bpftrace: Fix build error
Add required header to avoid build errors.

Fixes: 290dadea ("watchpoint: Permit specifying function+function_arg
XOR absolute address")

Signed-off-by: Sumanth Korikkar <sumanthk@linux.ibm.com>

src/arch/arch.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stdexcept>
 #include <string>
 #include <vector>
 

tests/runtime/watchpoint
@@ -14,12 +14,14 @@ REQUIRES awk '$3 == "jiffies" {got=1} END {exit !got}' /proc/kallsyms
 NAME function_arg_addr
 RUN bpftrace -v -e 'watchpoint:increment+arg1:4:w { printf("hit!\n"); exit() }' -c ./testprogs/watchpoint_func
 EXPECT hit!
+ARCH aarch64|x86_64
 TIMEOUT 5
 REQUIRES_FEATURE signal
 
 NAME async_function_arg_addr
 RUN bpftrace -v -e 'asyncwatchpoint:increment+arg1:4:w { printf("hit!\n"); exit() }' -c ./testprogs/watchpoint_func
 EXPECT hit!
+ARCH aarch64|x86_64
 TIMEOUT 5
 REQUIRES_FEATURE signal
 
@@ -27,35 +29,41 @@ NAME function_arg_addr_process_flag
 RUN bpftrace -v -e 'watchpoint:increment+arg1:4:w { printf("hit!\n"); exit() }' -p $(pidof watchpoint_func)
 BEFORE ./testprogs/watchpoint_func
 EXPECT hit!
+ARCH aarch64|x86_64
 TIMEOUT 5
 REQUIRES_FEATURE signal
 
 NAME many_function_probes
 RUN bpftrace -e 'watchpoint:increment+arg0:4:w { printf("hit!\n") }' -c ./testprogs/watchpoint_func_many_probes
 EXPECT You are out of watchpoint registers
+ARCH aarch64|x86_64
 TIMEOUT 5
 REQUIRES_FEATURE signal
 
 NAME unwatch
 RUN bpftrace runtime/scripts/watchpoint_unwatch.bt -c ./testprogs/watchpoint_unwatch
 EXPECT count=1
+ARCH aarch64|x86_64
 TIMEOUT 5
 REQUIRES_FEATURE signal
 
 NAME function_multiattach
 RUN bpftrace -v runtime/scripts/watchpoint_multiattach.bt -c ./testprogs/watchpoint_func_wildcard
 EXPECT count=3
+ARCH aarch64|x86_64
 TIMEOUT 5
 REQUIRES_FEATURE signal
 
 NAME wildcarded_function
 RUN bpftrace -v -e 'watchpoint:increment_*+arg0:4:w { printf("hit!\n") }' -c ./testprogs/watchpoint_func_wildcard
 EXPECT You are out of watchpoint registers
+ARCH aarch64|x86_64
 TIMEOUT 5
 REQUIRES_FEATURE signal
 
 NAME unique_probe_bodies
 RUN bpftrace -v -e 'watchpoint:increment_*+arg0:4:w { printf("%s!\n", probe) }' -c ./testprogs/watchpoint_func_wildcard
 EXPECT .*increment_0:4:w!
+ARCH aarch64|x86_64
 TIMEOUT 5
 REQUIRES_FEATURE signal