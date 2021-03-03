985aae132403d461317f6eb2e5f5634ba1776b92
bas smit <bas@baslab.org>
2021-03-03 01:20:22 +0100

    llvm: abstract section lookup by moving to orc
    This way the orc can do everything it needs to get the section out (like
    JIT compile on lookup) instead of the user digging into the orc internal.

--- src/bpforc.h
+++ src/bpforc.h
@@ -26,31 +26,16 @@ public:
   uint8_t *allocateCodeSection(uintptr_t Size,
                                unsigned Alignment,
                                unsigned SectionID,
-                               StringRef SectionName) override
-  {
-    uint8_t *addr = SectionMemoryManager::allocateCodeSection(
-        Size, Alignment, SectionID, SectionName);
-    sections_[SectionName.str()] = std::make_tuple(addr, Size);
-    return addr;
-  }
-
+                               StringRef SectionName) override;
   uint8_t *allocateDataSection(uintptr_t Size,
                                unsigned Alignment,
                                unsigned SectionID,
                                StringRef SectionName,
-                               bool isReadOnly) override
-  {
-    uint8_t *addr = SectionMemoryManager::allocateDataSection(
-        Size, Alignment, SectionID, SectionName, isReadOnly);
-    sections_[SectionName.str()] = std::make_tuple(addr, Size);
-    return addr;
-  }
-
+                               bool isReadOnly) override;
 private:
   SectionMap &sections_;
 };
 
-#if LLVM_VERSION_MAJOR >= 7
 class BpfOrc
 {
 private:
@@ -68,104 +53,15 @@ private:
   IRCompileLayer<decltype(ObjectLayer), SimpleCompiler> CompileLayer;
 #endif
 
-public:
   SectionMap sections_;
-
-  BpfOrc(TargetMachine *TM)
-      : TM(TM),
-        DL(this->TM->createDataLayout()),
-        Resolver(createLegacyLookupResolver(
-            ES,
-#if LLVM_VERSION_MAJOR >= 11
-            [](llvm::StringRef Name __attribute__((unused))) -> JITSymbol {
-              return nullptr;
-            },
-#else
-            [](const std::string &Name __attribute__((unused))) -> JITSymbol {
-              return nullptr;
-            },
-#endif
-            [](Error Err) { cantFail(std::move(Err), "lookup failed"); })),
-#if LLVM_VERSION_MAJOR > 8
-        ObjectLayer(AcknowledgeORCv1Deprecation,
-                    ES,
-                    [this](VModuleKey) {
-                      return LegacyRTDyldObjectLinkingLayer::Resources{
-                        std::make_shared<MemoryManager>(sections_), Resolver
-                      };
-                    }),
-        CompileLayer(AcknowledgeORCv1Deprecation,
-                     ObjectLayer,
-                     SimpleCompiler(*this->TM))
-  {
-  }
-#elif LLVM_VERSION_MAJOR == 8
-        ObjectLayer(ES,
-                    [this](VModuleKey) {
-                      return LegacyRTDyldObjectLinkingLayer::Resources{
-                        std::make_shared<MemoryManager>(sections_), Resolver
-                      };
-                    }),
-        CompileLayer(ObjectLayer, SimpleCompiler(*this->TM))
-  {
-  }
-#else
-        ObjectLayer(ES,
-                    [this](VModuleKey) {
-                      return RTDyldObjectLinkingLayer::Resources{
-                        std::make_shared<MemoryManager>(sections_), Resolver
-                      };
-                    }),
-        CompileLayer(ObjectLayer, SimpleCompiler(*this->TM))
-  {
-  }
-#endif
-
-  void compileModule(std::unique_ptr<Module> M)
-  {
-    auto K = ES.allocateVModule();
-    cantFail(CompileLayer.addModule(K, std::move(M)));
-    cantFail(CompileLayer.emitAndFinalize(K));
-  }
-
-  const DataLayout &getDataLayout() const
-  {
-    return DL;
-  }
-
-  LLVMContext &getContext()
-  {
-    return CTX;
-  }
-
-  TargetMachine & getTargetMachine()
-  {
-    return *TM;
-  }
-
-  static std::unique_ptr<BpfOrc> Create()
-  {
-    std::string targetTriple = "bpf-pc-linux";
-
-    LLVMInitializeBPFTargetInfo();
-    LLVMInitializeBPFTarget();
-    LLVMInitializeBPFTargetMC();
-    LLVMInitializeBPFAsmPrinter();
-
-    std::string error;
-    const Target *target = TargetRegistry::lookupTarget(targetTriple, error);
-    if (!target)
-      throw std::runtime_error("Could not create LLVM target " + error);
-
-    TargetOptions opt;
-    auto RM = Reloc::Model();
-    auto TM = target->createTargetMachine(targetTriple, "generic", "", opt, RM);
-
-    return std::make_unique<BpfOrc>(TM);
-  }
+public:
+  BpfOrc(TargetMachine *TM);
+  void compile(std::unique_ptr<Module> M);
+  const DataLayout &getDataLayout() const;
+  LLVMContext &getContext();
+  TargetMachine & getTargetMachine();
+  static std::unique_ptr<BpfOrc> Create();
+  std::optional<std::tuple<uint8_t *, uintptr_t>> getSection(const std::string & name) const;
 };
-#else
-#error Unsupported LLVM version
-#endif
 
 } // namespace bpftrace
