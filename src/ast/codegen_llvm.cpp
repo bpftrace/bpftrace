684acb371ce4dca0910dc34a44635b0dd75beb7d
bas smit <bas@baslab.org>
2021-03-12 10:17:15 +0100

    Allow pointer unop increment/decrement
    This allows us to do `@ptr++` instead of `@ptr+=1`. To allow for this we
    need to slightly overhaul the logic as maps were always initialized to
    uint regardless of whether they were already set. As increment only used
    to work on ints that was ok, but now that it needs to support pointers
    it should only be done if there is no default value.

--- src/ast/codegen_llvm.cpp
+++ src/ast/codegen_llvm.cpp
@@ -1420,6 +1420,77 @@ static bool unop_skip_accept(Unop &unop)
   return false;
 }
 
+void CodegenLLVM::unop_int(Unop &unop)
+{
+  SizedType &type = unop.expr->type;
+  switch (unop.op)
+  {
+    case bpftrace::Parser::token::LNOT: {
+      auto ty = expr_->getType();
+      Value *zero_value = Constant::getNullValue(ty);
+      expr_ = b_.CreateICmpEQ(expr_, zero_value);
+      // CreateICmpEQ() returns 1-bit integer
+      // Cast it to the same type of the operand
+      // Use unsigned extention, otherwise !0 becomes -1
+      expr_ = b_.CreateIntCast(expr_, ty, false);
+      break;
+    }
+    case bpftrace::Parser::token::BNOT:
+      expr_ = b_.CreateNot(expr_);
+      break;
+    case bpftrace::Parser::token::MINUS:
+      expr_ = b_.CreateNeg(expr_);
+      break;
+    case bpftrace::Parser::token::INCREMENT:
+    case bpftrace::Parser::token::DECREMENT: {
+      createIncDec(unop);
+      break;
+    }
+    case bpftrace::Parser::token::MUL: {
+      // When dereferencing a 32-bit integer, only read in 32-bits, etc.
+      int size = type.GetSize();
+      auto as = type.GetAS();
+
+      AllocaInst *dst = b_.CreateAllocaBPF(SizedType(type.type, size), "deref");
+      b_.CreateProbeRead(ctx_, dst, size, expr_, as, unop.loc);
+      expr_ = b_.CreateIntCast(b_.CreateLoad(dst),
+                               b_.getInt64Ty(),
+                               type.IsSigned());
+      b_.CreateLifetimeEnd(dst);
+      break;
+    }
+  }
+}
+
+void CodegenLLVM::unop_ptr(Unop &unop)
+{
+  SizedType &type = unop.expr->type;
+  switch (unop.op)
+  {
+    case bpftrace::Parser::token::MUL: {
+      if (unop.type.IsIntegerTy() || unop.type.IsPtrTy())
+      {
+        auto *et = type.GetPointeeTy();
+        // Pointer always 64 bits wide
+        int size = unop.type.IsIntegerTy() ? et->GetIntBitWidth() / 8 : 8;
+        AllocaInst *dst = b_.CreateAllocaBPF(*et, "deref");
+        b_.CreateProbeRead(ctx_, dst, size, expr_, type.GetAS(), unop.loc);
+        expr_ = b_.CreateIntCast(b_.CreateLoad(dst),
+                                 b_.getInt64Ty(),
+                                 unop.type.IsSigned());
+        b_.CreateLifetimeEnd(dst);
+      }
+      break;
+    }
+    case bpftrace::Parser::token::INCREMENT:
+    case bpftrace::Parser::token::DECREMENT: {
+      createIncDec(unop);
+      break;
+    }
+    default:; // Do nothing
+  }
+}
+
 void CodegenLLVM::visit(Unop &unop)
 {
   auto scoped_del = ScopedExprDeleter(nullptr);
@@ -1429,105 +1500,11 @@ void CodegenLLVM::visit(Unop &unop)
   SizedType &type = unop.expr->type;
   if (type.IsIntegerTy())
   {
-    switch (unop.op)
-    {
-      case bpftrace::Parser::token::LNOT:
-      {
-        auto ty = expr_->getType();
-        Value *zero_value = Constant::getNullValue(ty);
-        expr_ = b_.CreateICmpEQ(expr_, zero_value);
-        // CreateICmpEQ() returns 1-bit integer
-        // Cast it to the same type of the operand
-        // Use unsigned extention, otherwise !0 becomes -1
-        expr_ = b_.CreateIntCast(expr_, ty, false);
-        break;
-      }
-      case bpftrace::Parser::token::BNOT: expr_ = b_.CreateNot(expr_); break;
-      case bpftrace::Parser::token::MINUS: expr_ = b_.CreateNeg(expr_); break;
-      case bpftrace::Parser::token::INCREMENT:
-      case bpftrace::Parser::token::DECREMENT:
-      {
-        bool is_increment = unop.op == bpftrace::Parser::token::INCREMENT;
-
-        if (unop.expr->is_map)
-        {
-          Map &map = static_cast<Map&>(*unop.expr);
-          auto [key, scoped_key_deleter] = getMapKey(map);
-          Value *oldval = b_.CreateMapLookupElem(ctx_, map, key, unop.loc);
-          AllocaInst *newval = b_.CreateAllocaBPF(map.type, map.ident + "_newval");
-          if (is_increment)
-            b_.CreateStore(b_.CreateAdd(oldval, b_.getInt64(1)), newval);
-          else
-            b_.CreateStore(b_.CreateSub(oldval, b_.getInt64(1)), newval);
-          b_.CreateMapUpdateElem(ctx_, map, key, newval, unop.loc);
-
-          if (unop.is_post_op)
-            expr_ = oldval;
-          else
-            expr_ = b_.CreateLoad(newval);
-          b_.CreateLifetimeEnd(newval);
-        }
-        else if (unop.expr->is_variable)
-        {
-          Variable &var = static_cast<Variable&>(*unop.expr);
-          Value *oldval = b_.CreateLoad(variables_[var.ident]);
-          Value *newval;
-          if (is_increment)
-            newval = b_.CreateAdd(oldval, b_.getInt64(1));
-          else
-            newval = b_.CreateSub(oldval, b_.getInt64(1));
-          b_.CreateStore(newval, variables_[var.ident]);
-
-          if (unop.is_post_op)
-            expr_ = oldval;
-          else
-            expr_ = newval;
-        }
-        else
-        {
-          LOG(FATAL) << "invalid expression passed to " << opstr(unop);
-        }
-        break;
-      }
-      case bpftrace::Parser::token::MUL:
-      {
-        // When dereferencing a 32-bit integer, only read in 32-bits, etc.
-        int size = type.IsPtrTy() ? type.GetPointeeTy()->GetSize()
-                                  : type.GetSize();
-        auto as =  type.GetAS();
-
-        AllocaInst *dst = b_.CreateAllocaBPF(SizedType(type.type, size), "deref");
-        b_.CreateProbeRead(ctx_, dst, size, expr_, as, unop.loc);
-        expr_ = b_.CreateIntCast(b_.CreateLoad(dst),
-                                 b_.getInt64Ty(),
-                                 type.IsSigned());
-        b_.CreateLifetimeEnd(dst);
-        break;
-      }
-    }
+    unop_int(unop);
   }
   else if (type.IsPtrTy())
   {
-    switch (unop.op)
-    {
-      case bpftrace::Parser::token::MUL:
-      {
-        if (unop.type.IsIntegerTy() || unop.type.IsPtrTy())
-        {
-          auto *et = type.GetPointeeTy();
-          // Pointer always 64 bits wide
-          int size = unop.type.IsIntegerTy() ? et->GetIntBitWidth() / 8 : 8;
-          AllocaInst *dst = b_.CreateAllocaBPF(*et, "deref");
-          b_.CreateProbeRead(ctx_, dst, size, expr_, type.GetAS(), unop.loc);
-          expr_ = b_.CreateIntCast(b_.CreateLoad(dst),
-                                   b_.getInt64Ty(),
-                                   unop.type.IsSigned());
-          b_.CreateLifetimeEnd(dst);
-        }
-        break;
-      }
-      default:; // Do nothing
-    }
+    unop_ptr(unop);
   }
   else
   {
@@ -3222,6 +3199,54 @@ void CodegenLLVM::probereadDatastructElem(Value *src_data,
                                elem_type.IsSigned());
       b_.CreateLifetimeEnd(dst);
     }
+  }
+}
+
+void CodegenLLVM::createIncDec(Unop &unop)
+{
+  bool is_increment = unop.op == bpftrace::Parser::token::INCREMENT;
+  SizedType &type = unop.expr->type;
+  uint64_t step = type.IsPtrTy() ? type.GetPointeeTy()->GetSize() : 1;
+
+  if (unop.expr->is_map)
+  {
+    Map &map = static_cast<Map &>(*unop.expr);
+    auto [key, scoped_key_deleter] = getMapKey(map);
+    Value *oldval = b_.CreateMapLookupElem(ctx_, map, key, unop.loc);
+    AllocaInst *newval = b_.CreateAllocaBPF(map.type, map.ident + "_newval");
+    if (is_increment)
+      b_.CreateStore(b_.CreateAdd(oldval, b_.GetIntSameSize(step, oldval)),
+                     newval);
+    else
+      b_.CreateStore(b_.CreateSub(oldval, b_.GetIntSameSize(step, oldval)),
+                     newval);
+    b_.CreateMapUpdateElem(ctx_, map, key, newval, unop.loc);
+
+    if (unop.is_post_op)
+      expr_ = oldval;
+    else
+      expr_ = b_.CreateLoad(newval);
+    b_.CreateLifetimeEnd(newval);
+  }
+  else if (unop.expr->is_variable)
+  {
+    Variable &var = static_cast<Variable &>(*unop.expr);
+    Value *oldval = b_.CreateLoad(variables_[var.ident]);
+    Value *newval;
+    if (is_increment)
+      newval = b_.CreateAdd(oldval, b_.GetIntSameSize(step, oldval));
+    else
+      newval = b_.CreateSub(oldval, b_.GetIntSameSize(step, oldval));
+    b_.CreateStore(newval, variables_[var.ident]);
+
+    if (unop.is_post_op)
+      expr_ = oldval;
+    else
+      expr_ = newval;
+  }
+  else
+  {
+    LOG(FATAL) << "invalid expression passed to " << opstr(unop);
   }
 }
 
