d8dda6eb7a480004196b0ca104e6875b4fd9f381
Daniel Xu <dxu@dxuuu.xyz>
2021-06-07 18:00:23 -0700

    tests: Fix spew about untraceable functions on custom mocks too

--- tests/codegen/call_kstack.cpp
+++ tests/codegen/call_kstack.cpp
@@ -19,64 +19,64 @@ TEST(codegen, call_kstack)
 
 TEST(codegen, call_kstack_mapids)
 {
-  BPFtrace bpftrace;
-  Driver driver(bpftrace);
+  auto bpftrace = get_mock_bpftrace();
+  Driver driver(*bpftrace);
 
   ASSERT_EQ(driver.parse_str(
                 "kprobe:f { @x = kstack(5); @y = kstack(6); @z = kstack(6) }"),
             0);
 
   ClangParser clang;
-  clang.parse(driver.root_, bpftrace);
+  clang.parse(driver.root_, *bpftrace);
 
   // Override to mockbpffeature.
-  bpftrace.feature_ = std::make_unique<MockBPFfeature>(true);
-  ast::SemanticAnalyser semantics(driver.root_, bpftrace);
+  bpftrace->feature_ = std::make_unique<MockBPFfeature>(true);
+  ast::SemanticAnalyser semantics(driver.root_, *bpftrace);
   ASSERT_EQ(semantics.analyse(), 0);
   ASSERT_EQ(semantics.create_maps(true), 0);
 
-  ast::CodegenLLVM codegen(driver.root_, bpftrace);
+  ast::CodegenLLVM codegen(driver.root_, *bpftrace);
   codegen.compile();
 
-  ASSERT_EQ(std::distance(bpftrace.maps.begin(), bpftrace.maps.end()), 6);
-  ASSERT_EQ(bpftrace.maps.CountStackTypes(), 2U);
+  ASSERT_EQ(std::distance(bpftrace->maps.begin(), bpftrace->maps.end()), 6);
+  ASSERT_EQ(bpftrace->maps.CountStackTypes(), 2U);
 
   StackType stack_type;
   stack_type.limit = 5;
-  ASSERT_TRUE(bpftrace.maps.Has(stack_type));
+  ASSERT_TRUE(bpftrace->maps.Has(stack_type));
   stack_type.limit = 6;
-  ASSERT_TRUE(bpftrace.maps.Has(stack_type));
+  ASSERT_TRUE(bpftrace->maps.Has(stack_type));
 }
 
 TEST(codegen, call_kstack_modes_mapids)
 {
-  BPFtrace bpftrace;
-  Driver driver(bpftrace);
+  auto bpftrace = get_mock_bpftrace();
+  Driver driver(*bpftrace);
 
   ASSERT_EQ(driver.parse_str("kprobe:f { @x = kstack(perf); @y = "
                              "kstack(bpftrace); @z = kstack() }"),
             0);
 
   ClangParser clang;
-  clang.parse(driver.root_, bpftrace);
+  clang.parse(driver.root_, *bpftrace);
 
   // Override to mockbpffeature.
-  bpftrace.feature_ = std::make_unique<MockBPFfeature>(true);
-  ast::SemanticAnalyser semantics(driver.root_, bpftrace);
+  bpftrace->feature_ = std::make_unique<MockBPFfeature>(true);
+  ast::SemanticAnalyser semantics(driver.root_, *bpftrace);
   ASSERT_EQ(semantics.analyse(), 0);
   ASSERT_EQ(semantics.create_maps(true), 0);
 
-  ast::CodegenLLVM codegen(driver.root_, bpftrace);
+  ast::CodegenLLVM codegen(driver.root_, *bpftrace);
   codegen.compile();
 
-  ASSERT_EQ(std::distance(bpftrace.maps.begin(), bpftrace.maps.end()), 6);
-  ASSERT_EQ(bpftrace.maps.CountStackTypes(), 2U);
+  ASSERT_EQ(std::distance(bpftrace->maps.begin(), bpftrace->maps.end()), 6);
+  ASSERT_EQ(bpftrace->maps.CountStackTypes(), 2U);
 
   StackType stack_type;
   stack_type.mode = StackMode::perf;
-  ASSERT_TRUE(bpftrace.maps.Has(stack_type));
+  ASSERT_TRUE(bpftrace->maps.Has(stack_type));
   stack_type.mode = StackMode::bpftrace;
-  ASSERT_TRUE(bpftrace.maps.Has(stack_type));
+  ASSERT_TRUE(bpftrace->maps.Has(stack_type));
 }
 
 } // namespace codegen
--- tests/codegen/call_ustack.cpp
+++ tests/codegen/call_ustack.cpp
@@ -19,64 +19,64 @@ TEST(codegen, call_ustack)
 
 TEST(codegen, call_ustack_mapids)
 {
-  BPFtrace bpftrace;
-  Driver driver(bpftrace);
+  auto bpftrace = get_mock_bpftrace();
+  Driver driver(*bpftrace);
 
   ASSERT_EQ(driver.parse_str(
                 "kprobe:f { @x = ustack(5); @y = ustack(6); @z = ustack(6) }"),
             0);
 
   ClangParser clang;
-  clang.parse(driver.root_, bpftrace);
+  clang.parse(driver.root_, *bpftrace);
 
   // Override to mockbpffeature.
-  bpftrace.feature_ = std::make_unique<MockBPFfeature>(true);
-  ast::SemanticAnalyser semantics(driver.root_, bpftrace);
+  bpftrace->feature_ = std::make_unique<MockBPFfeature>(true);
+  ast::SemanticAnalyser semantics(driver.root_, *bpftrace);
   ASSERT_EQ(semantics.analyse(), 0);
   ASSERT_EQ(semantics.create_maps(true), 0);
 
-  ast::CodegenLLVM codegen(driver.root_, bpftrace);
+  ast::CodegenLLVM codegen(driver.root_, *bpftrace);
   codegen.compile();
 
-  ASSERT_EQ(std::distance(bpftrace.maps.begin(), bpftrace.maps.end()), 6);
-  ASSERT_EQ(bpftrace.maps.CountStackTypes(), 2U);
+  ASSERT_EQ(std::distance(bpftrace->maps.begin(), bpftrace->maps.end()), 6);
+  ASSERT_EQ(bpftrace->maps.CountStackTypes(), 2U);
 
   StackType stack_type;
   stack_type.limit = 5;
-  ASSERT_TRUE(bpftrace.maps.Has(stack_type));
+  ASSERT_TRUE(bpftrace->maps.Has(stack_type));
   stack_type.limit = 6;
-  ASSERT_TRUE(bpftrace.maps.Has(stack_type));
+  ASSERT_TRUE(bpftrace->maps.Has(stack_type));
 }
 
 TEST(codegen, call_ustack_modes_mapids)
 {
-  BPFtrace bpftrace;
-  Driver driver(bpftrace);
+  auto bpftrace = get_mock_bpftrace();
+  Driver driver(*bpftrace);
 
   ASSERT_EQ(driver.parse_str("kprobe:f { @x = ustack(perf); @y = "
                              "ustack(bpftrace); @z = ustack() }"),
             0);
 
   ClangParser clang;
-  clang.parse(driver.root_, bpftrace);
+  clang.parse(driver.root_, *bpftrace);
 
   // Override to mockbpffeature.
-  bpftrace.feature_ = std::make_unique<MockBPFfeature>(true);
-  ast::SemanticAnalyser semantics(driver.root_, bpftrace);
+  bpftrace->feature_ = std::make_unique<MockBPFfeature>(true);
+  ast::SemanticAnalyser semantics(driver.root_, *bpftrace);
   ASSERT_EQ(semantics.analyse(), 0);
   ASSERT_EQ(semantics.create_maps(true), 0);
 
-  ast::CodegenLLVM codegen(driver.root_, bpftrace);
+  ast::CodegenLLVM codegen(driver.root_, *bpftrace);
   codegen.compile();
 
-  ASSERT_EQ(std::distance(bpftrace.maps.begin(), bpftrace.maps.end()), 6);
-  ASSERT_EQ(bpftrace.maps.CountStackTypes(), 2U);
+  ASSERT_EQ(std::distance(bpftrace->maps.begin(), bpftrace->maps.end()), 6);
+  ASSERT_EQ(bpftrace->maps.CountStackTypes(), 2U);
 
   StackType stack_type;
   stack_type.mode = StackMode::perf;
-  ASSERT_TRUE(bpftrace.maps.Has(stack_type));
+  ASSERT_TRUE(bpftrace->maps.Has(stack_type));
   stack_type.mode = StackMode::bpftrace;
-  ASSERT_TRUE(bpftrace.maps.Has(stack_type));
+  ASSERT_TRUE(bpftrace->maps.Has(stack_type));
 }
 
 } // namespace codegen
