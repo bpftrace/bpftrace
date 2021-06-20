bpftrace/bpftrace

commit 75b33a1a57c17805b3d773ac4e8378d08c79a043

Avoid wrong downcast in str and buf
UBSan  reports the following error when running bpftrace_test.

```
/work/bpftrace2/src/ast/semantic_analyser.cpp:662:22: runtime error: downcast of address 0x7fffe80cced0 which does not point to an object of type 'bpftrace::ast::Integer'
0x7fffe80cced0: note: object is of type 'bpftrace::ast::String'
 00 00 00 00  a8 fa 4b 01 00 00 00 00  00 00 00 00 00 00 00 00  01 00 00 00 16 00 00 00  00 00 00 00
              ^~~~~~~~~~~~~~~~~~~~~~~
              vptr for 'bpftrace::ast::String'
```

the problem test is

```
test("kprobe:f { str(arg0, \"hello\"); }", 10)
```

and the program tries to downcast "hello" to ast::Integer.

To fix this, do static cast only in the final pss. In the
final pass we know it's a integer literal by check_arg().

src/ast/semantic_analyser.cpp
@@ -627,22 +627,22 @@ void SemanticAnalyser::visit(Call &call)
         }
       }
 
-      if (call.vargs->size() == 2)
+      if (is_final_pass() && call.vargs->size() == 2 &&
+          check_arg(call, Type::integer, 1, false))
       {
         auto &size_arg = *call.vargs->at(1);
-        long value = static_cast<Integer &>(size_arg).n;
-        if (value < 0)
+        if (size_arg.is_literal)
         {
-          LOG(ERROR, call.loc, err_)
-              << call.func << "cannot use negative length (" << value << ")";
+          auto &integer = static_cast<Integer &>(size_arg);
+          long value = integer.n;
+          if (value < 0)
+            LOG(ERROR, call.loc, err_)
+                << call.func << "cannot use negative length (" << value << ")";
         }
       }
 
       // Required for cases like strncmp(str($1), str(2), 4))
       call.type.SetAS(t.GetAS());
-      if (is_final_pass() && call.vargs->size() > 1) {
-        check_arg(call, Type::integer, 1, false);
-      }
     }
     has_pos_param_ = false;
   }
@@ -664,29 +664,34 @@ void SemanticAnalyser::visit(Call &call)
     size_t max_buffer_size = bpftrace_.strlen_;
     size_t buffer_size = max_buffer_size;
 
-    if (call.vargs->size() == 1)
-      if (arg.type.IsArrayTy())
-        buffer_size = arg.type.GetNumElements() *
-                      arg.type.GetElementTy()->GetSize();
-      else
-        LOG(ERROR, call.loc, err_)
-            << call.func << "() expects a length argument for non-array type "
-            << typestr(arg.type.type);
-    else
+    if (is_final_pass())
     {
-      if (is_final_pass())
-        check_arg(call, Type::integer, 1, false);
-
-      auto &size_arg = *call.vargs->at(1);
-      if (size_arg.is_literal)
-      {
-        long value = static_cast<Integer &>(size_arg).n;
-        if (value < 0)
-        {
+      if (call.vargs->size() == 1)
+        if (arg.type.IsArrayTy())
+          buffer_size = arg.type.GetNumElements() *
+                        arg.type.GetElementTy()->GetSize();
+        else
           LOG(ERROR, call.loc, err_)
-              << call.func << "cannot use negative length (" << value << ")";
+              << call.func << "() expects a length argument for non-array type "
+              << typestr(arg.type.type);
+      else
+      {
+        if (check_arg(call, Type::integer, 1, false))
+        {
+          auto &size_arg = *call.vargs->at(1);
+          if (size_arg.is_literal)
+          {
+            auto &integer = static_cast<Integer &>(size_arg);
+            long value = integer.n;
+            if (value < 0)
+            {
+              LOG(ERROR, call.loc, err_)
+                  << call.func << " cannot use negative length (" << value
+                  << ")";
+            }
+            buffer_size = value;
+          }
         }
-        buffer_size = value;
       }
     }
 
@@ -2890,6 +2895,40 @@ bool SemanticAnalyser::check_available(const Call &call, const AttachPoint &ap)
   return true;
 }
 
+void SemanticAnalyser::update_assign_map_type(const Map &map,
+                                              SizedType &type,
+                                              const SizedType &new_type)
+{
+  const std::string &map_ident = map.ident;
+  if ((type.IsTupleTy() && new_type.IsTupleTy() &&
+       type.GetFields().size() != new_type.GetFields().size()) ||
+      (type.type != new_type.type) ||
+      (type.IsRecordTy() && type.GetName() != new_type.GetName()))
+  {
+    LOG(ERROR, map.loc, err_)
+        << "Type mismatch for " << map_ident << ": "
+        << "trying to assign value of type '" << new_type
+        << "' when map already contains a value of type '" << type;
+    return;
+  }
+
+  // all integers are 64bit
+  if (type.IsIntTy())
+    return;
+
+  if (type.IsTupleTy() && new_type.IsTupleTy())
+  {
+    auto &fields = type.GetFields();
+    auto &new_fields = new_type.GetFields();
+    for (size_t i = 0; i < fields.size(); i++)
+    {
+      update_assign_map_type(map, fields[i].type, new_fields[i].type);
+    }
+  }
+
+  type = new_type;
+}
+
 /*
  * assign_map_type
  *
@@ -2906,7 +2945,8 @@ void SemanticAnalyser::assign_map_type(const Map &map, const SizedType &type)
       if (is_final_pass()) {
         LOG(ERROR, map.loc, err_) << "Undefined map: " + map_ident;
       }
-      else {
+      else
+      {
         search->second = type;
       }
     }
@@ -2916,6 +2956,7 @@ void SemanticAnalyser::assign_map_type(const Map &map, const SizedType &type)
           << "trying to assign value of type '" << type
           << "' when map already contains a value of type '" << search->second;
     }
+    update_assign_map_type(map, search->second, type);
   }
   else {
     // This map hasn't been seen before

tests/semantic_analyser.cpp
@@ -581,7 +581,7 @@ TEST(semantic_analyser, call_str)
 TEST(semantic_analyser, call_str_2_lit)
 {
   test("kprobe:f { str(arg0, 3); }", 0);
-  test("kprobe:f { str(arg0, -3); }", 1);
+  test("kprobe:f { str(arg0, -3); }", 10);
   test("kprobe:f { @x = str(arg0, 3); }", 0);
   test("kprobe:f { str(arg0, \"hello\"); }", 10);
 }
@@ -604,11 +604,11 @@ TEST(semantic_analyser, call_str_state_leak_regression_test)
 TEST(semantic_analyser, call_buf)
 {
   test("kprobe:f { buf(arg0, 1); }", 0);
-  test("kprobe:f { buf(arg0, -1); }", 1);
+  test("kprobe:f { buf(arg0, -1); }", 10);
   test("kprobe:f { @x = buf(arg0, 1); }", 0);
   test("kprobe:f { $x = buf(arg0, 1); }", 0);
   test("kprobe:f { buf(); }", 1);
-  test("kprobe:f { buf(\"hello\"); }", 1);
+  test("kprobe:f { buf(\"hello\"); }", 10);
   test("struct x { int c[4] }; kprobe:f { $foo = (struct x*)0; @x = "
        "buf($foo->c); }",
        0);
@@ -1896,7 +1896,7 @@ TEST(semantic_analyser, tuple)
   test(R"_(BEGIN { $t = (1, 2); $t = 5; })_", 1);
   test(R"_(BEGIN { $t = (1, count()) })_", 1);
 
-  test(R"_(BEGIN { @t = (1, 2); @t = (4, "other"); })_", 10);
+  test(R"_(BEGIN { @t = (1, 2); @t = (4, "other"); })_", 1);
   test(R"_(BEGIN { @t = (1, 2); @t = 5; })_", 1);
   test(R"_(BEGIN { @t = (1, count()) })_", 1);
   test(R"_(BEGIN { @t = (1, (aaa)0) })_", 1);

src/ast/semantic_analyser.h
@@ -84,6 +84,9 @@ private:
   void check_stack_call(Call &call, bool kernel);
 
   void assign_map_type(const Map &map, const SizedType &type);
+  void update_assign_map_type(const Map &map,
+                              SizedType &type,
+                              const SizedType &new_type);
 
   void builtin_args_tracepoint(AttachPoint *attach_point, Builtin &builtin);
   ProbeType single_provider_type(void);

commit 735a19d1bfa76df22339466efd98f9121096fca1

semantics: Don't wait until final pass to process buf()
Given a `buf(@something, $len)`, it's problematic to wait until the
final pass to process `$len`. The reason is b/c if `$len` is a constant,
it affects the call's return type size. If the result of `buf(..)` is
assigned to a map, the type of the map changes in `map_val_`.

However, the type stored in `ast::Map` remains unchanged because
`ast::Map` retrieves its type from `map_val_` _before_ `buf()` is
processed. And because `$len` was being processed in the final pass, the
type stored in `ast::Map` is not updated.

This works fine when `SemanticAnalyser` is in charge of creating maps
as `map_val_` always remains up to date. But because in the next few
commits we split out that logic to ResourceAnalyser pass, we need the
final types stored in AST nodes to be accurate.

If the information is not accruate, the map sizes as created by
RequiredResources is not in sync with what codegen expects, causing
verifier errors.

Fixes: 75b33a1a ("Avoid wrong downcast in str and buf")

src/ast/semantic_analyser.cpp
@@ -692,8 +692,8 @@ void SemanticAnalyser::visit(Call &call)
       return;
 
     auto &arg = *call.vargs->at(0);
-    if (!(arg.type.IsIntTy() || arg.type.IsStringTy() || arg.type.IsPtrTy() ||
-          arg.type.IsArrayTy()))
+    if (is_final_pass() && !(arg.type.IsIntTy() || arg.type.IsStringTy() ||
+                             arg.type.IsPtrTy() || arg.type.IsArrayTy()))
     {
       LOG(ERROR, call.loc, err_)
           << call.func
@@ -704,33 +704,34 @@ void SemanticAnalyser::visit(Call &call)
     size_t max_buffer_size = bpftrace_.strlen_;
     size_t buffer_size = max_buffer_size;
 
-    if (is_final_pass())
+    if (call.vargs->size() == 1)
     {
-      if (call.vargs->size() == 1)
-        if (arg.type.IsArrayTy())
-          buffer_size = arg.type.GetNumElements() *
-                        arg.type.GetElementTy()->GetSize();
-        else
-          LOG(ERROR, call.loc, err_)
-              << call.func << "() expects a length argument for non-array type "
-              << typestr(arg.type.type);
-      else
+      if (arg.type.IsArrayTy())
+        buffer_size = arg.type.GetNumElements() *
+                      arg.type.GetElementTy()->GetSize();
+      else if (is_final_pass())
+        LOG(ERROR, call.loc, err_)
+            << call.func << "() expects a length argument for non-array type "
+            << typestr(arg.type.type);
+    }
+    else
+    {
+      if (is_final_pass())
+        check_arg(call, Type::integer, 1, false);
+
+      auto &size_arg = *call.vargs->at(1);
+      if (size_arg.is_literal)
       {
-        if (check_arg(call, Type::integer, 1, false))
+        auto *integer = dynamic_cast<Integer *>(&size_arg);
+        if (integer)
         {
-          auto &size_arg = *call.vargs->at(1);
-          if (size_arg.is_literal)
+          long value = integer->n;
+          if (value < 0)
           {
-            auto &integer = static_cast<Integer &>(size_arg);
-            long value = integer.n;
-            if (value < 0)
-            {
-              LOG(ERROR, call.loc, err_)
-                  << call.func << " cannot use negative length (" << value
-                  << ")";
-            }
-            buffer_size = value;
+            LOG(ERROR, call.loc, err_)
+                << call.func << " cannot use negative length (" << value << ")";
           }
+          buffer_size = value;
         }
       }
     }

tests/semantic_analyser.cpp
@@ -612,7 +612,7 @@ TEST(semantic_analyser, call_str_state_leak_regression_test)
 TEST(semantic_analyser, call_buf)
 {
   test("kprobe:f { buf(arg0, 1); }", 0);
-  test("kprobe:f { buf(arg0, -1); }", 10);
+  test("kprobe:f { buf(arg0, -1); }", 1);
   test("kprobe:f { @x = buf(arg0, 1); }", 0);
   test("kprobe:f { $x = buf(arg0, 1); }", 0);
   test("kprobe:f { buf(); }", 1);