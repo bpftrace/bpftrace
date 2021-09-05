d4fd305e40e820af79797ade356e477b3a6935e8
Khem Raj <raj.khem@gmail.com>
2021-09-04 17:28:17 -0700

    orc: Fix build with clang >= 13
    Fixes errors like
    src/ast/bpforc/bpforcv2.cpp:3:9: error: constructor for 'bpftrace::BpfOrc' must explicitly initialize the member 'ES' which does not have a default constructor
    BpfOrc::BpfOrc(TargetMachine *TM, DataLayout DL)
            ^
    
    Fixes https://github.com/iovisor/bpftrace/issues/1963
    
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- src/ast/bpforc/bpforc.h
+++ src/ast/bpforc/bpforc.h
@@ -20,6 +20,9 @@
 #ifdef LLVM_ORC_V2
 #include <llvm/ExecutionEngine/Orc/Core.h>
 #include <llvm/ExecutionEngine/Orc/JITTargetMachineBuilder.h>
+#if LLVM_VERSION_MAJOR >= 13
+#include <llvm/ExecutionEngine/Orc/ExecutorProcessControl.h>
+#endif
 #endif
 
 #include <optional>
@@ -72,7 +75,11 @@ private:
   std::unique_ptr<TargetMachine> TM;
   DataLayout DL;
 #if LLVM_VERSION_MAJOR >= 7
+#ifdef LLVM_ORC_V2
+  std::unique_ptr<ExecutionSession> ES;
+#else // LLVM_ORC_V1
   ExecutionSession ES;
+#endif
 #endif
 #if LLVM_VERSION_MAJOR >= 7 && LLVM_VERSION_MAJOR < 12
   std::shared_ptr<SymbolResolver> Resolver;
@@ -98,7 +105,21 @@ private:
 #endif
 
 public:
+#if LLVM_VERSION_MAJOR >= 13
+  ~BpfOrc()
+  {
+    if (auto Err = ES->endSession())
+      ES->reportError(std::move(Err));
+  }
+#endif
+#ifdef LLVM_ORC_V2
+  BpfOrc(TargetMachine *TM,
+         DataLayout DL,
+         std::unique_ptr<ExecutionSession> ES);
+#else
   BpfOrc(TargetMachine *TM, DataLayout DL);
+#endif
+
   void compile(std::unique_ptr<Module> M);
 
   /* Helper for creating a orc object, responsible for creating internal objects
@@ -134,7 +155,7 @@ public:
 #ifdef LLVM_ORC_V2
   Expected<JITEvaluatedSymbol> lookup(StringRef Name)
   {
-    return ES.lookup({ &MainJD }, Mangle(Name.str()));
+    return ES->lookup({ &MainJD }, Mangle(Name.str()));
   }
 #endif
 };
