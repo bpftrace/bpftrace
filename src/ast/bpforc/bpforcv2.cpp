da8891c4969c0a6e1aac6167a7ae3a8ea5565aa4
Khem Raj <raj.khem@gmail.com>
2021-09-04 17:28:17 -0700

    orc: Fix build with clang >= 13
    Fixes errors like
    src/ast/bpforc/bpforcv2.cpp:3:9: error: constructor for 'bpftrace::BpfOrc' must explicitly initialize the member 'ES' which does not have a default constructor
    BpfOrc::BpfOrc(TargetMachine *TM, DataLayout DL)
            ^
    
    Fixes https://github.com/iovisor/bpftrace/issues/1963
    
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- src/ast/bpforc/bpforcv2.cpp
+++ src/ast/bpforc/bpforcv2.cpp
@@ -1,21 +1,23 @@
 // Included by bpforc.cpp
 
-BpfOrc::BpfOrc(TargetMachine *TM, DataLayout DL)
+BpfOrc::BpfOrc(TargetMachine *TM,
+               DataLayout DL,
+               std::unique_ptr<ExecutionSession> ES)
     : TM(std::move(TM)),
       DL(std::move(DL)),
-      ObjectLayer(ES,
+      ES(std::move(ES)),
+      ObjectLayer(*(this->ES),
                   [this]() {
                     return std::make_unique<MemoryManager>(sections_);
                   }),
-      CompileLayer(ES,
+      CompileLayer(*this->ES,
                    ObjectLayer,
                    std::make_unique<SimpleCompiler>(*this->TM)),
-      Mangle(ES, this->DL),
+      Mangle(*this->ES, this->DL),
       CTX(std::make_unique<LLVMContext>()),
-      MainJD(cantFail(ES.createJITDylib("<main>")))
+      MainJD(cantFail(this->ES->createJITDylib("<main>")))
 {
 }
-
 LLVMContext &BpfOrc::getContext()
 {
   return *CTX.getContext();
@@ -34,8 +36,13 @@ std::unique_ptr<BpfOrc> BpfOrc::Create()
   // return unique_ptrs
   auto DL = cantFail(JTMB.getDefaultDataLayoutForTarget());
   auto TM = cantFail(JTMB.createTargetMachine());
-
-  return std::make_unique<BpfOrc>(TM.release(), std::move(DL));
+#if LLVM_VERSION_MAJOR >= 13
+  auto EPC = SelfExecutorProcessControl::Create();
+  auto ES = std::make_unique<ExecutionSession>(std::move(*EPC));
+#else
+  auto ES = std::make_unique<ExecutionSession>();
+#endif
+  return std::make_unique<BpfOrc>(TM.release(), std::move(DL), std::move(ES));
 }
 
 void BpfOrc::compile(std::unique_ptr<Module> M)
