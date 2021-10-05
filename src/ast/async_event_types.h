0d82e5b6035b05a08a88fc4b8a41d786732b8f35
Daniel Xu <dxu@dxuuu.xyz>
2021-10-05 14:56:50 -0700

    Make #include'ing async_event_types.h not require linking LLVM
    Before, if you #include'd async_event_types.h, you needed to link in
    libLLVMSupport.a in your final binary. This was b/c using (pretty much)
    any LLVM header pulled in the LLVM ABI checking machinery. That
    machinery requires linking against libLLVMSupport.a.
    
    We make this change so that the AOT runtime can be free of LLVM
    dependencies. This makes the AOT runtime both slimmer and easier to
    compile on distros that don't ship static LLVM libraries (ie
    libLLVMSupport.a).

--- src/ast/async_event_types.h
+++ src/ast/async_event_types.h
@@ -1,14 +1,28 @@
 #pragma once
 
-#include "irbuilderbpf.h"
+#include <cstddef>
 #include <cstdint>
-#include <llvm/IR/IRBuilder.h>
-#include <llvm/IR/Type.h>
+#include <vector>
+
+// Forward declare some LLVM types here. We do not #include any LLVM
+// headers b/c that will pull in LLVM's ABI checking machinery. We want
+// this header to be independent of LLVM during link time so that AOT
+// does not need to link against LLVM.
+namespace llvm {
+class Type;
+} // namespace llvm
+namespace bpftrace {
+namespace ast {
+class IRBuilderBPF;
+} // namespace ast
+} // namespace bpftrace
 
 /*
-  The main goal here is to keep the struct definitions close to each other,
-  making it easier to spot type mismatches.
-*/
+ * The main goal here is to keep the struct definitions close to each other,
+ * making it easier to spot type mismatches.
+ *
+ * If you update a type, remember to update the .cpp too!
+ */
 
 namespace bpftrace {
 namespace AsyncEvent {
@@ -20,15 +34,7 @@ struct Print
   uint32_t top;
   uint32_t div;
 
-  std::vector<llvm::Type*> asLLVMType(ast::IRBuilderBPF& b)
-  {
-    return {
-      b.getInt64Ty(), // asyncid
-      b.getInt32Ty(), // map id
-      b.getInt32Ty(), // top
-      b.getInt32Ty(), // div
-    };
-  }
+  std::vector<llvm::Type*> asLLVMType(ast::IRBuilderBPF& b);
 } __attribute__((packed));
 
 struct PrintNonMap
@@ -38,14 +44,7 @@ struct PrintNonMap
   // See below why we don't use a flexible length array
   uint8_t content[0];
 
-  std::vector<llvm::Type*> asLLVMType(ast::IRBuilderBPF& b, size_t size)
-  {
-    return {
-      b.getInt64Ty(),                            // asyncid
-      b.getInt64Ty(),                            // print id
-      llvm::ArrayType::get(b.getInt8Ty(), size), // content
-    };
-  }
+  std::vector<llvm::Type*> asLLVMType(ast::IRBuilderBPF& b, size_t size);
 } __attribute__((packed));
 
 struct MapEvent
@@ -53,13 +52,7 @@ struct MapEvent
   uint64_t action_id;
   uint32_t mapid;
 
-  std::vector<llvm::Type*> asLLVMType(ast::IRBuilderBPF& b)
-  {
-    return {
-      b.getInt64Ty(), // asyncid
-      b.getInt32Ty(), // map id
-    };
-  }
+  std::vector<llvm::Type*> asLLVMType(ast::IRBuilderBPF& b);
 } __attribute__((packed));
 
 struct Time
@@ -67,13 +60,7 @@ struct Time
   uint64_t action_id;
   uint32_t time_id;
 
-  std::vector<llvm::Type*> asLLVMType(ast::IRBuilderBPF& b)
-  {
-    return {
-      b.getInt64Ty(), // asyncid
-      b.getInt32Ty(), // time id
-    };
-  }
+  std::vector<llvm::Type*> asLLVMType(ast::IRBuilderBPF& b);
 } __attribute__((packed));
 
 struct Strftime
@@ -81,13 +68,7 @@ struct Strftime
   uint64_t strftime_id;
   uint64_t nsecs_since_boot;
 
-  std::vector<llvm::Type*> asLLVMType(ast::IRBuilderBPF& b)
-  {
-    return {
-      b.getInt64Ty(), // strftime id
-      b.getInt64Ty(), // strftime arg, time elapsed since boot
-    };
-  }
+  std::vector<llvm::Type*> asLLVMType(ast::IRBuilderBPF& b);
 } __attribute__((packed));
 
 struct Buf
@@ -99,13 +80,7 @@ struct Buf
   // the issue doesn't exist in GCC 7.5.x.
   char content[0];
 
-  std::vector<llvm::Type*> asLLVMType(ast::IRBuilderBPF& b, size_t length)
-  {
-    return {
-      b.getInt8Ty(),                               // buffer length
-      llvm::ArrayType::get(b.getInt8Ty(), length), // buffer content
-    };
-  }
+  std::vector<llvm::Type*> asLLVMType(ast::IRBuilderBPF& b, size_t length);
 } __attribute__((packed));
 
 struct HelperError
@@ -114,14 +89,7 @@ struct HelperError
   uint64_t error_id;
   int32_t return_value;
 
-  std::vector<llvm::Type*> asLLVMType(ast::IRBuilderBPF& b)
-  {
-    return {
-      b.getInt64Ty(), // asyncid
-      b.getInt64Ty(), // error_id
-      b.getInt32Ty(), // return value
-    };
-  }
+  std::vector<llvm::Type*> asLLVMType(ast::IRBuilderBPF& b);
 } __attribute__((packed));
 
 struct Watchpoint
@@ -130,14 +98,7 @@ struct Watchpoint
   uint64_t watchpoint_idx;
   uint64_t addr;
 
-  std::vector<llvm::Type*> asLLVMType(ast::IRBuilderBPF& b)
-  {
-    return {
-      b.getInt64Ty(), // asyncid
-      b.getInt64Ty(), // watchpoint_idx
-      b.getInt64Ty(), // addr
-    };
-  }
+  std::vector<llvm::Type*> asLLVMType(ast::IRBuilderBPF& b);
 } __attribute__((packed));
 
 struct WatchpointUnwatch
@@ -145,13 +106,7 @@ struct WatchpointUnwatch
   uint64_t action_id;
   uint64_t addr;
 
-  std::vector<llvm::Type*> asLLVMType(ast::IRBuilderBPF& b)
-  {
-    return {
-      b.getInt64Ty(), // asyncid
-      b.getInt64Ty(), // addr
-    };
-  }
+  std::vector<llvm::Type*> asLLVMType(ast::IRBuilderBPF& b);
 } __attribute__((packed));
 
 } // namespace AsyncEvent
