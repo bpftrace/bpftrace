a29d3b54aef90b001b026565f3edae4a66402dff
Yucong Sun <sunyucong@gmail.com>
2021-10-21 14:21:35 -0700

    Switch build.sh to use bionic and llvm 12 as base

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -eu
 pushd docker
-docker build --network host -t bpftrace-builder-alpine -f Dockerfile.alpine .
+docker build --network host -t bpftrace-builder-${BASE} --build-arg LLVM_VERSION=${LLVM_VERSION} -f Dockerfile.${BASE} .
 popd
