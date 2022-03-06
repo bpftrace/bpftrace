9f4addb21c4633c0f7cbbc353f748d592f38074c
bas smit <bas@baslab.org>
2022-03-06 12:00:14 +0100

    tests: add ability to skip a tool parsing test
    Small overhaul to avoid leaking local variables as global state and some
    shellcheck recommendations.

--- tests/tools-parsing-test.sh
+++ tests/tools-parsing-test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -u
 set +e;
 
 if [[ $EUID -ne 0 ]]; then
@@ -8,42 +9,67 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
 BPFTRACE_EXECUTABLE=${BPFTRACE_EXECUTABLE:-$DIR/../src/bpftrace};
-
 EXIT_STATUS=0;
-
 TOOLDIR=""
-
 OLDTOOLS=${TOOLS_TEST_OLDVERSION:-}
+IFS=',' read -ra SKIP_TOOLS <<< "${TOOLS_TEST_DISABLE:-"NONE"}"
 
-function tooldir() {
-    for dir in "../../tools" "/vagrant/tools"; do
-        if [[ -d "$dir" ]]; then
-            TOOLDIR="$dir"
-            return
-        fi
-    done
+function set_tooldir() {
+  local dir
+  for dir in "../../tools" "/vagrant/tools"; do
+      if [[ -d "$dir" ]]; then
+          TOOLDIR="$dir"
+          return
+      fi
+  done
 
-    >&2 echo "Tool dir not found"
-    exit 1
+  >&2 echo "Tool dir not found"
+  exit 1
 }
 
-tooldir
-
-for f in "$TOOLDIR"/*.bt; do
-  if [[ $OLDTOOLS =~ $(basename $f) ]]; then
-      tool=$(dirname $f)/old/$(basename $f)
+function do_test() {
+  local file="$1"
+  if $BPFTRACE_EXECUTABLE --unsafe -d "$file" 2>/dev/null >/dev/null; then
+    echo "$file    passed"
   else
-      tool=$f
-  fi
-  if $BPFTRACE_EXECUTABLE --unsafe -d $tool 2>/dev/null >/dev/null; then
-    echo "$tool    passed"
-  else
-    echo "$tool    failed";
-    $BPFTRACE_EXECUTABLE --unsafe -d $tool;
+    echo "$file    failed";
+    $BPFTRACE_EXECUTABLE --unsafe -d "$file";
     EXIT_STATUS=1;
   fi
-done
+}
+
+function skip_test() {
+  local name
+  name="$(basename "$1")"
+  for i in "${SKIP_TOOLS[@]}"; do
+    if [[ "$i" == "$name" ]]; then
+      return 0
+    fi
+  done
+  return 1
+}
+
+
+function do_tests () {
+  local f
+  local tool
+  for f in "$TOOLDIR"/*.bt; do
+    if skip_test "$f"; then
+      echo "Skipping $f"
+    else
+      if [[ $OLDTOOLS =~ $(basename "$f") ]]; then
+        tool="$(dirname "$f")/old/$(basename "$f")"
+        do_test "$tool"
+      else
+        do_test "$f"
+      fi
+    fi
+  done
+}
+
+
+set_tooldir
+do_tests
 
 exit $EXIT_STATUS
