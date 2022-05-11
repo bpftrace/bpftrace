bpftrace/bpftrace

commit 2b7e38ab60bf288ed8681ae08135710b5f5f6b1e

Load program just once for wildcard probes
Currently when tracing wildcard kprobes/kretprobes we load
program for each final probe. Like for:

  # bpftrace -e 'kprobe:kvm_a* {  }'
  Attaching 111 probes...
  ...

We will have:
  # bpftool prog | grep kprobe | wc -l
  111

111 loaded programs, which are the same.

Adding map of loaded programs based on their 'orig_name',
and make sure they are loaded just once. This way we prevent
multiple copies of the same program loaded for wildcard
probe and have just one.

Signed-off-by: Jiri Olsa <jolsa@kernel.org>

src/attached_probe.cpp
@@ -193,7 +193,7 @@ AttachedProbe::AttachedProbe(Probe &probe, std::tuple<uint8_t *, uintptr_t> func
 {
   load_prog();
   if (bt_verbose)
-    std::cerr << "Attaching " << probe_.name << std::endl;
+    std::cerr << "Attaching " << probe_.orig_name << std::endl;
   switch (probe_.type)
   {
     case ProbeType::kprobe:
@@ -305,7 +305,7 @@ AttachedProbe::~AttachedProbe()
   if (err)
     LOG(ERROR) << "failed to detach probe: " << probe_.name;
 
-  if (progfd_ >= 0)
+  if (close_progfd_ && progfd_ >= 0)
     close(progfd_);
 }
 
@@ -627,8 +627,48 @@ void AttachedProbe::resolve_offset_kprobe(bool safe_mode)
       path, symbol, sym_offset, func_offset, safe_mode, probe_.type);
 }
 
+std::map<std::string, int> AttachedProbe::cached_prog_fds_;
+
+bool AttachedProbe::use_cached_progfd(void)
+{
+  // Enabled for so far only for kprobes/kretprobes
+  if (probe_.type != ProbeType::kprobe && probe_.type != ProbeType::kretprobe)
+    return false;
+
+  // Only for the wildcard probe, because we can have multiple
+  // programs attached to single probe
+  if (!has_wildcard(probe_.orig_name))
+    return false;
+
+  // Keep map of loaded programs based on their 'orig_name',
+  // and make sure they are loaded just once and use cached
+  // fd as probe's progfd_.
+  // This way we prevent multiple copies of the same program
+  // loaded for wildcard probe.
+  auto search = cached_prog_fds_.find(probe_.orig_name);
+
+  if (search != cached_prog_fds_.end())
+  {
+    progfd_ = search->second;
+    close_progfd_ = false;
+    return true;
+  }
+
+  return false;
+}
+
+void AttachedProbe::cache_progfd(void)
+{
+  if (probe_.type != ProbeType::kprobe && probe_.type != ProbeType::kretprobe)
+    return;
+  cached_prog_fds_[probe_.orig_name] = progfd_;
+}
+
 void AttachedProbe::load_prog()
 {
+  if (use_cached_progfd())
+    return;
+
   uint8_t *insns = std::get<0>(func_);
   int prog_len = std::get<1>(func_);
   const char *license = "GPL";
@@ -652,9 +692,16 @@ void AttachedProbe::load_prog()
     if (bt_verbose)
       log_level = 1;
 
-    // bpf_prog_load rejects colons in the probe name
-    strncpy(name, probe_.name.c_str(), STRING_SIZE - 1);
+    // Use orig_name for program name so we get proper name for
+    // wildcard probes, replace wildcards with '.'
+    std::string orig_name = probe_.orig_name;
+    std::replace(orig_name.begin(), orig_name.end(), '*', '.');
+
+    strncpy(name, orig_name.c_str(), STRING_SIZE - 1);
     namep = name;
+
+    // bpf_prog_load rejects colons in the probe name,
+    // so start the name after the probe type, after ':'
     if (strrchr(name, ':') != NULL)
       namep = strrchr(name, ':') + 1;
 
@@ -757,6 +804,8 @@ void AttachedProbe::load_prog()
               << "The verifier log: " << std::endl
               << log_buf.get() << std::endl;
   }
+
+  cache_progfd();
 }
 
 void AttachedProbe::attach_kprobe(bool safe_mode)

tests/runtime/probe
@@ -27,6 +27,12 @@ EXPECT SUCCESS kprobe_offset [0-9][0-9]*
 TIMEOUT 5
 AFTER ./testprogs/syscall read
 
+NAME kprobe_wildcard
+RUN {{BPFTRACE}} --unsafe -e 'kprobe:ksys_* { system("/usr/sbin/bpftool prog | grep kprobe | grep ksys_ | wc -l"); exit(); }'
+EXPECT 1
+TIMEOUT 5
+REQUIRES /usr/sbin/bpftool
+
 # Note: this test may fail if you've installed a new kernel but not rebooted
 # yet. Reason is b/c bpftrace will look for a vmlinux based on the running kernel's
 # version.
@@ -57,6 +63,12 @@ RUN {{BPFTRACE}} runtime/scripts/kretprobe_order.bt
 EXPECT first second
 TIMEOUT 5
 AFTER /bin/bash -c "./testprogs/syscall nanosleep 1000"; /bin/bash -c "./testprogs/syscall nanosleep 1000"
+
+NAME kretprobe_wildcard
+RUN {{BPFTRACE}} --unsafe -e 'kretprobe:ksys_* { system("/usr/sbin/bpftool prog | grep kprobe | grep ksys_ | wc -l"); exit(); }'
+EXPECT 1
+TIMEOUT 5
+REQUIRES /usr/sbin/bpftool
 
 NAME uprobe
 PROG uprobe:/bin/bash:echo_builtin { printf("arg0: %d\n", arg0); exit();}

src/attached_probe.h
@@ -68,9 +68,14 @@ private:
   void attach_iter(void);
   int detach_iter(void);
 
+  static std::map<std::string, int> cached_prog_fds_;
+  bool use_cached_progfd(void);
+  void cache_progfd(void);
+
   Probe &probe_;
   std::tuple<uint8_t *, uintptr_t> func_;
   std::vector<int> perf_event_fds_;
+  bool close_progfd_ = true;
   int progfd_ = -1;
   uint64_t offset_ = 0;
   int tracing_fd_ = -1;

commit 11d83df09db537d957982df728c8d94b2a0b3228

Do not reuse fd for kprobes with the probe builtin
When using the 'probe' builtin in combination with wildcards, a single
prog must be generated for each attach point. This also prevents
re-using loaded programs for multiple kprobe attach points.

Fixes: 2b7e38ab ("Load program just once for wildcard probes")

src/attached_probe.cpp
@@ -635,9 +635,9 @@ bool AttachedProbe::use_cached_progfd(void)
   if (probe_.type != ProbeType::kprobe && probe_.type != ProbeType::kretprobe)
     return false;
 
-  // Only for the wildcard probe, because we can have multiple
-  // programs attached to single probe
-  if (!has_wildcard(probe_.orig_name))
+  // Only for a wildcard probe which does not need expansion,
+  // because we can have multiple programs attached to a single probe
+  if (!has_wildcard(probe_.orig_name) || probe_.need_expansion)
     return false;
 
   // Keep map of loaded programs based on their 'orig_name',

src/bpftrace.cpp
@@ -235,6 +235,7 @@ int BPFtrace::add_probe(ast::Probe &p)
       probe.orig_name = p.name();
       probe.ns = attach_point->ns;
       probe.name = attach_point->name(target, func_id);
+      probe.need_expansion = p.need_expansion;
       probe.freq = attach_point->freq;
       probe.address = attach_point->address;
       probe.func_offset = attach_point->func_offset;

src/types.h
@@ -493,6 +493,7 @@ struct Probe
   std::string orig_name;        // original full probe name,
                                 // before wildcard expansion
   std::string name;             // full probe name
+  bool need_expansion;
   std::string pin;              // pin file for iterator probes
   std::string ns;               // for USDT probes, if provider namespace not from path
   uint64_t loc = 0;             // for USDT probes

tests/runtime/probe
@@ -34,6 +34,12 @@ EXPECT progs: 1
 TIMEOUT 5
 REQUIRES /usr/sbin/bpftool
 
+NAME kprobe_wildcard probe builtin
+RUN {{BPFTRACE}} --unsafe -e 'kprobe:ksys_* { @ = probe; printf("progs: "); system("/usr/sbin/bpftool prog | grep kprobe | grep ksys_ | wc -l"); exit(); }'
+EXPECT progs: [1-9][0-9]+
+TIMEOUT 5
+REQUIRES /usr/sbin/bpftool
+
 # Note: this test may fail if you've installed a new kernel but not rebooted
 # yet. Reason is b/c bpftrace will look for a vmlinux based on the running kernel's
 # version.