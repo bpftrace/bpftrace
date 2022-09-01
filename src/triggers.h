56106a650a8f86d4d164b9881b3354a5e5fa12f3
Tomislav Novak <tnovak@fb.com>
2022-09-01 13:46:36 -0700

    arm: Ensure BEGIN and END triggers are 4-byte aligned
    If bpftrace is compiled with -mthumb (set by default when using the Android NDK
    toolchain), BEGIN_trigger and END_trigger functions don't meet the alignment
    requirement in uprobe_register (offset must be 4-byte aligned). This breaks
    BEGIN and END probes.
    
    ```
    $ readelf -s bpftrace |grep _trigger
      1: 00b91105     2 FUNC    GLOBAL DEFAULT   14 BEGIN_trigger
      2: 00b91107     2 FUNC    GLOBAL DEFAULT   14 END_trigger
    ```
    
    Force the compiler to generate ARM code for these two functions to make sure
    they are properly aligned.

--- src/triggers.h
+++ src/triggers.h
@@ -1,7 +1,22 @@
 #pragma once
 
+#if defined(__arm__)
+// Force the compiler to generate A32 instructions for the two trigger functions
+// to ensure they end up being 4-byte (UPROBE_SWBP_INSN_SIZE) aligned, otherwise
+// uprobe_register() fails with EINVAL (see kernel/events/uprobes.c).
+#define __target_attr __attribute__((target("arm")))
+#else
+#define __target_attr
+#endif
+
 extern "C"
 {
-  void __attribute__((noinline)) BEGIN_trigger() { asm (""); }
-  void __attribute__((noinline)) END_trigger() { asm (""); }
+  void __attribute__((noinline)) __target_attr BEGIN_trigger()
+  {
+    asm("");
+  }
+  void __attribute__((noinline)) __target_attr END_trigger()
+  {
+    asm("");
+  }
 }
