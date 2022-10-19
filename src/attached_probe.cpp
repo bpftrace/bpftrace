bpftrace/bpftrace

commit b09df8e20ac279cfcdadb56f19484d18ed527716

kprobe multi: use vendored libbpf enum everywhere
Necessary for building bpftrace on systems which do not have
BPF_TRACE_KPROBE_MULTI.

src/attached_probe.cpp
@@ -844,7 +844,8 @@ void AttachedProbe::attach_multi_kprobe(void)
   opts.kprobe_multi.syms = syms.data();
   opts.kprobe_multi.cnt = syms.size();
   opts.kprobe_multi.flags = probe_.type == ProbeType::kretprobe
-                                ? BPF_F_KPROBE_MULTI_RETURN
+                                ? static_cast<enum ::bpf_attach_type>(
+                                      libbpf::BPF_TRACE_KPROBE_MULTI)
                                 : 0;
 
   if (bt_verbose)

src/bpffeature.cpp
@@ -357,7 +357,7 @@ bool BPFfeature::has_kprobe_multi()
   attr.prog_type = static_cast<enum ::bpf_prog_type>(
       libbpf::BPF_PROG_TYPE_KPROBE);
   attr.expected_attach_type = static_cast<enum ::bpf_attach_type>(
-      BPF_TRACE_KPROBE_MULTI);
+      libbpf::BPF_TRACE_KPROBE_MULTI);
   attr.insns = reinterpret_cast<struct bpf_insn*>(insns);
   attr.license = "GPL";
 

commit df17ceea8ec25fc6fce2774f798762e9a067cffb

Fix kretprobe multi attachement
Commit b09df8e20ac279cfcdadb56f19484d18ed527716 made a mistake in
setting a kprobe_multi flag for kretprobes.

Fixes: b09df8e20ac279cfcdadb56f19484d18ed527716

src/attached_probe.cpp
@@ -835,8 +835,7 @@ void AttachedProbe::attach_multi_kprobe(void)
   opts.kprobe_multi.syms = syms.data();
   opts.kprobe_multi.cnt = syms.size();
   opts.kprobe_multi.flags = probe_.type == ProbeType::kretprobe
-                                ? static_cast<enum ::bpf_attach_type>(
-                                      libbpf::BPF_TRACE_KPROBE_MULTI)
+                                ? BPF_F_KPROBE_MULTI_RETURN
                                 : 0;
 
   if (bt_verbose)