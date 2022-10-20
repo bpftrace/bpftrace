0f4e6b708702b39cebdc9e6036913b3c1aea2890
Delyan Kratunov <delyank@meta.com>
2022-10-20 12:14:10 -0700

    Implement .BTF.ext func_info relocation
    Instead of using the .rel entries for .BTF.ext, we just parse .BTF.ext
    ourselves and emit new kernel-style entries to be used for bpf_prog_load
    calls.

--- src/bpfprogram.h
+++ src/bpfprogram.h
@@ -25,6 +25,10 @@ private:
   void assemble();
   void relocateInsns();
   void relocateMaps();
+  void relocateFuncInfos();
+  void appendFileFuncInfos(const struct btf_ext_info_sec *src,
+                           size_t func_info_rec_size,
+                           size_t insn_offset);
 
   BpfBytecode &bytecode_;
   BPFtrace &bpftrace_;
@@ -32,6 +36,12 @@ private:
   std::vector<uint8_t> code_;
   // Offset in code_ where the .text begins (if .text was appended)
   size_t text_offset_;
+
+  // Storage for kernel bpf_func_infos.
+  // Note that ELF bpf_func_infos  store byte offsets from the section
+  // start in insn_off, while the kernel expects _instruction_ offsets
+  // from the beginning of the program code (i.e. what's in code_).
+  std::vector<uint8_t> func_infos_;
 };
 
 } // namespace bpftrace
