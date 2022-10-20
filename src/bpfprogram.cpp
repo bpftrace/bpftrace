5da9bd5f53f9ad00a861f09c0f551105459cef04
Delyan Kratunov <delyank@meta.com>
2022-10-20 12:14:10 -0700

    Implement .BTF.ext func_info relocation
    Instead of using the .rel entries for .BTF.ext, we just parse .BTF.ext
    ourselves and emit new kernel-style entries to be used for bpf_prog_load
    calls.

--- src/bpfprogram.cpp
+++ src/bpfprogram.cpp
@@ -1,16 +1,50 @@
 #include "bpfprogram.h"
 #include "log.h"
 
-#include <bcc/libbpf.h>
+#include <cstdint>
 #include <cstring>
 #include <elf.h>
 #include <linux/bpf.h>
+#include <linux/btf.h>
 #include <optional>
 #include <stdexcept>
 #include <string>
 #include <tuple>
 
 namespace bpftrace {
+
+// We currently support building against really old
+// kernel/elf headers. These defines provide the information
+// that might be missing and are a stopgap until a high
+// enough libbpf is a requirement and this code can be removed.
+
+#ifndef R_BPF_64_64
+#define R_BPF_64_64 1
+#endif
+
+#ifndef BPF_PSEUDO_FUNC
+#define BPF_PSEUDO_FUNC 4
+#endif
+
+struct btf_ext_header
+{
+  __u16 magic;
+  __u8 version;
+  __u8 flags;
+  __u32 hdr_len;
+
+  /* All offsets are in bytes relative to the end of this header */
+  __u32 func_info_off;
+  __u32 func_info_len;
+};
+
+struct btf_ext_info_sec
+{
+  __u32 sec_name_off; /* offset to section name */
+  __u32 num_info;
+  /* Followed by num_info * record_size number of bytes */
+  __u8 data[0];
+};
 
 std::optional<BpfProgram> BpfProgram::CreateFromBytecode(
     const BpfBytecode &bytecode,
@@ -27,7 +61,11 @@ std::optional<BpfProgram> BpfProgram::CreateFromBytecode(
 BpfProgram::BpfProgram(const BpfBytecode &bytecode,
                        const char *name,
                        BPFtrace &bpftrace)
-    : bytecode_(bytecode), bpftrace_(bpftrace), name_(name), code_()
+    : bytecode_(bytecode),
+      bpftrace_(bpftrace),
+      name_(name),
+      code_(),
+      text_offset_(0)
 {
 }
 
@@ -133,6 +171,95 @@ void BpfProgram::relocateInsns()
                           bytecode_.find(".rel.text") != bytecode_.end();
     if (need_text_rels)
       throw std::logic_error("Relocations in .text are not implemented yet");
+  }
+
+  // Step 4: deal with bpf_func_infos.
+  relocateFuncInfos();
+}
+
+// Assumed structure:
+//
+// code_[0..text_offset_) - program
+// code_[text_offset_..code_.size()) - .text
+void BpfProgram::relocateFuncInfos()
+{
+  const auto *btfsec = bytecode_.find(".BTF")->second.data();
+  auto *btfhdr = reinterpret_cast<const struct btf_header *>(btfsec);
+  auto *btfstr = btfsec + btfhdr->hdr_len + btfhdr->str_off;
+
+  const auto *btfextsec = bytecode_.find(".BTF.ext")->second.data();
+
+  auto *exthdr = reinterpret_cast<const struct btf_ext_header *>(btfextsec);
+  auto *exthdr_end = btfextsec + exthdr->hdr_len;
+  auto *func_info_end = exthdr_end + exthdr->func_info_len;
+
+  auto *ptr = exthdr_end + exthdr->func_info_off;
+
+  auto func_info_rec_size = *reinterpret_cast<const __u32 *>(ptr);
+  if (sizeof(bpf_func_info) > func_info_rec_size)
+    throw std::invalid_argument("Unsupported bpf_func_info size");
+  ptr += sizeof(__u32);
+
+  // We need to find the bpf_ext_info_secs for our program section and,
+  // optionally, for .text. They're likely not in the order we need them in,
+  // so find them first, then copy things over, so we keep the invariant that
+  // the first func_info is for the function at offset 0.
+  //
+  const btf_ext_info_sec *text_funcinfo_sec = nullptr;
+  const btf_ext_info_sec *prog_funcinfo_sec = nullptr;
+
+  while (ptr < func_info_end)
+  {
+    auto *info_sec = reinterpret_cast<const struct btf_ext_info_sec *>(ptr);
+    auto *name = reinterpret_cast<const char *>(btfstr +
+                                                info_sec->sec_name_off);
+    if (text_offset_ > 0 && strcmp(name, ".text") == 0)
+      text_funcinfo_sec = reinterpret_cast<const btf_ext_info_sec *>(ptr);
+    else if (strcmp(name, name_.c_str()) == 0)
+      prog_funcinfo_sec = reinterpret_cast<const btf_ext_info_sec *>(ptr);
+
+    ptr += sizeof(struct btf_ext_info_sec) +
+           (uintptr_t)info_sec->num_info * func_info_rec_size;
+  }
+
+  if (prog_funcinfo_sec == nullptr)
+    throw std::invalid_argument("Missing btf_ext_info_sec for program section");
+  if (text_offset_ > 0 && text_funcinfo_sec == nullptr)
+    throw std::invalid_argument("Missing btf_ext_info_sec for .text section");
+
+  appendFileFuncInfos(prog_funcinfo_sec, func_info_rec_size, 0);
+  if (text_offset_ > 0)
+    appendFileFuncInfos(text_funcinfo_sec,
+                        func_info_rec_size,
+                        text_offset_ / sizeof(bpf_insn));
+}
+
+// Copy all ELF func_infos from src and convert them to kernel
+// bpf_func_infos, adding insn_off to the final value.
+void BpfProgram::appendFileFuncInfos(const struct btf_ext_info_sec *src,
+                                     size_t func_info_rec_size,
+                                     size_t insn_off)
+{
+  auto *ptr = reinterpret_cast<const uint8_t *>(src);
+  auto *hdr_end = ptr + sizeof(struct btf_ext_info_sec);
+  auto cnt = src->num_info;
+
+  size_t dstoff = func_infos_.size();
+  func_infos_.resize(dstoff + cnt * sizeof(struct bpf_func_info));
+
+  ptr = hdr_end;
+  while (ptr < hdr_end + cnt * func_info_rec_size)
+  {
+    auto *src_info = reinterpret_cast<const struct bpf_func_info *>(ptr);
+    auto *dst_info = reinterpret_cast<struct bpf_func_info *>(
+        func_infos_.data() + dstoff);
+
+    dst_info->type_id = src_info->type_id;
+    dst_info->insn_off = (src_info->insn_off / sizeof(struct bpf_insn)) +
+                         insn_off;
+
+    dstoff += sizeof(struct bpf_func_info);
+    ptr += func_info_rec_size;
   }
 }
 
--- src/bpfprogram.h
+++ src/bpfprogram.h
@@ -26,6 +26,10 @@ private:
   void assemble();
   void relocateInsns();
   void relocateMaps();
+  void relocateFuncInfos();
+  void appendFileFuncInfos(const struct btf_ext_info_sec *src,
+                           size_t func_info_rec_size,
+                           size_t insn_offset);
 
   const BpfBytecode &bytecode_;
   BPFtrace &bpftrace_;
@@ -33,6 +37,12 @@ private:
   std::vector<uint8_t> code_;
   // Offset in code_ where the .text begins (if .text was appended)
   size_t text_offset_;
+
+  // Storage for kernel bpf_func_infos.
+  // Note that ELF bpf_func_infos  store byte offsets from the section
+  // start in insn_off, while the kernel expects _instruction_ offsets
+  // from the beginning of the program code (i.e. what's in code_).
+  std::vector<uint8_t> func_infos_;
 };
 
 } // namespace bpftrace
