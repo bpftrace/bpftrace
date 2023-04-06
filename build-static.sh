720ed9b5f10c78e9b5de293503bbe131565f5601
Haosdent Huang <haosdent@gmail.com>
2023-04-06 17:38:07 +0800

    Include DWARF support in static build.

--- build-static.sh
+++ build-static.sh
@@ -3,5 +3,6 @@ set -eu
 export BASE=ubuntu-glibc
 export LLVM_VERSION=12
 export RUN_TESTS=0
+export BUILD_TESTING=ON
 ./build-docker-image.sh
-docker run --network host --rm -it -u $(id -u):$(id -g) -v $(pwd):$(pwd) -e STATIC_LINKING=ON -e STATIC_LIBC=OFF -e ALLOW_UNSAFE_PROBE=OFF -e VENDOR_GTEST=ON -e RUN_TESTS=${RUN_TESTS} -e EMBED_USE_LLVM=ON bpftrace-builder-${BASE} "$(pwd)/build-static" Release "$@"
+docker run --network host --rm -it -u $(id -u):$(id -g) -v $(pwd):$(pwd) -e STATIC_LINKING=ON -e STATIC_LIBC=OFF -e ALLOW_UNSAFE_PROBE=OFF -e VENDOR_GTEST=ON -e BUILD_TESTING=${BUILD_TESTING} -e RUN_TESTS=${RUN_TESTS} -e EMBED_USE_LLVM=ON bpftrace-builder-${BASE} "$(pwd)/build-static" Release "$@"
