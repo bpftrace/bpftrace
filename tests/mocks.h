8ada0534936760e552f47be2390e530c331ed141
Shuyi Cheng <chengshuyi@linux.alibaba.com>
2023-06-19 12:21:11 +0800

    add semantic analyser test for nsecs()
    Signed-off-by: Shuyi Cheng <chengshuyi@linux.alibaba.com>

--- tests/mocks.h
+++ tests/mocks.h
@@ -114,6 +114,7 @@ public:
     has_kprobe_multi_ = std::make_optional<bool>(has_features);
     has_skb_output_ = std::make_optional<bool>(has_features);
     map_ringbuf_ = std::make_optional<bool>(has_features);
+    has_ktime_get_tai_ns_ = std::make_optional<bool>(has_features);
   };
   bool has_features_;
 };
