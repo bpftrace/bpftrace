9136d1342a133b8f044bbc821d9d7265a188f6a0
Jiri Olsa <jolsa@kernel.org>
2023-10-26 08:56:37 +0000

    Add runtime tests for uprobe_multi link
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>

--- tests/runtime/engine/parser.py
+++ tests/runtime/engine/parser.py
@@ -165,6 +165,7 @@ class TestParser(object):
                     "dwarf",
                     "aot",
                     "kprobe_multi",
+                    "uprobe_multi",
                     "skboutput",
                     "get_tai_ns",
                     "get_func_ip",
