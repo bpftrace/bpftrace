6115cf0904673bc3940ba2d0215cc7415983664b
Daniel Xu <dxu@dxuuu.xyz>
2023-12-26 11:21:05 -0700

    NFC: Extract std::filesystem detection to header
    We had a few instances of copy paste in the code base. Better to pull it
    all into one place so it's not repeated.

--- tests/child.cpp
+++ tests/child.cpp
@@ -13,17 +13,8 @@
 
 #include "child.h"
 #include "childhelper.h"
+#include "filesystem.h"
 #include "utils.h"
-
-#if __has_include(<filesystem>)
-#include <filesystem>
-namespace std_filesystem = std::filesystem;
-#elif __has_include(<experimental/filesystem>)
-#include <experimental/filesystem>
-namespace std_filesystem = std::experimental::filesystem;
-#else
-#error "neither <filesystem> nor <experimental/filesystem> are present"
-#endif
 
 namespace bpftrace {
 namespace test {
