1c8bcc5e0c662e883bffa20fdf1869e804b8a033
Ben Olson <matthew.olson@intel.com>
2024-01-27 10:06:02 -0600

    Fixes a build issue that occurs when libdw is not installed.
    This is done by only generating the Dwarf test data if libdw is present. We
    don't need libdw to do the generation, but the test data is only used if libdw
    can be used to read it.
    
    Also fixes a difficult-to-reproduce issue related to using file-level
    dependencies in CMake, causing `data_source.o` to be re-generated for each
    target that depends on it.

--- tests/data/configure_dwarf_headers.cmake
+++ tests/data/configure_dwarf_headers.cmake
@@ -2,9 +2,6 @@
 # at cmake configuration stage and _not_ during build. So this script
 # is wrapped in a custom command so that it's only run when necessary.
 
-file(READ ${BTF_DATA_HEX} BTF_DATA)
-file(READ ${FUNC_LIST_HEX} FUNC_LIST)
 file(READ ${DWARF_DATA_HEX} DWARF_DATA)
 
-configure_file(${BTF_DATA_H_IN} ${BTF_DATA_H})
 configure_file(${DWARF_DATA_H_IN} ${DWARF_DATA_H})
