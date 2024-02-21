f42a1fc342cb62ff2570c7f3d16870a8550e236e
Alastair Robertson <ajor@meta.com>
2024-02-21 05:19:23 -0800

    Add CollectNodes utility visitor

---
+++ src/ast/passes/collect_nodes.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <functional>
+#include <vector>
+
+#include "ast/visitors.h"
+
+namespace bpftrace::ast {
+
+/*
+ * CollectNodes
+ *
+ * Recurses into the provided node and builds a list of all descendants of the
+ * requested type which match a predicate.
+ */
+template <typename NodeT>
+class CollectNodes : public Visitor {
+public:
+  void run(
+      Node &node,
+      std::function<bool(const NodeT &)> pred = [](const auto &) {
+        return true;
+      })
+  {
+    pred_ = pred;
+    node.accept(*this);
+  }
+  const std::vector<std::reference_wrapper<NodeT>> nodes() const
+  {
+    return nodes_;
+  }
+
+private:
+  void visit(NodeT &node) override
+  {
+    if (pred_(node)) {
+      nodes_.push_back(node);
+    }
+  }
+
+  std::vector<std::reference_wrapper<NodeT>> nodes_;
+  std::function<bool(const NodeT &)> pred_;
+};
+
+} // namespace bpftrace::ast
