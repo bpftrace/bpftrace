7b03523a76698f8c0900ac94b6b7e95060c0f8e1
Alastair Robertson <ajor@meta.com>
2024-05-15 03:00:30 -0700

    Fix intptrcast runtime tests
    Explicitly put numbers into an array on the stack instead of relying on
    architecture-specific calling conventions for stack-passed parameters.
    
    This removes the dependency on the stack pointer's value (which changed
    as of #3095) and makes the tests architecture independent.

--- tests/testprogs/intptrcast.c
+++ tests/testprogs/intptrcast.c
@@ -1,26 +1,15 @@
-int fn(short p1,
-       short p2,
-       short p3,
-       short p4,
-       short p5,
-       unsigned long p6,
-       short p7,
-       short p8,
-       short p9)
+#include <stdint.h>
+
+int fn(uint16_t nums[])
 {
-  return p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9;
+  return nums[0] + nums[1];
 }
 
 int main()
 {
-  fn(0x123,
-     0x456,
-     0x789,
-     0xabc,
-     0xdef,
-     0x1234567887654321,
-     0xcba,
-     0xcba,
-     0xcba);
+  uint16_t nums[] = {
+    0x123, 0x456, 0x789, 0xabc, 0xdef, 0xcba,
+  };
+  fn(nums);
   return 0;
 }
