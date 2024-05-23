7be07700e066139224cf099b1ff625689f7caae3
Alastair Robertson <ajor@meta.com>
2024-05-23 07:29:47 -0700

    CMake: Re-generate codegen_includes.cpp when test files are added or removed
    - CONFIGURE_DEPENDS triggers re-generation of the list of source files
      as necessary
    - Pass list of source files through to the sub command so we only have a
      single glob to deal with
    - Correctly set up the custom command's OUTPUT parameter so that
      codegen_includes.cpp is re-generated before it is compiled into
      bpftrace_test

--- tests/codegen/generate_codegen_includes.cmake
+++ tests/codegen/generate_codegen_includes.cmake
@@ -1,12 +1,12 @@
 # Combine all codegen tests into a single compilation unit to improve build
 # performance. https://github.com/bpftrace/bpftrace/issues/229
-function(generate_codegen_includes output)
+function(generate_codegen_includes output tests)
   file(REMOVE ${output})
-  file(GLOB tests ${TEST_SRC_DIR}/*.cpp)
   file(WRITE ${output} "")
   foreach(test ${tests})
     file(APPEND ${output} "#include \"${test}\"\n")
   endforeach()
 endfunction()
 
-generate_codegen_includes(${CMAKE_BINARY_DIR}/codegen_includes.cpp)
+separate_arguments(CODEGEN_SOURCES NATIVE_COMMAND ${CODEGEN_SOURCES})
+generate_codegen_includes("${CODEGEN_INCLUDES_CPP}" "${CODEGEN_SOURCES}")
