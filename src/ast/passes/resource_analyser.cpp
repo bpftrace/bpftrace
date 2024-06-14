7905bef7d40100ccbf0796e48ff0d5b7a73c57d7
Daniel Xu <dxu@dxuuu.xyz>
2024-06-14 13:01:50 -0600

    codegen: Support big path()
    This commit wires up path() codegen to use new string scratch buffer
    support. Now path() has same implementation as big str().

--- src/ast/passes/resource_analyser.cpp
+++ src/ast/passes/resource_analyser.cpp
@@ -155,7 +155,7 @@ void ResourceAnalyser::visit(Call &call)
       resources_.time_args.push_back(get_literal_string(*call.vargs->at(0)));
     else
       resources_.time_args.push_back("%H:%M:%S\n");
-  } else if (call.func == "str") {
+  } else if (call.func == "str" || call.func == "path") {
     resources_.str_buffers++;
   } else if (call.func == "strftime") {
     resources_.strftime_args.push_back(get_literal_string(*call.vargs->at(0)));
