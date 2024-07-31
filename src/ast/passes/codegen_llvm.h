a1504145efc7f7987231292fc4cdab1dcc8205c3
Fangrui Song <i@maskray.me>
2024-07-31 13:12:56 -0700

    codegen: Remove unused MCJIT.h include
    The usage was removed by #2376. Add some needed headers previously
    transitively included by MCJIT.h.
    
    MCJIT will be removed from LLVM:
    https://discourse.llvm.org/t/rfc-removing-mcjit-and-runtimedyld/80464

--- src/ast/passes/codegen_llvm.h
+++ src/ast/passes/codegen_llvm.h
@@ -6,10 +6,11 @@
 #include <ostream>
 #include <tuple>
 
-#include <llvm/ExecutionEngine/MCJIT.h>
 #include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
 #include <llvm/Support/raw_os_ostream.h>
+#include <llvm/Target/TargetMachine.h>
 
 #include "ast/async_ids.h"
 #include "ast/dibuilderbpf.h"
