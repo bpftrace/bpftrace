262da99d09ad28bd18d7bb13ff2df9b04f70b3ba
Daniel Xu <dxu@dxuuu.xyz>
2024-08-29 12:26:43 -0700

    irbuilder: NFC: Add asserts for big allocas
    First we remove the unused arraysize parameter for allocas. It was a
    Value so we can't statically know the real value. Since it was unused,
    delete some of the overloads.
    
    With arraysize deleted, we can now statically assert on alloca size. The
    new asserts help ensure that we don't try to allocate something too big
    on the stack. All such allocations need to go onto a scratch map.

--- src/ast/irbuilderbpf.h
+++ src/ast/irbuilderbpf.h
@@ -38,12 +38,6 @@ public:
                               const std::string &name = "");
   AllocaInst *CreateAllocaBPFInit(const SizedType &stype,
                                   const std::string &name);
-  AllocaInst *CreateAllocaBPF(llvm::Type *ty,
-                              llvm::Value *arraysize,
-                              const std::string &name = "");
-  AllocaInst *CreateAllocaBPF(const SizedType &stype,
-                              llvm::Value *arraysize,
-                              const std::string &name = "");
   AllocaInst *CreateAllocaBPF(int bytes, const std::string &name = "");
   void CreateMemsetBPF(Value *ptr, Value *val, uint32_t size);
   void CreateMemcpyBPF(Value *dst, Value *src, uint32_t size);
