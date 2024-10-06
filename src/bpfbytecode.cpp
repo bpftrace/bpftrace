bpftrace/bpftrace

commit 2cc006027d7cde725c7a3a6cf50312221cfc3975

Store bpf_object in BpfBytecode
bpf_object is libbpf's structure for representing a collection of BPF
programs, maps, etc. Typically, it's parsed from a single ELF file.

Since we now emit BTF information on maps into the produced ELF, we can
let libbpf parse it for us so that don't have to store it elsewhere
(currently in IMap/Map/FakeMap/MapManager).

This is another step towards libbpf-based loading and attachment of
programs. Eventually, we'd like to get into the state when BpfBytecode
only contains bpf_object and everything is managed by libbpf.

src/bpfbytecode.cpp
@@ -6,6 +6,14 @@
 
 namespace bpftrace {
 
+BpfBytecode::BpfBytecode(const void *elf, size_t elf_size)
+{
+  bpf_object_ = std::unique_ptr<struct bpf_object, bpf_object_deleter>(
+      bpf_object__open_mem(elf, elf_size, nullptr));
+  if (!bpf_object_)
+    throw std::runtime_error("The produced ELF is not a valid BPF object");
+}
+
 void BpfBytecode::addSection(const std::string &name,
                              std::vector<uint8_t> &&data)
 {

src/bpfbytecode.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "bpffeature.h"
+
+#include <bpf/libbpf.h>
 #include <cereal/access.hpp>
 #include <cstdint>
 #include <string>
@@ -16,6 +18,7 @@ public:
   BpfBytecode()
   {
   }
+  BpfBytecode(const void *elf, size_t elf_size);
 
   BpfBytecode(const BpfBytecode &) = delete;
   BpfBytecode &operator=(const BpfBytecode &) = delete;
@@ -30,6 +33,17 @@ public:
 
 private:
   SectionMap sections_;
+
+  // We need a custom deleter for bpf_object which will call bpf_object__close.
+  // Note that it is not possible to run bpf_object__close in ~BpfBytecode
+  // as the desctuctor may be called upon move assignment.
+  struct bpf_object_deleter {
+    void operator()(struct bpf_object *object)
+    {
+      bpf_object__close(object);
+    }
+  };
+  std::unique_ptr<struct bpf_object, bpf_object_deleter> bpf_object_;
 
   friend class cereal::access;
   template <typename Archive>

src/ast/elf_parser.cpp
@@ -4,7 +4,7 @@
 namespace bpftrace {
 namespace elf {
 
-BpfBytecode parseBpfBytecodeFromElfObject(void *const elf)
+BpfBytecode parseBpfBytecodeFromElfObject(void *const elf, size_t elf_size)
 {
   char *fileptr = (char *)elf;
   Elf64_Ehdr *ehdr = (Elf64_Ehdr *)elf;
@@ -28,7 +28,7 @@ BpfBytecode parseBpfBytecodeFromElfObject(void *const elf)
   assert(strtable_shdr->sh_type == SHT_STRTAB);
   char *strtable = fileptr + strtable_shdr->sh_offset;
 
-  BpfBytecode result;
+  BpfBytecode result(elf, elf_size);
 
   for (int i = 0; i < ehdr->e_shnum; ++i) {
     Elf64_Shdr *shdr = &shdrs[i];

src/ast/elf_parser.h
@@ -6,7 +6,7 @@
 namespace bpftrace {
 namespace elf {
 
-BpfBytecode parseBpfBytecodeFromElfObject(void* const elf);
+BpfBytecode parseBpfBytecodeFromElfObject(void* const elf, size_t elf_size);
 
 } // namespace elf
 } // namespace bpftrace

src/ast/passes/codegen_llvm.cpp
@@ -3552,7 +3552,7 @@ BpfBytecode CodegenLLVM::emit(void)
   assert(!output.empty());
 
   state_ = State::DONE;
-  return elf::parseBpfBytecodeFromElfObject(output.data());
+  return elf::parseBpfBytecodeFromElfObject(output.data(), output.size());
 }
 
 BpfBytecode CodegenLLVM::compile(void)

commit 62e83140b1672c1bf63c2325e1f1e7f3c6b452a6

bpfbytecode: Fix use-after-free
CodegenLLVM::emit() returns a BpfBytecode initialized with a reference
to a stack allocated SmallVector. When emit() returns, that SmallVector
is no longer valid.

Since libbpf does not make a copy of the elf file either, any
interaction with the opened bpf_object_ outside the constructor is a
UAF. And this occurs quite a lot.

Fix by making a copy of the elf file in BpfBytecode constructor.

Fixes: 2cc00602 ("Store bpf_object in BpfBytecode")

src/bpfbytecode.cpp
@@ -34,6 +34,7 @@ static std::optional<std::string> get_global_var_section_name(
 }
 
 BpfBytecode::BpfBytecode(std::span<const std::byte> elf)
+    : elf_(elf.begin(), elf.end())
 {
   int log_level = 0;
   // In debug mode, show full verifier log.
@@ -48,7 +49,7 @@ BpfBytecode::BpfBytecode(std::span<const std::byte> elf)
                        .kernel_log_level = static_cast<__u32>(log_level));
 
   bpf_object_ = std::unique_ptr<struct bpf_object, bpf_object_deleter>(
-      bpf_object__open_mem(elf.data(), elf.size(), &opts));
+      bpf_object__open_mem(elf_.data(), elf_.size(), &opts));
   if (!bpf_object_)
     LOG(BUG) << "The produced ELF is not a valid BPF object";
 

src/bpfbytecode.h
@@ -70,6 +70,7 @@ private:
   };
   std::unique_ptr<struct bpf_object, bpf_object_deleter> bpf_object_;
 
+  std::vector<std::byte> elf_;
   std::map<std::string, BpfMap> maps_;
   std::map<int, BpfMap *> maps_by_id_;
   std::map<std::string, BpfProgram> programs_;