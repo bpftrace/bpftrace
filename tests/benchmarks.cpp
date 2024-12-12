296cae783e17f334098c120c140423155504c929
Adin Scannell <adin@scannell.ca>
2024-12-11 22:33:53 -0800

    Add basic benchmark for parsing and passes
    This is a standalone executable for now, not wired into any tests. In
    the future we may want to expand the set of benchmarks and add more
    structure, to ensure that we can track parse times, compile times, etc.
    and catch any massive regressions. For now however, this is a simple
    tool that can be used to manually find regressions during refactors.
    
    The benchmark splits out the different stages as user counters. These will be
    pretty easy to see using `--benchmark_counters_tabular=true`. They are
    represented using "rates", and generally show their contribution to wall clock
    time for that specific benchmark (in terms of a second). So `0.9/s` would
    represent something that is responsible for 90% of wall-clock time.
    
    Here is an example of the `hello_world` benchmark:
    
    ```
    -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
    Benchmark                                                              Time             CPU   Iterations clang_parser codegen_emit codegen_generate_ir codegen_optimize field_analyser      parse resource_analyser semantic_analyser
    -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
    bpftrace::test::BM_compile/hello_world/process_time/real_time       42.8 ms         17.6 ms           17   0.897961/s  0.0545709/s          9.76501m/s      0.0278605/s     130.452u/s 3.76392m/s        758.345u/s        2.60491m/s
    ```
    
    Interestingly, for trivial programs the `clang_parser` is completely dominant.
    However, this will need to be built out a bit further before making any hard
    conclusions.

---
+++ tests/benchmarks.cpp
@@ -0,0 +1,148 @@
+#include "benchmark/benchmark.h"
+#include "gmock/gmock.h"
+
+#include "mocks.h"
+
+#include "ast/passes/codegen_llvm.h"
+#include "ast/passes/field_analyser.h"
+#include "ast/passes/resource_analyser.h"
+#include "ast/passes/semantic_analyser.h"
+
+#include "bpffeature.h"
+#include "bpftrace.h"
+#include "clang_parser.h"
+#include "driver.h"
+
+namespace bpftrace {
+namespace test {
+
+// We break the benchmarks into several phases, in order to track issues
+// separately. Note that currently each meaningful pass has its own phase, but
+// these could be logically merged as "optimizations" or some other passes with
+// some minor code restructuring.
+enum class Phase {
+  Parse,
+  FieldAnalyser,
+  ClangParser,
+  SemanticAnalyser,
+  ResourceAnalyser,
+  CodegenGenerateIR,
+  CodegenOptimize,
+  CodegenEmit,
+};
+
+constexpr const char *phaseName(Phase p)
+{
+  switch (p) {
+    case Phase::Parse:
+      return "parse";
+    case Phase::FieldAnalyser:
+      return "field_analyser";
+    case Phase::ClangParser:
+      return "clang_parser";
+    case Phase::SemanticAnalyser:
+      return "semantic_analyser";
+    case Phase::ResourceAnalyser:
+      return "resource_analyser";
+    case Phase::CodegenGenerateIR:
+      return "codegen_generate_ir";
+    case Phase::CodegenOptimize:
+      return "codegen_optimize";
+    case Phase::CodegenEmit:
+      return "codegen_emit";
+    default:
+      return "unknown";
+  };
+}
+
+template <Phase P, typename Func>
+void measure(benchmark::State &state, Func fn)
+{
+  auto start = std::chrono::high_resolution_clock::now();
+  fn();
+  auto end = std::chrono::high_resolution_clock::now();
+
+  // Add this component into the overall benchmark.
+  //
+  // Note that in an ideal world, we could track the aggregate process CPU time
+  // and break this down per component as well. Unfortunately, these APIs are
+  // not readily available through the benchmark APIs, and may be too expensive
+  // to do in the context of this loop (whereas VDSO-based time is cheap).
+  // Therefore, we frame everything in terms of real-time, including the
+  // overall benchmarks themselves. This may be subject to plenty of noise and
+  // side effects, but at least it is also representative of a user's
+  // interactive experience.
+  auto time = std::chrono::duration<double>(end - start).count();
+  constexpr const char *name = phaseName(P);
+  auto counter = state.counters.find(name);
+  if (counter != state.counters.end()) {
+    counter->second += time;
+  } else {
+    state.counters[name] = benchmark::Counter(time,
+                                              benchmark::Counter::kIsRate);
+  }
+}
+
+void BM_compile(benchmark::State &state, const std::string &input)
+{
+  auto bpftrace = get_mock_bpftrace();
+  bpftrace->feature_ = std::make_unique<MockBPFfeature>(true);
+
+  for (auto _ : state) {
+    std::unique_ptr<Driver> driver;
+    std::unique_ptr<ast::FieldAnalyser> fields;
+    std::unique_ptr<ClangParser> clang;
+    std::unique_ptr<ast::SemanticAnalyser> semantics;
+    std::unique_ptr<ast::ResourceAnalyser> resources;
+    std::optional<RequiredResources> required_resources;
+    std::unique_ptr<ast::CodegenLLVM> codegen;
+
+    measure<Phase::Parse>(state, [&] {
+      driver = std::make_unique<Driver>(*bpftrace);
+      ASSERT_EQ(driver->parse_str(input), 0);
+    });
+    measure<Phase::FieldAnalyser>(state, [&] {
+      fields = std::make_unique<ast::FieldAnalyser>(driver->ctx.root,
+                                                    *bpftrace);
+      ASSERT_EQ(fields->analyse(), 0);
+    });
+    measure<Phase::ClangParser>(state, [&] {
+      clang = std::make_unique<ClangParser>();
+      clang->parse(driver->ctx.root, *bpftrace);
+    });
+    measure<Phase::SemanticAnalyser>(state, [&] {
+      semantics = std::make_unique<ast::SemanticAnalyser>(driver->ctx,
+                                                          *bpftrace);
+      ASSERT_EQ(semantics->analyse(), 0);
+    });
+    measure<Phase::ResourceAnalyser>(state, [&] {
+      resources = std::make_unique<ast::ResourceAnalyser>(driver->ctx.root,
+                                                          *bpftrace);
+      required_resources = resources->analyse();
+      ASSERT_TRUE(required_resources.has_value());
+    });
+    measure<Phase::CodegenGenerateIR>(state, [&] {
+      bpftrace->resources = required_resources.value();
+      codegen = std::make_unique<ast::CodegenLLVM>(driver->ctx.root, *bpftrace);
+      codegen->generate_ir();
+    });
+    measure<Phase::CodegenOptimize>(state, [&] { codegen->optimize(); });
+    measure<Phase::CodegenEmit>(state, [&] { codegen->emit(false); });
+  }
+}
+
+} // namespace test
+} // namespace bpftrace
+
+BENCHMARK_CAPTURE(bpftrace::test::BM_compile, hello_world, R"(
+BEGIN
+{
+    printf("hello world!\n");
+    exit();
+}
+)")
+    ->MeasureProcessCPUTime()
+    ->UseRealTime()
+    ->Unit(benchmark::kMillisecond);
+
+BENCHMARK_MAIN();
