26109ab0deb8b6a450aa8caa1a9e7ad8c5f1e201
Daniel Xu <dxu@dxuuu.xyz>
2025-01-29 09:13:54 -0700

    nix: Remove unnecessary recursive set
    Nothing in that mkDerivation is recursive.

--- flake.nix
+++ flake.nix
@@ -64,7 +64,7 @@
           # Define lambda that returns a derivation for bpftrace given llvm version as input
           mkBpftrace =
             llvmVersion:
-              pkgs.stdenv.mkDerivation rec {
+              pkgs.stdenv.mkDerivation {
                 name = "bpftrace";
 
                 src = self;
