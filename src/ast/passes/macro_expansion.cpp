b601802f946bc4ff4d642da83240b11e4ff430bf
Daniel Xu <dxu@dxuuu.xyz>
2025-02-23 17:42:24 -0700

    Implement macro specialization
    Now this works:
    
    ```
    macro add_one($a) {
      $a + 1
    }
    
    macro add_one_to_each($a, @b) {
      $a + 1 + @b + 1
    }
    
    BEGIN {
      $a = 42;
      print(add_one($a));
    
      @b = 3;
      print(add_one_to_each($a, @b));
    }
    ```
    
    ```
    $ sudo ./build/src/bpftrace ~/scratch/macros.bt
    Attaching 1 probe...
    43
    47
    ^C
    
    @b: 3
    ```
    
    The error handling is still quite rough, but the main semantics should
    be enforced. Notably: hygiene, argument constraints, multiple
    specializations, and (by way of unknown call in semantic analysis)
    nested macro calls.
    
    Signed-off-by: Daniel Xu <dxu@dxuuu.xyz>

--- src/ast/passes/macro_expansion.cpp
+++ src/ast/passes/macro_expansion.cpp
@@ -2,30 +2,122 @@
 
 #include "log.h"
 
+#include <cstdint>
+
 namespace bpftrace::ast {
 
-MacroExpansion::MacroExpansion(ASTContext &ctx)
-    : Visitor<MacroExpansion>(ctx)
+MacroSpecializer::MacroSpecializer(ASTContext &ctx, std::ostream &out)
+    : Visitor<MacroSpecializer>(ctx), out_(out)
 {
 }
 
-void MacroExpansion::run()
+void MacroSpecializer::visit(Variable &var)
+{
+  if (auto it = vars_.find(var.ident); it != vars_.end()) {
+    var.ident = it->second;
+  } else {
+    LOG(ERROR, var.loc, err_) << "Unhygienic access to variable";
+  }
+}
+
+void MacroSpecializer::visit(Map &map)
+{
+  if (auto it = maps_.find(map.ident); it != maps_.end()) {
+    map.ident = it->second;
+  } else {
+    LOG(ERROR, map.loc, err_) << "Unhygienic access to map";
+  }
+}
+
+Expression *MacroSpecializer::specialize(Macro &macro, const Call &call)
+{
+  maps_.clear();
+  vars_.clear();
+
+  if (macro.args.size() != call.vargs.size()) {
+    LOG(ERROR, call.loc, err_)
+        << "Call to macro has wrong number arguments: " << macro.args.size()
+        << "!=" << call.vargs.size();
+  }
+
+  for (size_t i = 0; i < call.vargs.size(); i++) {
+    Expression *marg = macro.args[i];
+    Expression *carg = call.vargs[i];
+
+    if (auto *cvar = dynamic_cast<Variable *>(carg)) {
+      if (auto *mvar = dynamic_cast<Variable *>(marg)) {
+        vars_[mvar->ident] = cvar->ident;
+      } else {
+        LOG(ERROR, call.loc, err_)
+            << "Mismatched arg=" << i << " to macro call";
+      }
+    } else if (auto *cmap = dynamic_cast<Map *>(carg)) {
+      if (auto *mmap = dynamic_cast<Map *>(marg)) {
+        maps_[mmap->ident] = cmap->ident;
+      } else {
+        LOG(ERROR, call.loc, err_)
+            << "Mismatched arg=" << i << " to macro call";
+      }
+    } else {
+      LOG(BUG) << "Parser let in a non-var and non-map macro argument";
+    }
+  }
+
+  // TODO: clone the macro body
+  visit(macro.expr);
+
+  std::string errors = err_.str();
+  if (!errors.empty()) {
+    out_ << errors;
+    return nullptr;
+  } else {
+    return macro.expr;
+  }
+}
+
+MacroExpansion::MacroExpansion(ASTContext &ctx, std::ostream &out)
+    : Visitor<MacroExpansion>(ctx), out_(out)
+{
+}
+
+int MacroExpansion::run()
 {
   for (Macro *macro : ctx_.root->macros) {
     macros_[macro->name] = macro;
   }
 
   visit(ctx_.root);
+
+  std::string errors = err_.str();
+  if (!errors.empty()) {
+    out_ << errors;
+    return 1;
+  }
+
+  return 0;
 }
 
 Expression *MacroExpansion::replace(Call *call, [[maybe_unused]] void *)
 {
   if (auto it = macros_.find(call->func); it != macros_.end()) {
-    LOG(DEBUG) << "Expanding call to: '" << call->func << "'";
-    Macro *macro = it->second;
+    if (called_.contains(call->func)) {
+      LOG(ERROR, call->loc, err_)
+          << "The PoC can only handle a single call of: " << call->func;
+      return nullptr;
+    } else {
+      called_.insert(call->func);
+    }
 
-    // XXX: this does not rewrite macro args yet
-    return macro->expr;
+    Macro *macro = it->second;
+    MacroSpecializer specializer(ctx_, out_);
+    Expression *expr = specializer.specialize(*macro, *call);
+    if (expr) {
+      return expr;
+    } else {
+      LOG(ERROR, call->loc, err_)
+          << "Failed to specialize macro: " << call->func;
+      return call;
+    }
   }
 
   return call;
@@ -35,7 +127,8 @@ Pass CreateMacroExpansionPass()
 {
   auto fn = [](PassContext &ctx) {
     MacroExpansion expander(ctx.ast_ctx);
-    expander.run();
+    if (expander.run())
+      return PassResult::Error("");
 
     return PassResult::Success();
   };
