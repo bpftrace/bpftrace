2594dd374ddf05466931cc4f70cd9f7cd23119f1
Yitang Yang <stringcatwok@gmail.com>
2025-02-24 21:52:59 +0800

    Fix flex scanner push-back overflow issue
    If we use yy_scan_string to scan the source
    string, the buffer size will be set to the size of
    the string, rather than the default YY_BUF_SIZE.
    This can easily cause a "push-back overflow" issue
    when using the unput() function due to the smaller
    buffer size.
    
    Previously, we assumed the remaining buffer size
    was always YY_BUF_SIZE, but unparsed strings were
    still stored in the buffer. This also led to
    "push-back overflow" issues.
    
    We solve this issue by overriding YY_INPUT. In this
    case, the buffer size will be the default
    YY_BUF_SIZE (16384), and each read will only copy
    YY_READ_BUF_SIZE (8192) bytes of data to the buffer.
    This means we have 8192 bytes available for macro
    expansion, which is greater than the expansion
    limit of 1000 bytes.
    
    Signed-off-by: Yitang Yang <stringcatwok@gmail.com>

--- src/driver.cpp
+++ src/driver.cpp
@@ -1,10 +1,7 @@
 #include "driver.h"
 #include "parser.tab.hh"
 
-struct yy_buffer_state;
-
-extern struct yy_buffer_state *yy_scan_string(const char *yy_str,
-                                              yyscan_t yyscanner);
+extern void set_source_string(const std::string *s);
 extern int yylex_init(yyscan_t *scanner);
 extern int yylex_destroy(yyscan_t yyscanner);
 extern bpftrace::location loc;
@@ -22,7 +19,7 @@ void Driver::parse()
   if (debug) {
     parser.set_debug_level(1);
   }
-  yy_scan_string(ctx.source_->contents.c_str(), scanner);
+  set_source_string(&ctx.source_->contents);
   parser.parse();
   yylex_destroy(scanner);
 }
