3b7de342756b8de93b1153b5d1cbb1bda8c87e4a
Jordan Rome <linux@jordanrome.com>
2025-02-25 05:04:36 -0800

    Minor refactor of PidFilterPass
    Signed-off-by: Jordan Rome <linux@jordanrome.com>
    Signed-off-by: mingbo-lc <114450847+mingbo-lc@users.noreply.github.com>

--- src/ast/passes/pid_filter_pass.cpp
+++ src/ast/passes/pid_filter_pass.cpp
@@ -4,43 +4,11 @@
 
 namespace bpftrace::ast {
 
-Pass CreatePidFilterPass()
-{
-  auto fn = [](PassContext &ctx) {
-    auto pid_filter = PidFilterPass(ctx.ast_ctx, ctx.b);
-    pid_filter.analyse();
-    return PassResult::Success();
-  };
-
-  return Pass("PidFilter", fn);
-};
-
-void PidFilterPass::analyse()
-{
-  visit(ctx_.root);
-}
-
-void PidFilterPass::visit(Probe &probe)
-{
-  bool needs_pid_filter = false;
-
-  for (AttachPoint *ap : probe.attach_points) {
-    if (bpftrace_.pid().has_value() && probe_needs_pid_filter(ap)) {
-      needs_pid_filter = true;
-    }
-  }
-
-  if (needs_pid_filter) {
-    probe.block->stmts.insert(probe.block->stmts.begin(),
-                              add_pid_filter(probe.loc));
-  }
-}
-
 // If the probe can't filter by pid when attaching
 // then we inject custom AST to filter by pid.
 // Note: this doesn't work for AOT as the code has already
 // been generated
-bool PidFilterPass::probe_needs_pid_filter(AttachPoint *ap)
+bool probe_needs_pid_filter(AttachPoint *ap)
 {
   ProbeType type = probetype(ap->provider);
 
@@ -71,19 +39,50 @@ bool PidFilterPass::probe_needs_pid_filter(AttachPoint *ap)
   return false;
 }
 
-Statement *PidFilterPass::add_pid_filter(const location &loc)
+Statement *create_pid_filter(ASTContext &ctx, int pid, const location &loc)
 {
-  return ctx_.make_node<If>(
-      ctx_.make_node<Binop>(ctx_.make_node<Builtin>("pid", loc),
-                            Operator::NE,
-                            ctx_.make_node<Integer>(bpftrace_.pid().value_or(0),
-                                                    loc),
-                            loc),
-      ctx_.make_node<Block>(std::vector<Statement *>{ ctx_.make_node<Jump>(
-                                JumpType::RETURN, loc) },
-                            loc),
-      ctx_.make_node<Block>(std::vector<Statement *>{}, loc),
+  return ctx.make_node<If>(
+      ctx.make_node<Binop>(ctx.make_node<Builtin>("pid", loc),
+                           Operator::NE,
+                           ctx.make_node<Integer>(pid, loc),
+                           loc),
+      ctx.make_node<Block>(std::vector<Statement *>{ ctx.make_node<Jump>(
+                               JumpType::RETURN, loc) },
+                           loc),
+      ctx.make_node<Block>(std::vector<Statement *>{}, loc),
       loc);
+}
+
+Pass CreatePidFilterPass()
+{
+  auto fn = [](PassContext &ctx) {
+    auto pid_filter = PidFilterPass(ctx.ast_ctx, ctx.b);
+    pid_filter.analyse();
+    return PassResult::Success();
+  };
+
+  return Pass("PidFilter", fn);
+};
+
+void PidFilterPass::analyse()
+{
+  visit(ctx_.root);
+}
+
+void PidFilterPass::visit(Probe &probe)
+{
+  const auto pid = bpftrace_.pid();
+  if (!pid.has_value()) {
+    return;
+  }
+
+  for (AttachPoint *ap : probe.attach_points) {
+    if (probe_needs_pid_filter(ap)) {
+      probe.block->stmts.insert(probe.block->stmts.begin(),
+                                create_pid_filter(ctx_, *pid, probe.loc));
+      return;
+    }
+  }
 }
 
 } // namespace bpftrace::ast
