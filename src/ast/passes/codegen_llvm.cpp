46fed95cf2b9b5eb6c542813b10b9c37ecdf2113
Viktor Malik <viktor.malik@gmail.com>
2025-02-27 09:30:44 +0100

    codegen: Fix build for LLVM 20
    Address two issues with LLVM 20 build:
    
    - Intrinsic::getDeclaration was deprecated and replaced by
      Intrinsic::getOrInsertDeclaration
    - The second parameter of PassBuilder::buildPerModuleDefaultPipeline was
      changed. Since we always use the default value, just remove it.
    
    Signed-off-by: Viktor Malik <viktor.malik@gmail.com>

--- src/ast/passes/codegen_llvm.cpp
+++ src/ast/passes/codegen_llvm.cpp
@@ -1453,9 +1453,14 @@ ScopedExpr CodegenLLVM::visit(Call &call)
     assert(arg->type.IsIntegerTy());
     if (arg->type.GetSize() > 1) {
       llvm::Type *arg_type = b_.GetType(arg->type);
+#if LLVM_VERSION_MAJOR >= 20
+      llvm::Function *swap_fun = Intrinsic::getOrInsertDeclaration(
+          module_.get(), Intrinsic::bswap, { arg_type });
+#else
       llvm::Function *swap_fun = Intrinsic::getDeclaration(module_.get(),
                                                            Intrinsic::bswap,
                                                            { arg_type });
+#endif
 
       return ScopedExpr(b_.CreateCall(swap_fun, { scoped_arg.value() }),
                         std::move(scoped_arg));
@@ -3964,8 +3969,7 @@ void CodegenLLVM::optimize()
   pb.crossRegisterProxies(lam, fam, cgam, mam);
 
   ModulePassManager mpm = pb.buildPerModuleDefaultPipeline(
-      llvm::OptimizationLevel::O3,
-      /*LTOPreLink=*/false);
+      llvm::OptimizationLevel::O3);
   mpm.run(*module_, mam);
 
   state_ = State::OPT;
