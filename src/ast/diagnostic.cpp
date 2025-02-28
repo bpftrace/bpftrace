8ecad86c431d228d69fa58816761de11eab57c54
Adin Scannell <amscanne@meta.com>
2025-02-28 15:09:24 -0800

    ast: Allow incremental printing of diagnostics
    This is minor improvement that allows individual diagnostics to be
    printed. The `emit` methods build on each other in an obvious way.
    
    Signed-off-by: Adin Scannell <amscanne@meta.com>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/3852, branch: user/amscanne/diags/4

--- src/ast/diagnostic.cpp
+++ src/ast/diagnostic.cpp
@@ -4,22 +4,34 @@
 
 namespace bpftrace::ast {
 
-void Diagnostics::emit(std::ostream& out)
+void Diagnostics::emit(std::ostream& out) const
 {
-  // Emit all errors first, following by all warnings. Hints follow the
-  // structured log message in the same location.
-  foreach(Severity::Error, [&out](const Diagnostic& d) {
-    LOG(ERROR, d.loc(), out) << d.msg();
-    if (auto s = d.hint(); s.size() > 0) {
-      LOG(HINT, out) << s;
-    }
-  });
-  foreach(Severity::Warning, [&out](const Diagnostic& d) {
-    LOG(WARNING, d.loc(), out) << d.msg();
-    if (auto s = d.hint(); s.size() > 0) {
-      LOG(HINT, out) << s;
-    }
-  });
+  // Emit all errors first, following by all warnings.
+  emit(out, Severity::Error);
+  emit(out, Severity::Warning);
+}
+
+void Diagnostics::emit(std::ostream& out, Severity s) const
+{
+  foreach(s, [this, s, &out](const Diagnostic& d) { emit(out, s, d); });
+}
+
+void Diagnostics::emit(std::ostream& out, Severity s, const Diagnostic& d) const
+{
+  switch (s) {
+    case Severity::Warning:
+      LOG(WARNING, d.loc(), out) << d.msg();
+      if (auto msg = d.hint(); msg.size() > 0) {
+        LOG(HINT, out) << msg;
+      }
+      break;
+    case Severity::Error:
+      LOG(ERROR, d.loc(), out) << d.msg();
+      if (auto msg = d.hint(); msg.size() > 0) {
+        LOG(HINT, out) << msg;
+      }
+      break;
+  }
 }
 
 } // namespace bpftrace::ast
