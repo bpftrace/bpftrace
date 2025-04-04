fdb3e62eab9bde7a27ccdfc9fa80653e0eb0822d
Jordan Rome <linux@jordanrome.com>
2025-04-04 08:51:41 -0700

    Move has_module_btf into BTF class
    This removes a circular dependency
    between BPFfeature and BTF.
    
    Signed-off-by: Jordan Rome <linux@jordanrome.com>

--- src/btf.h
+++ src/btf.h
@@ -77,6 +77,7 @@ public:
   ~BTF();
 
   bool has_data();
+  bool has_module_btf();
   bool modules_loaded() const;
   size_t objects_cnt() const
   {
@@ -157,6 +158,7 @@ private:
   BPFtrace* bpftrace_ = nullptr;
   std::string all_funcs_;
   std::string all_rawtracepoints_;
+  std::optional<bool> has_module_btf_;
 };
 
 inline bool BTF::has_data()
