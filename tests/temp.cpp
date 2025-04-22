7fae655dd6d43ed9e4075e67642ff21e58f43a61
Adin Scannell <amscanne@meta.com>
2025-04-22 11:58:54 -0700

    util: add `TempDir` to create temporary directories
    In order to add a common working directory, add a `TempDir` utility
    which can collect a common set of temporary files.
    
    Signed-off-by: Adin Scannell <amscanne@meta.com>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4090, branch: user/amscanne/extern_objects/17

--- tests/temp.cpp
+++ tests/temp.cpp
@@ -6,7 +6,9 @@
 
 namespace bpftrace::test::types {
 
+using util::TempDir;
 using util::TempFile;
+using util::TempFileError;
 
 TEST(util, tempfile_no_pattern)
 {
@@ -35,6 +37,38 @@ TEST(util, tempfile_good_pattern)
   ASSERT_TRUE(bool(f2));
   EXPECT_TRUE(f1->path().filename().string().starts_with("testing."));
   EXPECT_TRUE(f2->path().filename().string().starts_with("testing."));
+}
+
+TEST(util, tempdir)
+{
+  auto d = TempDir::create();
+  ASSERT_TRUE(bool(d));
+  auto f1 = d->create_file("foo");
+  auto f2 = d->create_file("bar");
+  auto f3 = d->create_file();
+  ASSERT_TRUE(bool(f1));
+  ASSERT_TRUE(bool(f2));
+  ASSERT_TRUE(bool(f3));
+  EXPECT_TRUE(f1->path().filename().string().starts_with("foo."));
+  EXPECT_TRUE(f2->path().filename().string().starts_with("bar."));
+}
+
+TEST(util, tempdir_no_pattern)
+{
+  auto d = TempDir::create();
+  ASSERT_TRUE(bool(d));
+  auto f1 = d->create_file("foo", false);
+  auto f2 = d->create_file("bar", false);
+  ASSERT_TRUE(bool(f1));
+  ASSERT_TRUE(bool(f2));
+  EXPECT_EQ(f1->path().filename().string(), "foo");
+  EXPECT_EQ(f2->path().filename().string(), "bar");
+  auto ok = d->create_file("foo", false);
+  EXPECT_FALSE(bool(ok));
+  auto nowOk = handleErrors(std::move(ok), [&](const TempFileError &err) {
+    EXPECT_EQ(err.err(), EEXIST);
+  });
+  EXPECT_TRUE(bool(nowOk));
 }
 
 } // namespace bpftrace::test::types
