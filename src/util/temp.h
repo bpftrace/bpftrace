b42463cfa23a7eee0bddd2df37a3e40dac5263ab
Adin Scannell <amscanne@meta.com>
2025-04-22 11:58:54 -0700

    util: add `TempDir` to create temporary directories
    In order to add a common working directory, add a `TempDir` utility
    which can collect a common set of temporary files.
    
    Signed-off-by: Adin Scannell <amscanne@meta.com>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4090, branch: user/amscanne/extern_objects/17

--- src/util/temp.h
+++ src/util/temp.h
@@ -14,6 +14,10 @@ public:
       : origin_(std::move(origin)), err_(err) {};
   static char ID;
   void log(llvm::raw_ostream &OS) const override;
+  int err() const
+  {
+    return err_;
+  }
 
 private:
   std::string origin_;
@@ -23,9 +27,11 @@ private:
 // TempFile provide a convenient RAII wrapper for temporary files. The use of
 // temporary files should be generally discouraged, unless they are necessary
 // to interacting with other libraries or tools.
+//
+// Using `pattern = false` should only be done from `TempDir`.
 class TempFile {
 public:
-  static Result<TempFile> create(std::string pattern = "");
+  static Result<TempFile> create(std::string name = "", bool pattern = true);
   ~TempFile();
 
   TempFile(const TempFile &other) = delete;
@@ -58,6 +64,45 @@ private:
 
   std::filesystem::path path_;
   int fd_;
+};
+
+// TempDir provides a wrapper for temporary directories.
+//
+// There is no way to create directories with a fixed name.
+class TempDir {
+public:
+  static Result<TempDir> create(std::string pattern = "");
+  ~TempDir();
+
+  TempDir(const TempDir &other) = delete;
+  TempDir &operator=(const TempDir &other) = delete;
+
+  TempDir(TempDir &&other)
+  {
+    path_ = std::move(other.path_);
+  }
+  TempDir &operator=(TempDir &&other)
+  {
+    path_ = std::move(other.path_);
+    return *this;
+  }
+
+  const std::filesystem::path &path()
+  {
+    return path_;
+  }
+
+  // Creates a temporary file in this directory. Note that `name` need not
+  // contain any `X` characters if `pattern` is true, as these will be appended
+  // as a suffix. The `name` provided may hae path components, but this
+  // function will do not anything with respect to creating intermediate
+  // directories.
+  Result<TempFile> create_file(std::string name = "", bool pattern = true);
+
+private:
+  TempDir(std::filesystem::path &&path) : path_(std::move(path)) {};
+
+  std::filesystem::path path_;
 };
 
 } // namespace bpftrace::util
