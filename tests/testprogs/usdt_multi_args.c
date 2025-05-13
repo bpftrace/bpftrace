c1e8167156302f796d4f08807c5783f806d76b62
gray <greyschwinger@gmail.com>
2025-05-13 20:08:34 +0800

    test: Add usdt_multi_args to test arg0-11
    Signed-off-by: gray <greyschwinger@gmail.com>

---
+++ tests/testprogs/usdt_multi_args.c
@@ -0,0 +1,29 @@
+#ifdef HAVE_SYSTEMTAP_SYS_SDT_H
+#include <sys/sdt.h>
+#else
+#define DTRACE_PROBE1(a, b, c) (void)0
+#endif
+#include <stdint.h>
+
+int main()
+{
+  uint32_t a = 0xdeadbeef;
+  uint32_t b = 1;
+  uint64_t c = UINT64_MAX;
+  uint32_t d = 0xcafebabe;
+  uint32_t e = 0x8badf00d;
+  uint32_t f = 0xfeedface;
+  uint64_t g = 0x0123456789abcdefULL;
+  uint64_t h = 0x0ULL;
+  uint64_t i = 0x7fffffffffffffffULL;
+  uint64_t j = 0x5555555555555555ULL;
+  uint32_t k = 42;
+  uint64_t l = 0xaaaaaaaaaaaaaaaaULL;
+
+  while (1) {
+    DTRACE_PROBE12(usdt_multi_args, probe1, a, b, c, d, e, f, g, h, i, j, k, l);
+  }
+
+  return 0;
+}
+
