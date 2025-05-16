828ba67a006f04358ce651cf3f961f8d2801a50b
Daniel Xu <dxu@dxuuu.xyz>
2025-05-16 12:28:33 -0600

    Parse implicit module BTF
    If a kprobe did not have an explicit module in the attachpoint, we would
    fail to parse that module's BTF while at the same point allowing the
    attachment. This leads to user confusion, as they expect types defined
    in that module to be available.
    
    For example:
    
        kprobe:tun_net_xmit {
            $dev = (struct net_device *)arg1;
            $tun = (struct tun_struct *)$dev->priv;
        }
    does not parse b/c `struct tun_struct` is unrecognized. However, the
    same probe using fentry worked just fine:
    
        fentry:tun_net_xmit {
            $tun = (struct tun_struct *)args->dev->priv;
            print($tun->numqueues);
        }
    
    That was b/c fentry/fexit attachpoint parsing normalized the attachpoint
    and always inserted module information.
    
    Ideally we'd fix kprobe by doing the same thing in attachpoint parser,
    but that ends up being somewhat tricky as a bunch of code expects there
    to not be a module name for the default vmlinux module. It is also backwards
    incompatible in a risky way, as `probe` builtin will start returning a
    different string.
    
    So fix by teach BPFtrace::list_modules() to always try to extract module
    name for any kprobe/kretprobe function. Note we delete the calls to
    get_raw_tracepoint_modules() and get_func_modules(). Those are
    extraneous - the probe matcher already takes care of that work for us
    when returning us the attachpoint with the modules baked in.
    
    Signed-off-by: Daniel Xu <dxu@dxuuu.xyz>

--- tests/bpftrace.cpp
+++ tests/bpftrace.cpp
@@ -21,6 +21,7 @@ namespace bpftrace::test::bpftrace {
 #include "btf_common.h"
 
 using ::testing::ContainerEq;
+using ::testing::Contains;
 using ::testing::StrictMock;
 
 static const int STRING_SIZE = 64;
@@ -1303,6 +1304,42 @@ TEST(bpftrace, resolve_timestamp)
   // Now check that we handle rollover to a new second correctly
   bpftrace->resources.strftime_args.emplace_back("%s.%f");
   EXPECT_EQ(bpftrace->resolve_timestamp(bootmode, 2, 15), "1736725827.000000");
+}
+
+static std::set<std::string> list_modules(std::string_view ap)
+{
+  ast::ASTContext ast("stdin", ap.data());
+  auto bpftrace = get_mock_bpftrace();
+
+  auto ok = ast::PassManager()
+                .put(ast)
+                .put(static_cast<BPFtrace &>(*bpftrace))
+                .add(CreateParsePass())
+                .add(ast::CreateParseAttachpointsPass())
+                .run();
+  EXPECT_TRUE(ok && ast.diagnostics().ok());
+
+  return bpftrace->list_modules(ast);
+}
+
+// Test modules are extracted when module is not explicit in attachpoint
+TEST(bpftrace, list_modules_kprobe_implicit)
+{
+  auto modules = list_modules("k:queued_spin_lock_slowpath{},kr:func_in_mod{}");
+  EXPECT_EQ(modules.size(), 3);
+  EXPECT_THAT(modules, Contains("vmlinux"));
+  EXPECT_THAT(modules, Contains("kernel_mod"));
+  EXPECT_THAT(modules, Contains("other_kernel_mod"));
+}
+
+// Inverse of above
+TEST(bpftrace, list_modules_kprobe_explicit)
+{
+  auto modules = list_modules(
+      "k:vmlinux:queued_spin_lock_slowpath{},kr:kernel_mod:func_in_mod{}");
+  EXPECT_EQ(modules.size(), 2);
+  EXPECT_THAT(modules, Contains("vmlinux"));
+  EXPECT_THAT(modules, Contains("kernel_mod"));
 }
 
 } // namespace bpftrace::test::bpftrace
