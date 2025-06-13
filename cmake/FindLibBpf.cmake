1b0570bd7164e4af261d84d5a11e4bd17be1e62b
Adin Scannell <amscanne@meta.com>
2025-06-13 11:22:49 -0700

    cmake: link against static `libbpf`
    See #4151 for more detail.
    
    Signed-off-by: Adin Scannell <amscanne@meta.com>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4264, branch: user/amscanne/static_bpf/1

--- cmake/FindLibBpf.cmake
+++ cmake/FindLibBpf.cmake
@@ -1,16 +1,9 @@
-# - Try to find libbpf
-# Once done this will define
-#
 #  LIBBPF_FOUND - system has libbpf
 #  LIBBPF_INCLUDE_DIRS - the libbpf include directory
 #  LIBBPF_LIBRARIES - Link these to use libbpf
 #  LIBBPF_DEFINITIONS - Compiler switches required for using libbpf
 #  LIBBPF_VERSION_MAJOR - Libbpf major version
 #  LIBBPF_VERSION_MINOR - Libbpf minor version
-
-#if (LIBBPF_LIBRARIES AND LIBBPF_INCLUDE_DIRS)
-#  set (LibBpf_FIND_QUIETLY TRUE)
-#endif (LIBBPF_LIBRARIES AND LIBBPF_INCLUDE_DIRS)
 
 find_path (LIBBPF_INCLUDE_DIRS
   NAMES
@@ -20,9 +13,11 @@ find_path (LIBBPF_INCLUDE_DIRS
   PATHS
     ENV CPATH)
 
+# We require that `libbpf` be linked statically, as we will implicitly vendor
+# the header into embedded code. See #4151 for more information.
 find_library (LIBBPF_LIBRARIES
   NAMES
-    bpf
+    libbpf.a
   PATHS
     ENV LIBRARY_PATH
     ENV LD_LIBRARY_PATH)
@@ -30,7 +25,7 @@ set(LIBBPF_ERROR_MESSAGE "Please install the libbpf development package")
 
 include (FindPackageHandleStandardArgs)
 
-# handle the QUIETLY and REQUIRED arguments and set LIBBPF_FOUND to TRUE if all listed variables are TRUE
+# handle the QUIETLY and REQUIRED arguments and set LIBBPF_FOUND to TRUE if all listed variables are TRUE.
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibBpf ${LIBBPF_ERROR_MESSAGE}
   LIBBPF_LIBRARIES
   LIBBPF_INCLUDE_DIRS)
