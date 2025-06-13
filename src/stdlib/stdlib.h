dd68598b4670f0d4ee22715307eb99a376b551a9
Adin Scannell <amscanne@meta.com>
2025-06-13 13:48:43 -0700

    cmake: Build the `stdlib` dynamically
    This does not change the external structure, but generates the standard
    library dynamically based on all available files. It also includes
    standardized headers from `libbpf`.
    
    This change also shifts to using `string_view` to allow us to reference
    the objects embedded in binary data without needing new allocations.
    
    Signed-off-by: Adin Scannell <amscanne@meta.com>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4265, branch: user/amscanne/static_bpf/2

--- src/stdlib/stdlib.h
+++ src/stdlib/stdlib.h
@@ -8,7 +8,9 @@ namespace bpftrace::stdlib {
 class Stdlib {
 public:
   // files is the set of files embedded in the standard library.
-  static const std::map<std::string, std::string> files;
+  //
+  // This is constructed automatically from a generated `stdlib.cpp`.
+  static const std::map<std::string, std::string_view> files;
 };
 
 } // namespace bpftrace::stdlib
