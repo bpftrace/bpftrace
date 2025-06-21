d3e7b9865f17508b627a9ce3d773ec47fa7eda66
Adin Scannell <adin@scannell.ca>
2025-06-21 13:44:47 +0000

    async: refactor handles into a stateful class
    In the future, this will allow that class to load information about
    external types, and dispatch appropriately.
    
    Signed-off-by: Adin Scannell <adin@scannell.ca>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4297, branch: user/amscanne/btf_types3/1

--- src/async_action.h
+++ src/async_action.h
@@ -28,29 +28,31 @@ enum class AsyncAction {
   // clang-format on
 };
 
-const static size_t MAX_TIME_STR_LEN = 64;
-void exit_handler(BPFtrace &bpftrace, const void *data);
-void join_handler(BPFtrace &bpftrace, Output &out, const void *data);
-void time_handler(BPFtrace &bpftrace, Output &out, const void *data);
-void helper_error_handler(BPFtrace &bpftrace, Output &out, const void *data);
-void print_non_map_handler(BPFtrace &bpftrace, Output &out, const void *data);
-void print_map_handler(BPFtrace &bpftrace, Output &out, const void *data);
-void zero_map_handler(BPFtrace &bpftrace, const void *data);
-void clear_map_handler(BPFtrace &bpftrace, const void *data);
-void watchpoint_attach_handler(BPFtrace &bpftrace, const void *data);
-void watchpoint_detach_handler(BPFtrace &bpftrace, const void *data);
-void skboutput_handler(BPFtrace &bpftrace, void *data, int size);
-void syscall_handler(BPFtrace &bpftrace,
-                     Output &out,
-                     AsyncAction printf_id,
-                     uint8_t *arg_data);
-void cat_handler(BPFtrace &bpftrace,
-                 Output &out,
-                 AsyncAction printf_id,
-                 uint8_t *arg_data);
-void printf_handler(BPFtrace &bpftrace,
-                    Output &out,
-                    AsyncAction printf_id,
-                    uint8_t *arg_data);
+class AsyncHandlers {
+public:
+  const static size_t MAX_TIME_STR_LEN = 64;
+
+  AsyncHandlers(BPFtrace &bpftrace, Output &output)
+      : bpftrace(bpftrace), out(output) {};
+
+  void exit(const void *data);
+  void join(const void *data);
+  void time(const void *data);
+  void helper_error(const void *data);
+  void print_non_map(const void *data);
+  void print_map(const void *data);
+  void zero_map(const void *data);
+  void clear_map(const void *data);
+  void watchpoint_attach(const void *data);
+  void watchpoint_detach(const void *data);
+  void skboutput(void *data, int size);
+  void syscall(AsyncAction printf_id, uint8_t *arg_data);
+  void cat(AsyncAction printf_id, uint8_t *arg_data);
+  void printf(AsyncAction printf_id, uint8_t *arg_data);
+
+private:
+  BPFtrace &bpftrace;
+  Output &out;
+};
 
 } // namespace bpftrace::async_action
