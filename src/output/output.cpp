619262ce41955146978d973ca30c47d68140e23c
Adin Scannell <amscanne@meta.com>
2025-06-23 10:49:02 -0700

    output: add output APIs and libraries
    In order to implement extensible types, it is necessary to decouple the
    output from the definition of internal types. This change adds a layer
    of abstraction for output: there are high-level primitives for types to
    be printed, and high-level operations associated with the current set of
    "MessageTypes" for an abstract output.
    
    The core idea is that in the future, an extensible type can be asked for
    its representation in terms of the `output::Value` rather than having
    the output driver need to understand all of the different internal type
    representations. This provides a clear separation of concerns, and
    should allow us to build new types, functions, etc. without having to
    make changes to all the various output drivers.
    
    This specific change contains only the implementation of those two
    libraries: while it should build, nothing will use this yet. The
    subsequent change will effectively be a switch over to using these
    output drivers instead of the existing one.
    
    Signed-off-by: Adin Scannell <amscanne@meta.com>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4301, branch: user/amscanne/btf_types3/5

---
+++ src/output/output.cpp
@@ -0,0 +1,43 @@
+#include "output/output.h"
+#include "output/text.h"
+
+namespace bpftrace::output {
+
+std::partial_ordering Primitive::Array::operator<=>(const Array &other) const
+{
+  return values <=> other.values;
+}
+
+bool Primitive::Array::operator==(const Array &other) const
+{
+  return values == other.values;
+}
+
+std::partial_ordering Primitive::Tuple::operator<=>(const Tuple &other) const
+{
+  return values <=> other.values;
+}
+
+bool Primitive::Tuple::operator==(const Tuple &other) const
+{
+  return values == other.values;
+}
+
+std::partial_ordering Primitive::Record::operator<=>(const Record &other) const
+{
+  return fields <=> other.fields;
+}
+
+bool Primitive::Record::operator==(const Record &other) const
+{
+  return fields == other.fields;
+}
+
+std::ostream &operator<<(std::ostream &out, const Primitive &p)
+{
+  TextOutput output(out);
+  output.primitive(p);
+  return out;
+}
+
+} // namespace bpftrace::output
