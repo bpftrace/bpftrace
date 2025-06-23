f2df02a2254fae9e36e692ab3ba566cadb2a2ef2
Adin Scannell <amscanne@meta.com>
2025-06-23 11:39:49 -0700

    output: change to using generic output drivers
    The previous change introduced the `output` library, which included a
    generic type schema and drivers that were capable of displaying that
    schema is a way that aligns with the current output.
    
    This change removes the current output classes & output generation, and
    replaces it with a version that constructs objects from the output
    library and then calls the relevant methods. This forces a separation of
    concerns and modularity for the output drivers.
    
    Note that this change is rather large, and difficult to break up. There
    are a few pieces that are entangled:
    
    * The existing `output.cpp` file, which understands `SizedType` is
      converted into a `types_format.cpp` file, which also understands
      `SizedType` and how to unpack data into an "outputable"
      representation. Some of the `print_maps` methods are collected from
      `BPFtrace` in order to separate concerns more effectively.
    
    * The paths for creating outputs, which were duplicated in the AOT and
      regular cases are aggregated into `run_bpftrace`.
    
    * The async actions are updated to call the `format` method which
      constructs the object objects. These are then passed to the associated
      virtual methods, which generate the required messages.
    
    * A very small number of tests are updated, only to make the output
      consistent where capturing the inconsistencies would increase
      complexities. In general, great effort was taken to support the
      existing schema and format wherever possible.
    
    Signed-off-by: Adin Scannell <amscanne@meta.com>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4302, branch: user/amscanne/btf_types3/6

--- src/run_bpftrace.cpp
+++ src/run_bpftrace.cpp
@@ -1,7 +1,11 @@
 #include <csignal>
+#include <fstream>
 
 #include "log.h"
+#include "output/json.h"
+#include "output/text.h"
 #include "run_bpftrace.h"
+#include "types_format.h"
 
 using namespace bpftrace;
 
@@ -35,12 +39,22 @@ void check_is_root()
 }
 
 int run_bpftrace(BPFtrace &bpftrace,
-                 Output &output,
+                 const std::string &output_file,
+                 const std::string &output_format,
+                 const ast::CDefinitions &c_definitions,
                  BpfBytecode &bytecode,
                  std::vector<std::string> &&named_params)
 {
   int err;
 
+  // Check for required features.
+  if (!bpftrace.feature_->has_map_ringbuf()) {
+    LOG(ERROR) << "Your kernel is too old and is missing the "
+                  "BPF_MAP_TYPE_RINGBUF, which bpftrace requires.";
+    return 1;
+  }
+
+  // Process all arguments.
   auto named_param_vals = bpftrace.resources.global_vars.get_named_param_vals(
       named_params);
   if (!named_param_vals) {
@@ -57,8 +71,30 @@ int run_bpftrace(BPFtrace &bpftrace,
     }
     return 1;
   }
-
   bytecode.update_global_vars(bpftrace, std::move(*named_param_vals));
+
+  // Create our output.
+  std::ostream *os = &std::cout;
+  std::ofstream outputstream;
+  if (!output_file.empty()) {
+    outputstream.open(output_file);
+    if (outputstream.fail()) {
+      LOG(ERROR) << "Failed to open output file: \"" << output_file
+                 << "\": " << strerror(errno);
+      return 1;
+    }
+    os = &outputstream;
+  }
+  std::unique_ptr<output::Output> output;
+  if (output_format.empty() || output_format == "text") {
+    output = std::make_unique<output::TextOutput>(*os);
+  } else if (output_format == "json") {
+    output = std::make_unique<output::JsonOutput>(*os);
+  } else {
+    LOG(ERROR) << "Invalid output format \"" << output_format << "\"\n"
+               << "Valid formats: 'text', 'json'";
+    return 1;
+  }
 
   // Signal handler that lets us know an exit signal was received.
   struct sigaction act = {};
@@ -70,21 +106,35 @@ int run_bpftrace(BPFtrace &bpftrace,
   act.sa_handler = [](int) { BPFtrace::sigusr1_recv = true; };
   sigaction(SIGUSR1, &act, nullptr);
 
-  err = bpftrace.run(output, std::move(bytecode));
+  err = bpftrace.run(*output, c_definitions, std::move(bytecode));
   if (err)
     return err;
+
+  // Indicate that we are done the main loop.
+  output->end();
 
   // We are now post-processing. If we receive another SIGINT,
   // handle it normally (exit)
   act.sa_handler = SIG_DFL;
   sigaction(SIGINT, &act, nullptr);
 
-  std::cout << "\n\n";
-
-  if (bpftrace.run_benchmarks_) {
-    bpftrace.print_benchmark_results(output);
-  } else if (bpftrace.config_->print_maps_on_exit) {
-    err = bpftrace.print_maps(output);
+  // Print maps if needed (true by default).
+  if (!dry_run) {
+    if (bpftrace.run_benchmarks_) {
+      output->benchmark_results(bpftrace.benchmark_results);
+    }
+    if (bpftrace.config_->print_maps_on_exit) {
+      for (const auto &[_, map] : bpftrace.bytecode_.maps()) {
+        if (!map.is_printable())
+          continue;
+        auto res = format(bpftrace, c_definitions, map);
+        if (!res) {
+          std::cerr << "Error printing map: " << res.takeError();
+          continue;
+        }
+        output->map(map.name(), *res);
+      }
+    }
   }
 
   if (bpftrace.child_) {
