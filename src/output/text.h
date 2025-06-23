d8cf03b6eaed8edc50085d97570c840eb8bebc83
Adin Scannell <amscanne@meta.com>
2025-06-23 10:49:02 -0700

    output: add output APIs and libraries
    In order to implement extensible types, it is necessary to decouple the
    output from the definition of internal types. This change adds a layer
    of abstraction for output: there are high-level primitives for types to
    be printed, and high-level operations associated with the current set of
    "MessageTypes" for an abstract output.
    
    The core idea is that in the future, an extensible type can be asked for
    its representation in terms of the `output::Value` rather than having
    the output driver need to understand all of the different internal type
    representations. This provides a clear separation of concerns, and
    should allow us to build new types, functions, etc. without having to
    make changes to all the various output drivers.
    
    This specific change contains only the implementation of those two
    libraries: while it should build, nothing will use this yet. The
    subsequent change will effectively be a switch over to using these
    output drivers instead of the existing one.
    
    Signed-off-by: Adin Scannell <amscanne@meta.com>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4301, branch: user/amscanne/btf_types3/5

---
+++ src/output/text.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <iostream>
+
+#include "output/output.h"
+
+namespace bpftrace::output {
+
+class TextOutput : public Output {
+public:
+  explicit TextOutput(std::ostream &out = std::cout) : out_(out) {};
+
+  void map(const std::string &name, const Value &value) override;
+  void value(const Value &value) override;
+  void printf(const std::string &str) override;
+  void time(const std::string &time) override;
+  void cat(const std::string &cat) override;
+  void join(const std::string &join) override;
+  void syscall(const std::string &syscall) override;
+  void lost_events(uint64_t lost) override;
+  void attached_probes(uint64_t num_probes) override;
+  void helper_error(int retcode, const HelperErrorInfo &info) override;
+  void benchmark_results(
+      const std::vector<std::pair<std::string, uint32_t>> &results) override;
+  void end() override;
+
+  // Allows formatting of a specific primitive.
+  void primitive(const Primitive &p);
+
+private:
+  std::ostream &out_;
+};
+
+} // namespace bpftrace::output
