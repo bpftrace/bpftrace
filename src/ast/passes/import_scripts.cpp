07562d6b9a83594ba9a2f568b46c3b701c4395b5
Adin Scannell <amscanne@meta.com>
2025-07-27 12:50:09 -0700

    imports: split internal and external script resolution
    By spilling out internal (embedded) and external scripts are used, and
    placing the unstable feature pass in the middle, we effectively exempt
    the internal scripts from unstable warnings. This allows us to use
    "unstable" features internally without exposing this to any user script.
    
    Signed-off-by: Adin Scannell <amscanne@meta.com>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4399, branch: user/amscanne/helpers/8

--- src/ast/passes/import_scripts.cpp
+++ src/ast/passes/import_scripts.cpp
@@ -4,33 +4,53 @@
 
 namespace bpftrace::ast {
 
-Pass CreateImportScriptsPass()
+static void import_ast(ASTContext &ast, const ASTContext &other)
 {
-  return Pass::create("ImportScripts", [](ASTContext &ast, Imports &imports) {
-    // Clone all map declarations, subfunctions, etc. into the primary AST.
-    // Note that we may choose not to inline all definitions in the future, and
-    // define that namespace-based resolution is used for e.g. macro expansion,
-    // function matching, etc. But for now, we just support a trivial
-    // expansion.
-    for (const auto &[name, other] : imports.scripts) {
-      ast.diagnostics().add(std::move(other.diagnostics()));
-      if (other.root) {
-        ast.root->c_definitions += other.root->c_definitions;
-        for (const auto &decl : other.root->map_decls) {
-          ast.root->map_decls.push_back(clone(ast, decl));
-        }
-        for (const auto &fn : other.root->functions) {
-          ast.root->functions.push_back(clone(ast, fn));
-        }
-        for (const auto &macro : other.root->macros) {
-          ast.root->macros.push_back(clone(ast, macro));
-        }
-        for (const auto &probe : other.root->probes) {
-          ast.root->probes.push_back(clone(ast, probe));
-        }
-      }
+  // Clone all map declarations, subfunctions, etc. into the primary AST.
+  // Note that we may choose not to inline all definitions in the future, and
+  // define that namespace-based resolution is used for e.g. macro expansion,
+  // function matching, etc. But for now, we just support a trivial
+  // expansion.
+  ast.diagnostics().add(std::move(other.diagnostics()));
+  if (other.root) {
+    ast.root->c_definitions += other.root->c_definitions;
+    for (const auto &decl : other.root->map_decls) {
+      ast.root->map_decls.push_back(clone(ast, decl));
     }
-  });
+    for (const auto &fn : other.root->functions) {
+      ast.root->functions.push_back(clone(ast, fn));
+    }
+    for (const auto &macro : other.root->macros) {
+      ast.root->macros.push_back(clone(ast, macro));
+    }
+    for (const auto &probe : other.root->probes) {
+      ast.root->probes.push_back(clone(ast, probe));
+    }
+  }
+}
+
+Pass CreateImportExternalScriptsPass()
+{
+  return Pass::create("ImportExternalScripts",
+                      [](ASTContext &ast, Imports &imports) {
+                        for (const auto &[name, obj] : imports.scripts) {
+                          if (!obj.internal) {
+                            import_ast(ast, obj.ast);
+                          }
+                        }
+                      });
+}
+
+Pass CreateImportInternalScriptsPass()
+{
+  return Pass::create("ImportInternalScripts",
+                      [](ASTContext &ast, Imports &imports) {
+                        for (const auto &[name, obj] : imports.scripts) {
+                          if (obj.internal) {
+                            import_ast(ast, obj.ast);
+                          }
+                        }
+                      });
 }
 
 } // namespace bpftrace::ast
