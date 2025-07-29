c03961fe1e283f2854f6971969eae5c10a09bf7b
Jordan Rome <linux@jordanrome.com>
2025-07-29 15:23:12 -0700

    Add location expansion for RuntimeErrorInfo
    This is so runtime errors, helper errors,
    verifier errors, and, the soon to be, `errorf`
    calls show the call/macro chain.
    
    Example:
    ```
    bpftrace -e 'macro bad_div(@a) { print(10 / (uint64)@a); } BEGIN { @m0 = 0; bad_div(@m0); }'
    Attached 1 probe
    stdin:1:30-31: WARNING: Divide or modulo by 0 detected. This can lead to unexpected results. 1 is being used as the result.
    macro bad_div(@a) { print(10 / (uint64)@a); } BEGIN { @m0 = 0; bad_div(@m0); }
                                 ~
    stdin:1:64-76: WARNING: expanded from
    macro bad_div(@a) { print(10 / (uint64)@a); } BEGIN { @m0 = 0; bad_div(@m0); }
                                                                   ~~~~~~~~~~~~
    ```
    
    Signed-off-by: Jordan Rome <linux@jordanrome.com>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4413, branch: user/jordalgo/errorf/1
    Signed-off-by: Jordan Rome <linux@jordanrome.com>

--- src/output/json.cpp
+++ src/output/json.cpp
@@ -459,12 +459,13 @@ void JsonOutput::runtime_error(int retcode, const RuntimeErrorInfo &info)
     }
   }
 
+  // Json only prints the top level location
   out_ << R"(, "filename": )";
-  JsonEmitter<std::string>::emit(out_, info.filename);
+  JsonEmitter<std::string>::emit(out_, info.locations.begin()->filename);
   out_ << R"(, "line": )";
-  JsonEmitter<uint64_t>::emit(out_, info.line);
+  JsonEmitter<uint64_t>::emit(out_, info.locations.begin()->line);
   out_ << R"(, "col": )";
-  JsonEmitter<uint64_t>::emit(out_, info.column);
+  JsonEmitter<uint64_t>::emit(out_, info.locations.begin()->column);
   out_ << R"(})" << std::endl;
 }
 
