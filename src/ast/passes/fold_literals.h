38d74abb5f65a633dff169d41193309207d21051
Adin Scannell <amscanne@meta.com>
2025-07-29 17:49:44 -0700

    codegen: inline `sizeof` and `offsetof`.
    Signed-off-by: Adin Scannell <amscanne@meta.com>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4417, branch: user/amscanne/typeof/2

--- src/ast/passes/fold_literals.h
+++ src/ast/passes/fold_literals.h
@@ -1,9 +1,18 @@
 #pragma once
 
+#include "ast/ast.h"
 #include "ast/pass_manager.h"
 
 namespace bpftrace::ast {
 
+// Folds only a single expression.
+//
+// This may be used by subsequent passes when expressions are replaced by
+// literals. Note however, that it is up to the pass to ensure that the full
+// expression is recursively folded.
+void fold(ASTContext &ast, Expression &expr);
+
+// Fold all nodes.
 Pass CreateFoldLiteralsPass();
 
 } // namespace bpftrace::ast
