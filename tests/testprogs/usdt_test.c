5e49515b608ee9d2bd312e2396c2ecc7bd001636
Jordan Rome <linux@jordanrome.com>
2025-07-30 11:22:19 -0700

    Re-enable USDT arg tests
    These were broken because the strings weren't market
    as volatile.
    
    The `volatile` qualifier ensures that:
    
    *   The strings remain in memory at a stable address
    *   The compiler doesn't merge identical strings (which would break tracing)
    *   The strings aren't moved to read-only memory sections
    
    Signed-off-by: Jordan Rome <linux@jordanrome.com>

--- tests/testprogs/usdt_test.c
+++ tests/testprogs/usdt_test.c
@@ -8,9 +8,12 @@ static long myclock()
 {
   struct timeval tv;
   gettimeofday(&tv, NULL);
-  DTRACE_PROBE2(tracetest, testprobe, tv.tv_sec, "Hello world");
-  DTRACE_PROBE2(tracetest, testprobe2, tv.tv_sec, "Hello world2");
-  DTRACE_PROBE2(tracetest2, testprobe2, tv.tv_sec, "Hello world3");
+  static volatile char str1[] = "Hello World1";
+  static volatile char str2[] = "Hello World2";
+  static volatile char str3[] = "Hello World3";
+  DTRACE_PROBE2(tracetest, testprobe, tv.tv_sec, str1);
+  DTRACE_PROBE2(tracetest, testprobe2, tv.tv_sec, str2);
+  DTRACE_PROBE2(tracetest2, testprobe2, tv.tv_sec, str3);
   return tv.tv_sec;
 }
 
@@ -18,6 +21,8 @@ int main()
 {
   while (1) {
     myclock();
+    // Sleep is necessary to not overflow perf buffer
+    usleep(1000);
   }
   return 0;
 }
