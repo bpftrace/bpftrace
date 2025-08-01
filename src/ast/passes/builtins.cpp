59b2c8f09ae4371c7ece41732538445e767ebbd0
Adin Scannell <amscanne@meta.com>
2025-07-31 22:45:43 -0700

    passes: add new builtins pass
    This pass materializes fixed builtins. It is used as a replacement for
    the builtins in the fold literals pass.
    
    As an initial example of how this pass works, add builtins for the arch
    and whether bpftrace is being invoked in safe_mode.
    
    Signed-off-by: Adin Scannell <amscanne@meta.com>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4457, branch: user/amscanne/typeof/4

---
+++ src/ast/passes/builtins.cpp
@@ -0,0 +1,79 @@
+#include <optional>
+
+#include "arch/arch.h"
+#include "ast/passes/builtins.h"
+#include "ast/visitor.h"
+#include "bpftrace.h"
+
+namespace bpftrace::ast {
+
+namespace {
+
+class Builtins : public Visitor<Builtins, std::optional<Expression>> {
+public:
+  explicit Builtins(ASTContext &ast, BPFtrace &bpftrace)
+      : ast_(ast), bpftrace_(bpftrace) {};
+
+  using Visitor<Builtins, std::optional<Expression>>::visit;
+  std::optional<Expression> visit(Builtin &builtin);
+  std::optional<Expression> visit(Identifier &identifier);
+  std::optional<Expression> visit(Expression &expression);
+  std::optional<Expression> check(const std::string &ident, Node &node);
+
+private:
+  ASTContext &ast_;
+  BPFtrace &bpftrace_;
+};
+
+} // namespace
+
+std::optional<Expression> Builtins::check(const std::string &ident, Node &node)
+{
+  // N.B. this pass *should* include all the compile-time builtins (probe,
+  // provider, etc.) but it presently cannot due to the expansion rules. All
+  // builtins should be added here once probes are fully-expanded up front.
+  //
+  // All of these builtins should be directly evaluated and folded and not
+  // associated with any code generation. These builtins should be kept to the
+  // minimum possible set to support the standard library.
+  if (ident == "__builtin_arch") {
+    std::stringstream ss;
+    ss << bpftrace::arch::current();
+    return ast_.make_node<String>(ss.str(), Location(node.loc));
+  }
+  if (ident == "__builtin_safe_mode") {
+    return ast_.make_node<Boolean>(bpftrace_.safe_mode_, Location(node.loc));
+  }
+  return std::nullopt;
+}
+
+std::optional<Expression> Builtins::visit(Builtin &builtin)
+{
+  return check(builtin.ident, builtin);
+}
+
+std::optional<Expression> Builtins::visit(Identifier &identifier)
+{
+  return check(identifier.ident, identifier);
+}
+
+std::optional<Expression> Builtins::visit(Expression &expression)
+{
+  auto replacement = visit(expression.value);
+  if (replacement) {
+    expression.value = replacement->value;
+  }
+  return std::nullopt;
+}
+
+Pass CreateBuiltinsPass()
+{
+  auto fn = [&](ASTContext &ast, BPFtrace &bpftrace) {
+    Builtins builtins(ast, bpftrace);
+    builtins.visit(ast.root);
+  };
+
+  return Pass::create("Builtins", fn);
+};
+
+} // namespace bpftrace::ast
