734fe3a88075f7370df4d1fbd408f7666bd2eb10
Baolin Zhu <bowlingzhu@tencent.com>
2025-08-01 12:03:26 +0800

    build: try add llvmPackages.libc dependency
    Signed-off-by: Baolin Zhu <bowlingzhu@tencent.com>

--- flake.nix
+++ flake.nix
@@ -176,6 +176,7 @@
                   pkgs.libsystemtap
                   pkgs."llvmPackages_${toString llvmVersion}".libclang
                   pkgs."llvmPackages_${toString llvmVersion}".llvm
+                  pkgs."llvmPackages_${toString llvmVersion}".clang-unwrapped # for Clang C Standard Library
                   pkgs.pahole
                   pkgs.xxd
                   pkgs.zlib
@@ -207,6 +208,7 @@
                   bpftools
                   coreutils
                   pkgs."llvmPackages_${toString llvmVersion}".clang-tools # Needed for the nix-aware "wrapped" clang-tidy
+                  pkgs."llvmPackages_${toString llvmVersion}".clang-unwrapped # for Clang C Standard Library
                   gawk
                   git
                   gnugrep
