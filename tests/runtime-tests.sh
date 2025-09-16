329e3b2c88812ae11f95fc34dbe132e839a2f4e6
Jordan Rome <linux@jordanrome.com>
2025-09-16 14:47:19 -0700

    Remove runtime test target
    Running `sudo make runtime_tests` causes issues
    with the built files so just remove this target
    and update the readme.
    
    This also updates the runtime-tests.sh when run
    on it's own.
    
    Signed-off-by: Jordan Rome <linux@jordanrome.com>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4603, branch: user/jordalgo/uprobe_multi_comp/2
    Signed-off-by: Jordan Rome <linux@jordanrome.com>

--- tests/runtime-tests.sh
+++ tests/runtime-tests.sh
@@ -7,14 +7,15 @@ fi
 
 set -e;
 
-pushd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
-
-BPFTRACE_RUNTIME_TEST_EXECUTABLE=${BPFTRACE_RUNTIME_TEST_EXECUTABLE:-@CMAKE_BINARY_DIR@/src/bpftrace};
+TESTS_DIR="$(dirname "${BASH_SOURCE[0]}")";
+DIR="$( cd $TESTS_DIR >/dev/null && pwd )"
+BPFTRACE_RUNTIME_TEST_EXECUTABLE=${BPFTRACE_EXECUTABLE:-$DIR/../src/bpftrace};
 export BPFTRACE_RUNTIME_TEST_EXECUTABLE;
 
 echo "===================="
 echo "bpftrace --info:"
 echo "===================="
-"${BPFTRACE_RUNTIME_TEST_EXECUTABLE}" --info
+"${BPFTRACE_RUNTIME_TEST_EXECUTABLE}" --info;
 
+pushd $TESTS_DIR >/dev/null 2>&1
 python3 -u runtime/engine/main.py "$@"
