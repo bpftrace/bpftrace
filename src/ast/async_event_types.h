0ab0351d491dae70bb85a51380fb3263edf78801
Adin Scannell <amscanne@meta.com>
2025-09-24 14:20:43 -0700

    opaque value: handle unaligned accesses
    Since the types being bitcast will generally be small, it is more
    efficient to simply return an instance of these types. This requires
    that they are trivially copyable (which will be the case already).
    
    Note that the `constexpr` if is not a premature optimization here; some
    copied types lack a default constructor but *are* copyable. We still
    require them to be trivially copyable (to avoid that set of footguns),
    but allows these for objects that don't have alignment constraints.
    
    Fixes #4644
    
    Signed-off-by: Adin Scannell <amscanne@meta.com>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4651, branch: user/amscanne/fix_alignment/1
    (cherry picked from commit 8aa87a051f6451ecb1cc83ec4bd9294ab2a3508b)
    Signed-off-by: Jordan Rome <linux@jordanrome.com>

--- src/ast/async_event_types.h
+++ src/ast/async_event_types.h
@@ -65,11 +65,6 @@ struct Strftime {
 
 struct Buf {
   uint32_t length;
-  // Seems like GCC 7.4.x can't handle `char content[]`. Work around by using
-  // 0 sized array (a GCC extension that clang also accepts:
-  // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=70932). It also looks like
-  // the issue doesn't exist in GCC 7.5.x.
-  char content[0];
 
   std::vector<llvm::Type*> asLLVMType(ast::IRBuilderBPF& b, uint32_t length);
 } __attribute__((packed));
