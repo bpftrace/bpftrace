a4cf745672680de14feefcf8d2e289c0e8a51803
Viktor Malik <viktor.malik@gmail.com>
2025-10-02 14:47:29 +0200

    Build libbpf from source by default
    Introduce a new CMake variable USE_SYSTEM_LIBBPF which allows to choose
    whether to use libbpf from the system or build it from the submodule
    introduced by the previous commit. By default, the variable is unset.
    
    The build is done in FindLibbpf.cmake by running libbpf's make. Libbpf
    is installed into the build directory which theoretically allows to have
    different versions of libbpf for different builds (should that be ever
    needed).
    
    There are a few specifics things that had to be done:
    - We need to install libbpf headers during CMake configure phase since
      we need them (1) to determine libbpf version in CMake and (2) to build
      stdlib which embeds the headers.
    - The build itself is done by a combination of add_custom_command,
      add_custom_target, and add_custom_library calls with some dependencies
      in between. I was not able to come up with a simpler combination of
      commands (see the comment in FindLibbpf.cmake for some details).
    
    Since we now potentially require a non-released version of libbpf,
    remove the libbpf version check from CMake. In addition, when system
    libbpf is used, issue a warning since we are not able to guarantee that
    the system version contains all the required patches.
    
    Signed-off-by: Viktor Malik <viktor.malik@gmail.com>

--- cmake/FindLibBpf.cmake
+++ cmake/FindLibBpf.cmake
@@ -5,23 +5,64 @@
 #  LIBBPF_VERSION_MAJOR - Libbpf major version
 #  LIBBPF_VERSION_MINOR - Libbpf minor version
 
-find_path (LIBBPF_INCLUDE_DIRS
-  NAMES
-    bpf/bpf.h
-    bpf/btf.h
-    bpf/libbpf.h
-  PATHS
-    ENV CPATH)
+if (USE_SYSTEM_LIBBPF)
+  find_path (LIBBPF_INCLUDE_DIRS
+    NAMES
+      bpf/bpf.h
+      bpf/btf.h
+      bpf/libbpf.h
+    PATHS
+      ENV CPATH)
 
-# We require that `libbpf` be linked statically, as we will implicitly vendor
-# the header into embedded code. See #4151 for more information.
-find_library (LIBBPF_LIBRARIES
-  NAMES
-    libbpf.a
-  PATHS
-    ENV LIBRARY_PATH
-    ENV LD_LIBRARY_PATH)
-set(LIBBPF_ERROR_MESSAGE "Please install the libbpf.a static library")
+  # We require that `libbpf` be linked statically, as we will implicitly vendor
+  # the header into embedded code. See #4151 for more information.
+  find_library (LIBBPF_LIBRARIES
+    NAMES
+      libbpf.a
+    PATHS
+      ENV LIBRARY_PATH
+      ENV LD_LIBRARY_PATH)
+  set(LIBBPF_ERROR_MESSAGE
+    "Please set -DUSE_SYSTEM_LIBBPF=Off to use the libbpf stored as a submodule
+    (preferred) or install the libbpf.a static library")
+else()
+  set(LIBBPF_SOURCE ${CMAKE_CURRENT_SOURCE_DIR}/libbpf/src)
+  set(LIBBPF_BUILD ${CMAKE_CURRENT_BINARY_DIR}/libbpf)
+  set(LIBBPF_LIBRARY ${LIBBPF_BUILD}/lib64/libbpf.a)
+
+  # We need to install headers during configure phase for two reasons:
+  # 1. To parse libbpf version from libbpf_version.h below.
+  # 2. For bpftrace's stdlib since libbpf headers are included in stdlib bpf.c
+  #    files (and the rules to embed them are generated in configure phase).
+  execute_process(
+    COMMAND make PREFIX=${LIBBPF_BUILD} install_headers
+    WORKING_DIRECTORY ${LIBBPF_SOURCE}
+    OUTPUT_QUIET)
+
+  # Build and install libbpf from source. This needs to be done in a rather
+  # complicated way to ensure proper dependency between the exported
+  # LIBBPF_LIBRARIES and the built libbpf.a. Note that we cannot use
+  # add_custom_target with COMMAND since CMake considers the target as always
+  # outdated, forcing rebuild of libbpf (and transitively of the entire bpftrace
+  # every time).
+  add_custom_command(
+    OUTPUT ${LIBBPF_LIBRARY}
+    # -fPIE here is needed by latest binutils when building with Clang
+    COMMAND make PREFIX=${LIBBPF_BUILD} EXTRA_CFLAGS="-fPIE" install > /dev/null
+    WORKING_DIRECTORY ${LIBBPF_SOURCE}
+    DEPENDS ${LIBBPF_SOURCE}/*
+    COMMENT "Building libbpf"
+    VERBATIM)
+  add_custom_target(libbpf_build DEPENDS ${LIBBPF_LIBRARY})
+  add_library(libbpf_static STATIC IMPORTED GLOBAL)
+  set_target_properties(libbpf_static PROPERTIES
+    IMPORTED_LOCATION ${LIBBPF_LIBRARY})
+  add_dependencies(libbpf_static libbpf_build)
+
+  # Set the output variables directly
+  set(LIBBPF_LIBRARIES libbpf_static)
+  set(LIBBPF_INCLUDE_DIRS ${LIBBPF_BUILD}/include)
+endif()
 
 include (FindPackageHandleStandardArgs)
 
