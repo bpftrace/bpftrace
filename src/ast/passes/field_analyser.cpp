bpftrace/bpftrace

commit 4e2f3c7934580dfd565468d797cdc6d023075f96

arch: ensure all code is built all the time
Each architecture is a specialization of the generic `Arch` class. The
only `#ifdef` is used for a `constexpr` function which returns the
current architecture, which can be used to instantiate (in a non-virtual
way) the intended archiecture helpers.

In general, I expect that all unused implementations will be stripped
out by the linker as they are unreferenced. However, we can easily
create a parameterized test that *does* use all these implementations to
provide a uniform sanity test.

Updates #4017

Signed-off-by: Adin Scannell <amscanne@meta.com>

stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4269, branch: user/amscanne/kill_arch/1

src/ast/passes/field_analyser.cpp
@@ -297,8 +297,8 @@ void FieldAnalyser::resolve_args(Probe &probe)
         } else {
           ap->addWarning() << "No debuginfo found for " << ap->target;
         }
-        if (probe_args && probe_args->fields.size() >
-                              static_cast<size_t>(arch::max_arg() + 1)) {
+        if (probe_args &&
+            probe_args->fields.size() >= arch::Host::arguments().size()) {
           ap->addError() << "\'args\' builtin is not supported for "
                          << "probes with stack-passed arguments.";
         }

src/btf.cpp
@@ -495,7 +495,7 @@ std::shared_ptr<Struct> BTF::resolve_args(std::string_view func,
 
   const struct btf_param *p = btf_params(t);
   __u16 vlen = btf_vlen(t);
-  if (vlen > arch::max_arg() + 1) {
+  if (vlen >= arch::Host::arguments().size()) {
     err = "functions with more than 6 parameters are "
           "not supported.";
     return nullptr;
@@ -574,7 +574,7 @@ std::string BTF::get_all_funcs_from_btf(const BTFObj &btf_obj) const
     if (bpftrace_ && !bpftrace_->is_traceable_func(func_name))
       continue;
 
-    if (btf_vlen(t) > arch::max_arg() + 1)
+    if (btf_vlen(t) >= arch::Host::arguments().size())
       continue;
 
     funcs += btf_obj.name + ":" + func_name + "\n";
@@ -615,7 +615,7 @@ std::string BTF::get_all_raw_tracepoints_from_btf(const BTFObj &btf_obj) const
       break;
     }
 
-    if (btf_vlen(t) > arch::max_arg() + 1)
+    if (btf_vlen(t) >= arch::Host::arguments().size())
       continue;
 
     bool found = false;

commit 12ac08eece25d314d23004a5b1c275e2f3997f73

Fix off-by-one for function argument size comparison
Commit 4e2f3c793458 ("arch: ensure all code is built all the time")
removed the arch::max_arg function and replaced its usage with
arch::Host::arguments().size().

max_arg defined the maximum argument number (so, 0-based), unlike
arch::Host::arguments().size(), which is the size of the array.

The comparisons against this value were thus converted from > to >=.
However, comparisons to max_arg() + 1 were also converted to >=,
which is incorrect - they can stay the same, as max_arg + 1 is
equal to arch::Host::arguments().size().

This lead to BTF functions with the maximum number of
register-passed arguments to be incorrectly rejected for use
in fentry probes.

Change these comparisons back. Also fix an adjacent warning
to not hardcode '6' (the x86 value) for the maximum number.

Fixes: 4e2f3c793458 ("arch: ensure all code is built all the time")
Signed-off-by: Frank van der Linden <fvdl@google.com>

src/ast/passes/field_analyser.cpp
@@ -309,7 +309,7 @@ void FieldAnalyser::resolve_args(Probe &probe)
           ap->addWarning() << "No debuginfo found for " << ap->target;
         }
         if (probe_args &&
-            probe_args->fields.size() >= arch::Host::arguments().size()) {
+            probe_args->fields.size() > arch::Host::arguments().size()) {
           ap->addError() << "\'args\' builtin is not supported for "
                          << "probes with stack-passed arguments.";
         }

src/btf.cpp
@@ -506,9 +506,10 @@ std::shared_ptr<Struct> BTF::resolve_args(std::string_view func,
 
   const struct btf_param *p = btf_params(t);
   __u16 vlen = btf_vlen(t);
-  if (vlen >= arch::Host::arguments().size()) {
-    err = "functions with more than 6 parameters are "
-          "not supported.";
+  if (vlen > arch::Host::arguments().size()) {
+    err = "functions with more than " +
+          std::to_string(arch::Host::arguments().size()) +
+          " parameters are not supported.";
     return nullptr;
   }
 
@@ -585,7 +586,7 @@ std::string BTF::get_all_funcs_from_btf(const BTFObj &btf_obj) const
     if (bpftrace_ && !bpftrace_->is_traceable_func(func_name))
       continue;
 
-    if (btf_vlen(t) >= arch::Host::arguments().size())
+    if (btf_vlen(t) > arch::Host::arguments().size())
       continue;
 
     funcs += btf_obj.name + ":" + func_name + "\n";
@@ -626,7 +627,7 @@ std::string BTF::get_all_raw_tracepoints_from_btf(const BTFObj &btf_obj) const
       break;
     }
 
-    if (btf_vlen(t) >= arch::Host::arguments().size())
+    if (btf_vlen(t) > arch::Host::arguments().size())
       continue;
 
     bool found = false;