e3d7f7e9b6570806c42f5dd8644ccbdda037276f
Adin Scannell <amscanne@meta.com>
2025-12-06 23:18:21 -0800

    types: drop `is_internal` and rely only on the address space
    Signed-off-by: Adin Scannell <amscanne@meta.com>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4891, branch: user/amscanne/funcidx/2

--- src/types.cpp
+++ src/types.cpp
@@ -439,7 +439,6 @@ SizedType CreateUsername()
 SizedType CreateInet(size_t size)
 {
   auto st = SizedType(Type::inet, size);
-  st.is_internal = true;
   return st;
 }
 
@@ -489,7 +488,6 @@ SizedType CreateTuple(std::shared_ptr<Struct> &&tuple)
 SizedType CreateMacAddress()
 {
   auto st = SizedType(Type::mac_address, 6);
-  st.is_internal = true;
   return st;
 }
 
