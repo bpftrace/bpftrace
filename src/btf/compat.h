562c07bf3f027d1d77f2ab30a84d32a20de09493
Adin Scannell <amscanne@meta.com>
2025-12-12 00:49:59 -0800

    btf: support full conversion to SizedType
    This change adds suffient helpers to support the automatic conversion of
    e.g. `struct task_struct *`, which contains rcu, percpu pointers,
    self-referencial pointers, unions and everything else that was not yet
    supported. The resolution of cycles is supported by building an
    anonymous local map during resolution.
    
    Signed-off-by: Adin Scannell <amscanne@meta.com>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4932, branch: user/amscanne/funcidx/4

--- src/btf/compat.h
+++ src/btf/compat.h
@@ -26,38 +26,57 @@ private:
   std::string msg_;
 };
 
+// For struct resolution, this indicates the set of structs
+// that have already been resolved for the purposes of cycles.
+using compatTypeCache = std::map<uint32_t, std::shared_ptr<bpftrace::Struct>>;
+
 template <typename T>
 struct compatTypeFor {
-  Result<SizedType> operator()(const T &t)
-  {
-    return make_error<CompatTypeError>(t);
-  }
+  Result<SizedType> operator()(const T &t, compatTypeCache &type_cache);
 };
 
 template <typename... Ts>
 struct compatTypeFor<VariantType<Ts...>> {
-  Result<SizedType> operator()(const VariantType<Ts...> &type)
+  Result<SizedType> operator()(const VariantType<Ts...> &type, compatTypeCache &type_cache)
   {
-    return std::visit([&](const auto &v) { return getCompatType(v); },
+    return std::visit([&](const auto &v) { return getCompatType(v, type_cache); },
                       type.value());
   }
 };
 
-Result<SizedType> getCompatType(const Void &type);
-Result<SizedType> getCompatType(const Integer &type);
-Result<SizedType> getCompatType(const Pointer &type);
-Result<SizedType> getCompatType(const Array &type);
-Result<SizedType> getCompatType(const Struct &type);
-Result<SizedType> getCompatType(const Enum &type);
-Result<SizedType> getCompatType(const Enum64 &type);
-Result<SizedType> getCompatType(const TypeTag &type);
-Result<SizedType> getCompatType(const Typedef &type);
+Result<SizedType> getCompatType(const Void &type, compatTypeCache &type_cache);
+Result<SizedType> getCompatType(const Integer &type, compatTypeCache &type_cache);
+Result<SizedType> getCompatType(const Pointer &type, compatTypeCache &type_cache);
+Result<SizedType> getCompatType(const Array &type, compatTypeCache &type_cache);
+Result<SizedType> getCompatType(const Struct &type, compatTypeCache &type_cache);
+Result<SizedType> getCompatType(const Union &type, compatTypeCache &type_cache);
+Result<SizedType> getCompatType(const Enum &type, compatTypeCache &type_cache);
+Result<SizedType> getCompatType(const Enum64 &type, compatTypeCache &type_cache);
+Result<SizedType> getCompatType(const TypeTag &type, compatTypeCache &type_cache);
+Result<SizedType> getCompatType(const DeclTag &type, compatTypeCache &type_cache);
+Result<SizedType> getCompatType(const Typedef &type, compatTypeCache &type_cache);
+Result<SizedType> getCompatType(const Const &type, compatTypeCache &type_cache);
+Result<SizedType> getCompatType(const Volatile &type, compatTypeCache &type_cache);
+Result<SizedType> getCompatType(const Restrict &type, compatTypeCache &type_cache);
+Result<SizedType> getCompatType(const Function &type, compatTypeCache &type_cache);
+Result<SizedType> getCompatType(const FunctionProto &type, compatTypeCache &type_cache);
+Result<SizedType> getCompatType(const DataSection &type, compatTypeCache &type_cache);
+Result<SizedType> getCompatType(const Float &type, compatTypeCache &type_cache);
+Result<SizedType> getCompatType(const Var &type, compatTypeCache &type_cache);
+Result<SizedType> getCompatType(const ForwardDecl &type, compatTypeCache &type_cache);
+
+template <typename T>
+Result<SizedType> getCompatType(const T &type, compatTypeCache &type_cache)
+{
+  compatTypeFor<T> op;
+  return op(type, type_cache);
+}
 
 template <typename T>
 Result<SizedType> getCompatType(const T &type)
 {
-  compatTypeFor<T> op;
-  return op(type);
+  compatTypeCache type_cache; // Empty initial cache.
+  return getCompatType(type, type_cache);
 }
 
 } // namespace bpftrace::btf
