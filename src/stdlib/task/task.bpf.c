792f63940e2fa0e3d4994dd276cf73c5c9fa996e
Adin Scannell <amscanne@meta.com>
2025-12-12 00:49:59 -0800

    btf: support full conversion to SizedType
    This change adds suffient helpers to support the automatic conversion of
    e.g. `struct task_struct *`, which contains rcu, percpu pointers,
    self-referencial pointers, unions and everything else that was not yet
    supported. The resolution of cycles is supported by building an
    anonymous local map during resolution.
    
    Because these types are not resolved correctly, we need to fix the types
    for the string stdlib functions. This is positive: we drop the trick to
    use "unrepresentable" unions, but now need types match directly.
    
    Signed-off-by: Adin Scannell <amscanne@meta.com>
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4932, branch: user/amscanne/funcidx/4

--- src/stdlib/task/task.bpf.c
+++ src/stdlib/task/task.bpf.c
@@ -1,18 +1,19 @@
 #define __KERNEL__
 #include <vmlinux.h>
 #include <bpf/bpf_helpers.h>
-#include "../strings/strings.h"
 
 extern struct task_struct *bpf_task_from_pid(s32 pid) __weak __ksym;
 extern void bpf_task_release(struct task_struct *p) __weak __ksym;
 
-int __bpf_task_comm_from_pid(s32 pid, m_arg *out) {
+typedef char comm_str[64];
+
+int __bpf_task_comm_from_pid(s32 pid, comm_str *out) {
   if (!bpf_task_from_pid || !bpf_task_release)
     return -95; // EOPNOTSUPP: Operation not supported
 
   struct task_struct *task = bpf_task_from_pid(pid);
   if (task) {
-    __builtin_memcpy(&out->data, &task->comm, sizeof(*out));
+    __builtin_memcpy(out, &task->comm, sizeof(*out));
     bpf_task_release(task);
     return 0;
   }
