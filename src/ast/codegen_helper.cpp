b3180ca9d3bffc26fa4ecac707bc37851c105e53
Jordan Rome <jordan@jordanrome.com>
2026-01-18 22:47:58 -0500

    Consolidate map key and value allocations (#4954)
    These should work in the exact same way and therefore the logic in codegen
    should be the same.
    
    
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/4954, branch: user/jordalgo/map_allocations/1
    
    Signed-off-by: Jordan Rome <linux@jordanrome.com>
    Co-authored-by: Jordan Rome <linux@jordanrome.com>

--- src/ast/codegen_helper.cpp
+++ src/ast/codegen_helper.cpp
@@ -2,37 +2,9 @@
 
 namespace bpftrace::ast {
 
-// needAssignMapStatementAllocation determines if a map assignment requires a
-// new memory allocation. This happens only in a few cases e.g. if there are
-// two map assignments of tuples of different sizes e.g.
-//   @x = ("xxx", 1); @x = ("xxxxxxx", 1);
-// which requires a 0 memsetting and copying of each tuple element into the
-// new allocation before calling bpf_map_update_elem.
-//
-// Another case when we need an allocation is for a external struct e.g.
-//   $v = (struct task_struct *)arg1; @ = *$v;.
-//
-// Most cases we can reuse existing BPF memory and not create a new allocation.
-//
-// Note this function does NOT determine if an allocation should use scratch
-// buffer or the stack, that logic is in
-// IRBuilderBPF::CreateWriteMapValueAllocation
-bool needAssignMapStatementAllocation(const AssignMapStatement &assignment)
+bool needMapAllocation(const SizedType &ty)
 {
-  const auto &map = *assignment.map_access;
-  const auto &expr_type = assignment.expr.type();
-  if (shouldBeInBpfMemoryAlready(expr_type)) {
-    return false;
-  } else if (map.map->value_type.IsCStructTy() ||
-             map.map->value_type.IsArrayTy()) {
-    return !expr_type.is_internal;
-  }
-  return true;
-}
-
-bool needMapKeyAllocation(const Expression &key_expr)
-{
-  return !inBpfMemory(key_expr.type());
+  return !inBpfMemory(ty);
 }
 
 } // namespace bpftrace::ast
