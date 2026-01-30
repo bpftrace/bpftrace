d38d34eb8377609adc86d7310b0c9b1535b73498
Stanislaw Gruszka <stf_xl@wp.pl>
2026-01-30 15:32:08 +0100

    Fix wrong kprobe address check (#4969)
    We should only check if function is traceable if there is name for it.
    Remove wrong '|| probe.address = 0' check and return early from
    AttachedKprobeProbe::make() if there is no name and no address.
    
    Signed-off-by: Stanislaw Gruszka <stf_xl@wp.pl>

---
+++ tests/attached_probe.cpp
@@ -0,0 +1,23 @@
+#include "attached_probe.h"
+#include "mocks.h"
+#include "gtest/gtest.h"
+
+namespace bpftrace::test {
+
+TEST(attached_probe, kprobe_empty_name_and_zero_address)
+{
+  auto mock_bpftrace = get_mock_bpftrace();
+
+  BPFtrace &bpftrace = *mock_bpftrace;
+  BpfProgram prog(nullptr);
+
+  Probe probe;
+  probe.type = ProbeType::kprobe;
+  probe.attach_point = "";
+  probe.address = 0;
+
+  auto result = AttachedProbe::make(probe, prog, 0, bpftrace);
+  EXPECT_TRUE(!result);
+}
+
+} // namespace bpftrace::test
