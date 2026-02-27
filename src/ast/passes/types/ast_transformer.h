0238c5cd90e14e7773a932431337c986331290cb
Jordan Rome <jordan@jordanrome.com>
2026-02-27 17:23:00 -0500

    Minor refactor to avoid large headers (#5021)
    Just expose single public functions, e.g. "RunTypeApplicator", so all the
    visitors don't have to be defined in the header files. This is specifically
    for visitors used in type_resolver.cpp.
    
    No functional changes.
    
    
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/5021, branch: user/jordalgo/type_reorganize/3
    
    Signed-off-by: Jordan Rome <linux@jordanrome.com>
    Co-authored-by: Jordan Rome <linux@jordanrome.com>

--- src/ast/passes/types/ast_transformer.h
+++ src/ast/passes/types/ast_transformer.h
@@ -1,53 +1,14 @@
 #pragma once
 
 #include "ast/passes/types/type_resolver.h"
-#include "ast/visitor.h"
-
-#include <optional>
 
 namespace bpftrace::ast {
 
 class MacroRegistry;
 
-class AstTransformer
-    : public Visitor<AstTransformer, std::optional<Expression>> {
-public:
-  AstTransformer(ASTContext &ast,
-                 const MacroRegistry &macro_registry,
-                 const ResolvedTypes &resolved_types)
-      : ast_(ast),
-        macro_registry_(macro_registry),
-        resolved_types_(resolved_types) {};
-
-  using Visitor<AstTransformer, std::optional<Expression>>::visit;
-
-  std::optional<Expression> visit(Expression &expr);
-  std::optional<Expression> visit(Binop &binop);
-  std::optional<Expression> visit(Offsetof &offof);
-  std::optional<Expression> visit(Sizeof &szof);
-  std::optional<Expression> visit(Typeinfo &typeinfo);
-  std::optional<Expression> visit(FieldAccess &acc);
-
-  bool had_transforms() const
-  {
-    return had_transforms_;
-  }
-
-private:
-  ASTContext &ast_;
-  const MacroRegistry &macro_registry_;
-  const ResolvedTypes &resolved_types_;
-  bool had_transforms_ = false;
-
-  const SizedType &get_type(const TypeVariable &node) const
-  {
-    auto it = resolved_types_.find(node);
-    if (it != resolved_types_.end()) {
-      return it->second;
-    }
-    static SizedType none = CreateNone();
-    return none;
-  }
-};
+// Runs the AST transformer, returns true if any transforms were made.
+bool RunAstTransformer(ASTContext &ast,
+                       const MacroRegistry &macro_registry,
+                       const ResolvedTypes &resolved_types);
 
 } // namespace bpftrace::ast
