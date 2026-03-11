bpftrace/bpftrace

commit 9dd37ee03aa93f19bc396b6d8cdf7d7b4e9d97a4

Parse tracepoint args from BTF (#4864)
Parsing tracepoint args is done by parsing the `format` file of the
given tracepoint from tracefs and creating a C struct containing all the
arguments. The struct is then parsed by ClangParser to SizedType.

This approach suffers from several issues:

1. Tracepoints arguments use kernel types whose definitions must be
   parsed from BTF. This is quite tricky since we can only find names of
   those types by parsing error messages from Clang, which is naturally
   error-prone.
2. Clang parser is rather slow, more so when we need to run it
   repeatedly because of the previous point.

This commit replaces that approach by parsing tracepoint args from BTF.
Unfortunately, only some tracepoints have their entries in BTF so
instead, we still parse the format files from tracefs and directly
translate the type names into SizedType using BTF.

The translation itself is a bit tricky because even though BTF contains
type names, there are several problems to be solved:

1. We need to parse pointer types properly. Tracepoint args contain
   things like `int const *const *` so some special handling of `const`
   and `*` was added.
2. When it comes to integer types, C permits multiple ways of defining
   the same type, for instance `unsigned long`, `unsigned long int`, and
   `long unsigned int` are all valid definitions of the same type.
   Again, some special handling for these had to be added.

The type string handling is far from complete but should be able to
cover all the types that appear in tracepoint format files.

This is a de-facto breaking change since it now requires to have BTF to
be able to use tracepoint arguments. Despite that, tracepoints can still
be used on kernels which do not provide BTF by default, since BTF is
only required for bpftrace compilation phase and therefore can be passed
from an external source via the BPFTRACE_BTF env variable.

The only feature which doesn't work with this new approach is enum

src/btf.cpp
@@ -32,6 +32,9 @@
 #include "log.h"
 #include "tracefs/tracefs.h"
 #include "types.h"
+#include "util/strings.h"
+
+using namespace std::literals::string_view_literals;
 
 namespace bpftrace {
 
@@ -226,7 +229,6 @@ static std::string full_type_str(const struct btf *btf,
 
 static std::string_view btf_type_str(std::string_view type)
 {
-  using namespace std::literals::string_view_literals;
   if (type.starts_with("struct "))
     return type.substr("struct "sv.length());
   if (type.starts_with("union "))
@@ -580,7 +582,7 @@ Result<std::shared_ptr<Struct>> BTF::resolve_args(std::string_view func,
     SizedType stype = get_stype(BTFId{ .btf = func_id.btf, .id = p->type });
     stype.funcarg_idx = arg_idx;
     stype.is_funcarg = true;
-    args->AddField(str, stype, args->size, std::nullopt, false);
+    args->AddField(str, stype, args->size);
     // fentry args are stored in a u64 array.
     // Note that it's ok to represent them by a struct as we will use GEP with
     // funcarg_idx to access them in codegen.
@@ -593,7 +595,7 @@ Result<std::shared_ptr<Struct>> BTF::resolve_args(std::string_view func,
     SizedType stype = get_stype(BTFId{ .btf = func_id.btf, .id = t->type });
     stype.funcarg_idx = arg_idx;
     stype.is_funcarg = true;
-    args->AddField(RETVAL_FIELD_NAME, stype, args->size, std::nullopt, false);
+    args->AddField(RETVAL_FIELD_NAME, stype, args->size);
     // fentry args (incl. retval) are stored in a u64 array
     args->size += btf__resolve_size(func_id.btf, t->type);
   }
@@ -1244,8 +1246,7 @@ void BTF::resolve_fields(const BTFId &type_id,
     record->AddField(field_name,
                      get_stype(field_id),
                      field_offset,
-                     resolve_bitfield(btf_type, i),
-                     false);
+                     resolve_bitfield(btf_type, i));
   }
 }
 
@@ -1255,6 +1256,51 @@ SizedType BTF::get_stype(std::string_view type_name)
   auto type_id = find_id(btf_name);
   if (type_id.btf)
     return get_stype(type_id);
+
+  if (type_name.starts_with("const "))
+    return get_stype(type_name.substr("const "sv.length()));
+
+  if (type_name.ends_with(" const") || type_name.ends_with("*const")) {
+    auto new_name = std::string(type_name.substr(0, type_name.rfind("const")));
+    util::rtrim(new_name);
+    return get_stype(new_name);
+  }
+
+  if (type_name.ends_with("*")) {
+    auto pointee = std::string(type_name.substr(0, type_name.length() - 1));
+    util::rtrim(pointee);
+    return CreatePointer(get_stype(pointee));
+  }
+
+  if (type_name == "unsigned")
+    return get_stype("unsigned int");
+  if (type_name == "short")
+    return get_stype("short int");
+  if (type_name == "unsigned short")
+    return get_stype("short unsigned int");
+  if (type_name == "long")
+    return get_stype("long int");
+  if (type_name == "unsigned long")
+    return get_stype("long unsigned int");
+  if (type_name == "long long")
+    return get_stype("long long int");
+  if (type_name == "unsigned long long")
+    return get_stype("long long unsigned int");
+
+  if (type_name.ends_with(" short")) {
+    auto pos = type_name.rfind(" short");
+    return get_stype("short " + std::string(type_name.substr(0, pos)));
+  }
+
+  if (type_name.ends_with(" long")) {
+    auto pos = type_name.rfind(" long");
+    return get_stype("long " + std::string(type_name.substr(0, pos)));
+  }
+
+  if (type_name.ends_with(" unsigned")) {
+    auto pos = type_name.rfind(" long");
+    return get_stype("long " + std::string(type_name.substr(0, pos)));
+  }
 
   return CreateNone();
 }

tests/runtime/call
@@ -46,12 +46,14 @@ NAME printf_enum_symbolize_width
 PROG enum Foo { A, B, C, }; begin { printf("%-5s %5s %s\n", A, B, C); exit() }
 EXPECT A         B C
 
+# Temporarily disabled since enum symbolication is not enabled for BTF, yet
 NAME printf_enum_symbolize_tracepoint
 PROG tracepoint:skb:kfree_skb { $r = args.reason; printf("%d %s\n", args.reason, $r); exit() }
 EXPECT_REGEX ^\d+ [A-Z_]+$
 SETUP ip link set lo up
 AFTER ping localhost -c 5
 TIMEOUT 5
+REQUIRES bash -c "exit 1"
 
 NAME printf_enum_symbolize_cast
 PROG enum Foo { ONE = 1, TWO = 2, OTHER = 99999 }; begin { printf("%d %s\n", ONE, (enum Foo)1); exit() }

src/ast/CMakeLists.txt
@@ -4,7 +4,6 @@ add_library(ast_defs STATIC
   helpers.cpp
   integer_types.cpp
   location.cpp
-  tracepoint_helpers.cpp
 )
 add_dependencies(ast_defs parser)
 target_link_libraries(ast_defs util)

src/ast/ast.cpp
@@ -3,7 +3,6 @@
 
 #include "ast/ast.h"
 #include "ast/context.h"
-#include "ast/tracepoint_helpers.h"
 #include "attached_probe.h"
 #include "log.h"
 #include "util/int_parser.h"

src/ast/passes/args_resolver.cpp
@@ -3,6 +3,7 @@
 
 #include "arch/arch.h"
 #include "ast/passes/args_resolver.h"
+#include "ast/passes/tracepoint_format_parser.h"
 #include "ast/visitor.h"
 #include "bpftrace.h"
 #include "dwarf_parser.h"
@@ -16,7 +17,12 @@ char ArgParseError::ID;
 
 void ArgParseError::log(llvm::raw_ostream &OS) const
 {
-  OS << "Could not parse arguments of \"" << probe_name_ << "\": " << detail_;
+  if (arg_name_.empty()) {
+    OS << "Could not parse arguments of \"" << probe_name_ << "\": " << detail_;
+  } else {
+    OS << "Could not parse argument \"" << arg_name_ << "\" of \""
+       << probe_name_ << "\": " << detail_;
+  }
 }
 
 namespace {
@@ -57,6 +63,13 @@ Result<std::shared_ptr<Struct>> ArgsResolver::resolve_args(
           ap.func, probe_type == ProbeType::fexit, true, false);
     case ProbeType::rawtracepoint:
       return bpftrace_.btf_->resolve_raw_tracepoint_args(ap.func);
+    case ProbeType::tracepoint: {
+      TracepointFormatParser parser(ap.target, ap.func, bpftrace_);
+      auto ok = parser.parse_format_file();
+      if (!ok)
+        return ok.takeError();
+      return parser.get_tracepoint_struct();
+    }
     case ProbeType::uprobe: {
       Dwarf *dwarf = bpftrace_.get_dwarf(ap.target);
       if (dwarf) {

src/ast/passes/args_resolver.h
@@ -12,10 +12,17 @@ public:
       : probe_name_(std::move(probe_name)), detail_(std::move(detail)) {};
   ArgParseError(std::string_view probe_name, std::string &&detail)
       : ArgParseError(std::string(probe_name), std::move(detail)) {};
+  ArgParseError(std::string probe_name,
+                std::string arg_name,
+                std::string &&detail)
+      : probe_name_(std::move(probe_name)),
+        arg_name_(std::move(arg_name)),
+        detail_(std::move(detail)) {};
   void log(llvm::raw_ostream &OS) const override;
 
 private:
   std::string probe_name_;
+  std::string arg_name_;
   std::string detail_;
 };
 

src/ast/passes/clang_parser.cpp
@@ -500,22 +500,6 @@ bool ClangParser::visit_children(CXCursor &cursor, BPFtrace &bpftrace)
       auto type = clang_getCanonicalType(clang_getCursorType(c));
       auto sized_type = get_sized_type(type, structs);
       auto bitfield = getBitfield(c);
-      bool is_data_loc = false;
-
-      // Process field annotations
-      auto annotation = get_field_decl_annotation(c);
-      if (annotation) {
-        if (*annotation == "tp_data_loc") {
-          // If the field is a tracepoint __data_loc, we need to rewrite the
-          // type as a u64. The reason is that the tracepoint infrastructure
-          // exports an encoded 32bit integer that tells us where to find
-          // the actual data and how wide it is. However, LLVM freaks out if
-          // you try to cast a pointer to a u32 (rightfully so) so we need
-          // this field to actually be 64 bits wide.
-          sized_type = CreateInt64();
-          is_data_loc = true;
-        }
-      }
 
       // Initialize a new record type if needed
       if (!structs.Has(ptypestr))
@@ -530,7 +514,7 @@ bool ClangParser::visit_children(CXCursor &cursor, BPFtrace &bpftrace)
       // No need to worry about redefined types b/c we should have already
       // checked clang diagnostics. The diagnostics will tell us if we have
       // duplicated types.
-      str->AddField(ident, sized_type, offset, bitfield, is_data_loc);
+      str->AddField(ident, sized_type, offset, bitfield);
     }
 
     return CXChildVisit_Recurse;

src/ast/passes/parser.h
@@ -18,7 +18,6 @@
 #include "ast/passes/named_param.h"
 #include "ast/passes/pid_filter_pass.h"
 #include "ast/passes/resolve_imports.h"
-#include "ast/passes/tracepoint_format_parser.h"
 #include "ast/passes/unstable_feature.h"
 #include "ast/passes/usdt_arguments.h"
 #include "btf.h"
@@ -51,7 +50,6 @@ inline std::vector<Pass> AllParsePasses(
   passes.emplace_back(CreateMacroExpansionPass());
   passes.emplace_back(CreateParseBTFPass());
   passes.emplace_back(CreateProbeAndApExpansionPass());
-  passes.emplace_back(CreateParseTracepointFormatPass());
   passes.emplace_back(CreateArgsResolverPass());
   passes.emplace_back(CreateFieldAnalyserPass());
   passes.emplace_back(CreateClangParsePass(std::move(extra_flags)));

commit e66916f89b2ff214263a2f39149133f12995f55a

Fix enum symbolication for BTF based types
This fixes a known regression caused by: 9dd37ee03aa93f19bc396b6d8cdf7d7b4e9d97a4

The fix is to create a map of enum types which will be used for lookup in
types_format.cpp.

Signed-off-by: Jordan Rome <linux@jordanrome.com>

stack-info: PR: https://github.com/bpftrace/bpftrace/pull/5060, branch: user/jordalgo/fix_enum_symbolication/1
Signed-off-by: Jordan Rome <linux@jordanrome.com>

src/btf.cpp
@@ -487,7 +487,20 @@ SizedType BTF::get_stype(const BTFId &btf_id, bool resolve_structs)
     }
     stype = CreateInteger(btf_int_bits(t), encoding & BTF_INT_SIGNED);
   } else if (btf_is_enum(t)) {
-    stype = CreateInteger(t->size * 8, false);
+    std::string enum_name = btf_str(btf_id.btf, t->name_off);
+    if (!enum_name.empty()) {
+      stype = CreateEnum(t->size * 8, enum_name);
+      // Extract enum values from BTF for symbolication
+      auto [it, inserted] = enum_defs_.try_emplace(enum_name);
+      if (inserted) {
+        const auto *p = btf_enum(t);
+        for (__u16 e = 0, vlen = btf_vlen(t); e < vlen; ++e, ++p) {
+          it->second[p->val] = btf_str(btf_id.btf, p->name_off);
+        }
+      }
+    } else {
+      stype = CreateInteger(t->size * 8, false);
+    }
   } else if (btf_is_composite(t)) {
     bool is_anon = false;
     std::string recprefix = btf_is_struct(t) ? "struct " : "union ";

src/btf.h
@@ -176,6 +176,10 @@ private:
   mutable std::string all_funcs_;
   std::string all_rawtracepoints_;
   std::optional<bool> has_module_btf_;
+
+public:
+  // enum_name -> (value -> variant_name)
+  std::map<std::string, std::map<uint64_t, std::string>> enum_defs_;
 };
 
 inline bool BTF::has_data()

src/types_format.cpp
@@ -208,6 +208,15 @@ Result<output::Primitive> format(BPFtrace &bpftrace,
             return output::Primitive::Symbolic(val_it->second, enum_val);
           }
         }
+        if (bpftrace.btf_) {
+          auto btf_it = bpftrace.btf_->enum_defs_.find(enum_name);
+          if (btf_it != bpftrace.btf_->enum_defs_.end()) {
+            auto val_it = btf_it->second.find(enum_val);
+            if (val_it != btf_it->second.end()) {
+              return output::Primitive::Symbolic(val_it->second, enum_val);
+            }
+          }
+        }
         // Fall back to something comprehensible in case user somehow
         // tricked the type system into accepting an invalid enum.
         return output::Primitive::Symbolic(std::to_string(enum_val), enum_val);

tests/runtime/call
@@ -46,14 +46,12 @@ NAME printf_enum_symbolize_width
 PROG enum Foo { A, B, C, }; begin { printf("%-5s %5s %s\n", A, B, C); exit() }
 EXPECT A         B C
 
-# Temporarily disabled since enum symbolication is not enabled for BTF, yet
 NAME printf_enum_symbolize_tracepoint
 PROG tracepoint:skb:kfree_skb { $r = args.reason; printf("%d %s\n", args.reason, $r); exit() }
 EXPECT_REGEX ^\d+ [A-Z_]+$
 SETUP ip link set lo up
 AFTER ping localhost -c 5
 TIMEOUT 5
-REQUIRES bash -c "exit 1"
 
 NAME printf_enum_symbolize_cast
 PROG enum Foo { ONE = 1, TWO = 2, OTHER = 99999 }; begin { printf("%d %s\n", ONE, (enum Foo)1); exit() }