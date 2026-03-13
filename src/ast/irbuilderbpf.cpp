01447c4c6fb2f43ca3fa578343508bc20c6c8a6d
Jordan Rome <jordan@jordanrome.com>
2026-03-13 10:32:08 -0400

    Bump MIN_LLVM_MAJOR to 18 (#5067)
    Also cleanup some of the ifdefs needed for LLVM 17.
    
    
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/5067, branch: user/jordalgo/llvm_17_fix/1
    
    Signed-off-by: Jordan Rome <linux@jordanrome.com>
    Co-authored-by: Jordan Rome <linux@jordanrome.com>

--- src/ast/irbuilderbpf.cpp
+++ src/ast/irbuilderbpf.cpp
@@ -2409,7 +2409,6 @@ llvm::Value *IRBuilderBPF::CreateSafeGEP(llvm::Type *ty,
     ptr = CreateIntToPtr(ptr, getPtrTy());
   }
 
-#if LLVM_VERSION_MAJOR >= 18
   if (!preserve_static_offset_) {
 #if LLVM_VERSION_MAJOR >= 20
     preserve_static_offset_ = llvm::Intrinsic::getOrInsertDeclaration(
@@ -2420,7 +2419,6 @@ llvm::Value *IRBuilderBPF::CreateSafeGEP(llvm::Type *ty,
 #endif
   }
   ptr = CreateCall(preserve_static_offset_, ptr);
-#endif
 
   // Create the GEP itself; on newer versions of LLVM this coerces the pointer
   // value into a pointer to the given type, and older versions have guaranteed
