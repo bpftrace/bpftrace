bpftrace/bpftrace

commit 817faee42c9fa7b024fa27fcf4d4b911b82ba8fb

Remove automatic integer promotion (#4768)
This makes several changes to how we handle
the size and up-sizing of integer types.

1. For integer and negative integer literals
make their type the smallest possible type
they can fit into
2. Integer literals will always be unsigned
by default but can be re-created to be signed
if needed (this is in lieu of adding an additional
cast to a signed integer)
3. Negative Integer literals will always be
signed (as expected).
4. Remove automatic integer promotion for map
values and keys (except scalar map keys)
5. Add neccessary casting when comparing integers,
assigning them to variables, map keys, and map
values.
6. Check ints when passed to map aggregation
functions (e.g. `sum`, `avg`) to ensure they
are compatible.

This change also makes sure that tuples
are compatible when their contents contain
integers of different sizes. There will be a
follow-up PR that adds casting earlier, which
will allow us to remove the dependency on codegen
to copy elements individually from one to the other.

The reason for this change is to reduce confusing
logic around integer handling in codegen and
make integer types more robust/flexible.

For example this fails on master:
`begin { $a = (uint32)1; $a = -1; }`
but works fine here as we upcast both sides
to an int64 which safely holds both.

Signed-off-by: Jordan Rome <linux@jordanrome.com>
Co-authored-by: Jordan Rome <linux@jordanrome.com>

src/ast/passes/codegen_llvm.cpp
@@ -333,6 +333,9 @@ private:
   void createRet(Value *value = nullptr);
   int getReturnValueForProbe(ProbeType probe_type);
 
+  template <typename T>
+  ScopedExpr getIntegerLiteral(size_t size, T value);
+
   // Every time we see a watchpoint that specifies a function + arg pair, we
   // generate a special "setup" probe that:
   //
@@ -514,14 +517,32 @@ CodegenLLVM::CodegenLLVM(ASTContext &ast,
       debug_.createGlobalVariable(LICENSE, CreateString(license_size)));
 }
 
+template <typename T>
+ScopedExpr CodegenLLVM::getIntegerLiteral(size_t size, T value)
+{
+  switch (size) {
+    case 1:
+      return ScopedExpr(b_.getInt8(value));
+    case 2:
+      return ScopedExpr(b_.getInt16(value));
+    case 4:
+      return ScopedExpr(b_.getInt32(value));
+    case 8:
+      return ScopedExpr(b_.getInt64(value));
+    default:
+      LOG(BUG) << "Unsupported integer size: " << size;
+  }
+  __builtin_unreachable();
+}
+
 ScopedExpr CodegenLLVM::visit(Integer &integer)
 {
-  return ScopedExpr(b_.getInt64(integer.value));
+  return getIntegerLiteral(integer.type().GetSize(), integer.value);
 }
 
 ScopedExpr CodegenLLVM::visit(NegativeInteger &integer)
 {
-  return ScopedExpr(b_.getInt64(integer.value));
+  return getIntegerLiteral(integer.type().GetSize(), integer.value);
 }
 
 ScopedExpr CodegenLLVM::visit(Boolean &boolean)
@@ -838,7 +859,7 @@ ScopedExpr CodegenLLVM::visit(Call &call)
     // promote int to 64-bit
     Value *cast = b_.CreateIntCast(scoped_expr.value(),
                                    b_.getInt64Ty(),
-                                   call.vargs.front().type().IsSigned());
+                                   map.value_type.IsSigned());
     b_.CreatePerCpuMapElemAdd(map, scoped_key.value(), cast, call.loc);
     return ScopedExpr();
 
@@ -852,7 +873,7 @@ ScopedExpr CodegenLLVM::visit(Call &call)
     // promote int to 64-bit
     Value *expr = b_.CreateIntCast(scoped_expr.value(),
                                    b_.getInt64Ty(),
-                                   call.vargs.front().type().IsSigned());
+                                   map.value_type.IsSigned());
 
     llvm::Type *mm_struct_ty = b_.GetMapValueType(map.value_type);
 
@@ -917,7 +938,7 @@ ScopedExpr CodegenLLVM::visit(Call &call)
     // promote int to 64-bit
     Value *expr = b_.CreateIntCast(scoped_expr.value(),
                                    b_.getInt64Ty(),
-                                   call.vargs.at(2).type().IsSigned());
+                                   map.value_type.IsSigned());
 
     llvm::Type *avg_struct_ty = b_.GetMapValueType(map.value_type);
 
@@ -2861,6 +2882,7 @@ void CodegenLLVM::createTupleCopy(const SizedType &expr_type,
   auto *tuple_ty = b_.GetType(var_type);
   for (size_t i = 0; i < expr_type.GetFields().size(); ++i) {
     SizedType &t_type = expr_type.GetField(i).type;
+    SizedType &v_type = var_type.GetField(i).type;
     Value *offset_val = b_.CreateGEP(
         array_ty,
         src_val,
@@ -2868,15 +2890,21 @@ void CodegenLLVM::createTupleCopy(const SizedType &expr_type,
     Value *dst = b_.CreateGEP(tuple_ty,
                               dst_val,
                               { b_.getInt32(0), b_.getInt32(i) });
-    if (t_type.IsTupleTy() && !t_type.IsSameSizeRecursive(var_type)) {
+    if (t_type.IsTupleTy() && !t_type.IsSameSizeRecursive(v_type)) {
       createTupleCopy(t_type, var_type.GetField(i).type, dst, offset_val);
-    } else if (t_type.IsIntTy() && t_type.GetSize() < 8) {
-      // Integers are always stored as 64-bit in map keys
-      b_.CreateStore(b_.CreateIntCast(b_.CreateLoad(b_.GetType(t_type),
-                                                    offset_val),
-                                      b_.getInt64Ty(),
-                                      t_type.IsSigned()),
-                     dst);
+    } else if (t_type.IsIntTy()) {
+      // TODO: tuple casting/resizing should happen in an earlier pass.
+      // However for now, it's possible ints need to be sized up
+      // when they exist inside tuples see (update_tuple_type).
+      if (t_type.GetSize() != v_type.GetSize()) {
+        b_.CreateStore(b_.CreateIntCast(b_.CreateLoad(b_.GetType(t_type),
+                                                      offset_val),
+                                        b_.GetType(v_type),
+                                        v_type.IsSigned()),
+                       dst);
+      } else {
+        b_.CreateStore(b_.CreateLoad(b_.GetType(t_type), offset_val), dst);
+      }
     } else {
       b_.CreateMemcpyBPF(dst, offset_val, t_type.GetSize());
     }
@@ -2945,10 +2973,6 @@ ScopedExpr CodegenLLVM::visit(AssignMapStatement &assignment)
           value, map_type, expr, assignment.loc, expr_type.GetAS());
     }
   } else {
-    if (assignment.map->type().IsIntTy()) {
-      // Integers are always stored as 64-bit in map values
-      expr = b_.CreateIntCast(expr, b_.getInt64Ty(), map_type.IsSigned());
-    }
     b_.CreateStore(expr, value);
   }
   b_.CreateMapUpdateElem(
@@ -3379,11 +3403,7 @@ ScopedExpr CodegenLLVM::getMapKey(Map &map, Expression &key_expr)
       // Call-ee freed
     }
   } else if (map.key_type.IsIntTy()) {
-    // Integers are always stored as 64-bit in map keys
-    b_.CreateStore(b_.CreateIntCast(scoped_key_expr.value(),
-                                    b_.getInt64Ty(),
-                                    key_type.IsSigned()),
-                   key);
+    b_.CreateStore(scoped_key_expr.value(), key);
   } else if (map.key_type.IsBoolTy()) {
     b_.CreateStore(
         b_.CreateIntCast(scoped_key_expr.value(), b_.getInt8Ty(), false), key);
@@ -3465,14 +3485,10 @@ ScopedExpr CodegenLLVM::getMultiMapKey(Map &map,
       if ((map_key_size % 8) != 0)
         aligned = false;
     } else {
-      // promote map key to 64-bit:
-      Value *key_elem = b_.CreateIntCast(scoped_expr.value(),
-                                         b_.getInt64Ty(),
-                                         key_expr.type().IsSigned());
       if (aligned)
-        b_.CreateStore(key_elem, offset_val);
+        b_.CreateStore(scoped_expr.value(), offset_val);
       else
-        b_.createAlignedStore(key_elem, offset_val, 1);
+        b_.createAlignedStore(scoped_expr.value(), offset_val, 1);
     }
   }
   offset += map_key_size;

commit e852cdc20d33af53f563cf74144507933b8b5a24

Fix: un-aligned key access for hist/lhist/tseries
In the case of hist, lhist, and teries, we add additional metadata into the
map keys for bookkeeping. When the user-set map key size is not 8-byte
aligned then we do a specifically aligned store. However, we weren't doing
that in the case of integers or booleans, which are 1 byte, and this was
causing a verifier error.

This was caused by 817faee42 — "Remove automatic integer promotion
(#4768)". Before that the keys were automatically promoted to 8 bytes.

The fix is to check the map_key_size size for the right alignment
regardless of its type.

Fixes: https://github.com/bpftrace/bpftrace/issues/5180

Signed-off-by: Jordan Rome <jordalgo@meta.com>

stack-info: PR: https://github.com/bpftrace/bpftrace/pull/5181, branch: user/jordalgo/issue_5180/1
Signed-off-by: Jordan Rome <jordalgo@meta.com>

src/ast/passes/codegen_llvm.cpp
@@ -3632,7 +3632,6 @@ ScopedExpr CodegenLLVM::getMultiMapKey(Map &map,
   auto *key_type = ArrayType::get(b_.getInt8Ty(), size);
 
   int offset = 0;
-  bool aligned = true;
   // Construct a map key in the stack
   Value *offset_val = b_.CreateGEP(key_type,
                                    key,
@@ -3640,18 +3639,16 @@ ScopedExpr CodegenLLVM::getMultiMapKey(Map &map,
   size_t map_key_size = type_map_.map_key_type(map.ident).GetSize();
   size_t expr_size = type_map_.type(key_expr).GetSize();
 
+  bool aligned = (map_key_size % 8) == 0;
+
   if (inBpfMemory(type_map_.type(key_expr))) {
     b_.CreateMemcpyBPF(offset_val, scoped_expr.value(), expr_size);
-    if ((map_key_size % 8) != 0)
-      aligned = false;
   } else {
     if (type_map_.type(key_expr).IsArrayTy() ||
         type_map_.type(key_expr).IsCTypeTy()) {
       // Read the array/struct into the key
       b_.CreateProbeRead(
           offset_val, type_map_.type(key_expr), scoped_expr.value(), map.loc);
-      if ((map_key_size % 8) != 0)
-        aligned = false;
     } else {
       if (aligned)
         b_.CreateStore(scoped_expr.value(), offset_val);

tests/runtime/regression
@@ -160,3 +160,9 @@ NAME probe specific arguments
 PROG fentry:vmlinux:vfs_write { print(args.file); exit(); } t:syscalls:sys_enter_execve { print(args.filename); exit(); }
 EXPECT Attached 2 probes
 TIMEOUT 1
+
+# https://github.com/bpftrace/bpftrace/issues/5180
+NAME maps with hidden key fields aligned access
+PROG begin{ @a[(uint8)1] = hist(3,0); @b = count(); }
+EXPECT Attached 1 probe
+TIMEOUT 1