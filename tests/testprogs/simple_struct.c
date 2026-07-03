c805c81348ed4a15811e2e5b5fce67d248e43c29
Jordan Rome <jordan@jordanrome.com>
2026-07-03 08:20:47 -0400

    stdlib: Add `container_of` macro (#5236)
    This is equivalent to the libbpf macro of the same name.
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/5236, branch: user/jordalgo/container_of/2
    Signed-off-by: Jordan Rome <jordalgo@meta.com>
    
    ---------
    
    Signed-off-by: Jordan Rome <jordalgo@meta.com>

--- tests/testprogs/simple_struct.c
+++ tests/testprogs/simple_struct.c
@@ -8,11 +8,17 @@ int func(struct Foo *foo)
   return foo->m;
 }
 
+int n_func(int *n)
+{
+  return *n;
+}
+
 int main()
 {
   struct Foo foo;
   foo.m = 2;
   foo.n = 3;
   func(&foo);
+  n_func(&foo.n);
   return 0;
 }
