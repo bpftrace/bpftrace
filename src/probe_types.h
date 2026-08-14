c7bf808d5b01adb7333cb0b929bcb5ecf9fd0585
Rong Tao <rongtao@cestc.cn>
2026-08-14 19:49:07 +0800

    Remove useless structure Probe::need_expansion field (#5293)
    Probe::need_expansion is not used in anywhere. If compile bpftrace with
    `-DBUILD_ASAN=ON -DBUILD_UBSAN=ON`:
    
        $ sudo bpftrace -e 'begin {}'
        /home/rongtao/Git/bpftrace/bpftrace/src/probe_types.h:105:8:
        runtime error: load of value 188, which is not a valid value for type 'bool'
    
    This error comes from UBSan's bool type check: bool in memory can only be
    0 or 1, but you loaded 188 (0xbc) here. This usually means that there is
    an uninitialized bool member in the Probe structure/class in
    src/probe_types.h, which was then read or copied.
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>
    (cherry picked from commit e9b078a40d888ca1966d5ea45bc9db7f313d226e)
    Signed-off-by: Viktor Malik <viktor.malik@gmail.com>

--- src/probe_types.h
+++ src/probe_types.h
@@ -107,7 +107,6 @@ struct Probe {
   std::string path;         // file path if used
   std::string attach_point; // probe name (last component)
   std::string name;         // full probe name
-  bool need_expansion;
   std::string pin;  // pin file for iterator probes
   std::string ns;   // for USDT probes, if provider namespace not from path
   uint64_t loc = 0; // for USDT probes
