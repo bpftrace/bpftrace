feee51ed0f345295a4efd644caa3c195d0a23eb2
Rong Tao <rongtao@cestc.cn>
2026-08-24 11:24:06 +0800

    self-tests.sh: support --filter argument
    Usage:
    
        $ sudo ./self-tests.sh --filter=string
        ....
        [==========] Running 1 tests.
        [ RUN      ] cast_string_to_bool         << matched
        [       OK ] cast_string_to_bool (1 ns)
        [==========] 1 test ran.
        [  PASSED  ] 1 test.
        ....
        [==========] Running 1 tests.
        [ RUN      ] tuple_map_key_string_resize
        [       OK ] tuple_map_key_string_resize (1000 μs)
        [==========] 1 test ran.
        [  PASSED  ] 1 test.
        ....
        No probes to attach                      << not matched
    
        [==========] Running 2 tests.
        [ RUN      ] variable_strings_are_memset
        [       OK ] variable_strings_are_memset (1000 μs)
        [ RUN      ] map_strings_are_memset
        [       OK ] map_strings_are_memset (1000 μs)
        [==========] 2 tests ran.
        [  PASSED  ] 2 tests.
        ....
        No probes to attach
        No probes to attach
        No probes to attach
        No probes to attach
        ....
        No probes to attach
        [==========] Running 3 tests.
        [ RUN      ] basic_substring_match
        [       OK ] basic_substring_match (1 ns)
        [ RUN      ] substring_at_end
        [       OK ] substring_at_end (1000 μs)
        [ RUN      ] substring_at_beginning
        [       OK ] substring_at_beginning (1000 μs)
        [==========] 3 tests ran.
        [  PASSED  ] 3 tests.
    
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- tests/self-tests.sh
+++ tests/self-tests.sh
@@ -11,11 +11,70 @@ TESTS_DIR="$(dirname "${BASH_SOURCE[0]}")";
 DIR="$( cd "$TESTS_DIR" >/dev/null && pwd )"
 BPFTRACE_RUNTIME_TEST_EXECUTABLE=${BPFTRACE_EXECUTABLE:-$DIR/../src/bpftrace};
 export BPFTRACE_RUNTIME_TEST_EXECUTABLE;
+FILTER=
+
+__usage__()
+{
+    echo -e "
+self-test.sh [-h|--help] [--filter=<PATTERN>]
+
+    -h, --help           show this information.
+    --filter=[PATTERN]   only run the tests matching this pattern.
+"
+    exit ${1-0}
+}
+
+TEMP_ARGS=$(getopt --options h \
+    --long help \
+    --long filter: \
+    --name ${0} -- "$@")
+
+test $? != 0 && __usage__ 1
+
+eval set -- "$TEMP_ARGS"
+
+while true; do
+    case $1 in
+    --filter)
+        shift
+        FILTER=$1
+        shift
+        ;;
+    -h | --help)
+        shift
+        __usage__
+        ;;
+    --)
+        shift
+        break
+        ;;
+    esac
+done
 
 echo "===================="
 echo "bpftrace --info:"
 echo "===================="
 "${BPFTRACE_RUNTIME_TEST_EXECUTABLE}" --info;
 
-find "$TESTS_DIR"/self -type f -a -name \*.bt -print0 | xargs -0L1 "${BPFTRACE_RUNTIME_TEST_EXECUTABLE}" --test;
-exit $?
+filter_args=()
+[[ -n ${FILTER} ]] && filter_args=( --probe-filter ${FILTER} )
+
+for script in $(find "$TESTS_DIR"/self -type f -a -name \*.bt)
+do
+    tmplog=$(mktemp)
+
+    "${BPFTRACE_RUNTIME_TEST_EXECUTABLE}" --test "${filter_args[@]}" ${script} 2>&1 | tee "${tmplog}"
+
+    status=${PIPESTATUS[0]}
+    if [[ ${status} -ne 0 ]]; then
+        if [[ $(grep "No probes to attach" "${tmplog}") ]]; then
+            rm -f "${tmplog}"
+            continue
+        else
+            rm -f "${tmplog}"
+            exit "${status}"
+        fi
+    fi
+
+    rm -f "${tmplog}"
+done
