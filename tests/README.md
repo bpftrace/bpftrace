d38c58f5edfe6b61f592c9e30b46db4e54acf241
Rong Tao <rongtao@cestc.cn>
2026-08-24 11:24:06 +0800

    self-tests.sh: support --filter argument
    Usage:
    
        $ sudo ./self-tests.sh --filter=string
        ....
        [==========] Running 1 tests.
        [ RUN      ] cast_string_to_bool         << matched
        [       OK ] cast_string_to_bool (1 ns)
        [==========] 1 test ran.
        [  PASSED  ] 1 test.
        ....
        [==========] Running 1 tests.
        [ RUN      ] tuple_map_key_string_resize
        [       OK ] tuple_map_key_string_resize (1000 μs)
        [==========] 1 test ran.
        [  PASSED  ] 1 test.
        ....
        No probes to attach                      << not matched
    
        [==========] Running 2 tests.
        [ RUN      ] variable_strings_are_memset
        [       OK ] variable_strings_are_memset (1000 μs)
        [ RUN      ] map_strings_are_memset
        [       OK ] map_strings_are_memset (1000 μs)
        [==========] 2 tests ran.
        [  PASSED  ] 2 tests.
        ....
        No probes to attach
        No probes to attach
        No probes to attach
        No probes to attach
        ....
        No probes to attach
        [==========] Running 3 tests.
        [ RUN      ] basic_substring_match
        [       OK ] basic_substring_match (1 ns)
        [ RUN      ] substring_at_end
        [       OK ] substring_at_end (1000 μs)
        [ RUN      ] substring_at_beginning
        [       OK ] substring_at_beginning (1000 μs)
        [==========] 3 tests ran.
        [  PASSED  ] 3 tests.
    
    Co-authored-by: Jordan Rome <jordalgo@meta.com>
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

--- tests/README.md
+++ tests/README.md
@@ -15,6 +15,7 @@ These are `test:` probes written to test core functionality, and standard librar
 * Run: `sudo <builddir>/tests/self-tests.sh`
 * To debug a test, you can run an individual file directly, e.g. `<builddir>src/bpftrace --test <builddir>/tests/<testfile>`
 * To run only specific self tests, use `--probe-filter REGEX` to select tests by name, e.g. `<builddir>src/bpftrace --test --probe-filter 'map_' <builddir>/tests/<testfile>`
+* Use the `--filter` arg when running `self-tests.sh` to only run a subset of the tests. Implement the filter using the `bpftrace --probe-filter` parameter.
 
 ## Runtime tests
 
--- tests/self-tests.sh
+++ tests/self-tests.sh
@@ -11,11 +11,87 @@ TESTS_DIR="$(dirname "${BASH_SOURCE[0]}")";
 DIR="$( cd "$TESTS_DIR" >/dev/null && pwd )"
 BPFTRACE_RUNTIME_TEST_EXECUTABLE=${BPFTRACE_EXECUTABLE:-$DIR/../src/bpftrace};
 export BPFTRACE_RUNTIME_TEST_EXECUTABLE;
+FILTER=
+TMPLOG=
+
+__usage__()
+{
+    echo -e "
+self-test.sh [-h|--help] [--filter=<PATTERN>]
+
+    -h, --help           show this information.
+    --filter=[PATTERN]   only run the tests matching this pattern.
+"
+    exit ${1-0}
+}
+
+if ! TEMP_ARGS=$(getopt --options h \
+                        --long help \
+                        --long filter: \
+                        --name "${0}" -- "$@")
+then
+    __usage__ 1
+fi
+
+eval set -- "$TEMP_ARGS"
+
+while true; do
+    case $1 in
+    --filter)
+        shift
+        FILTER=$1
+        shift
+        ;;
+    -h | --help)
+        shift
+        __usage__
+        ;;
+    --)
+        shift
+        break
+        ;;
+    esac
+done
+
+cleanup()
+{
+    # Prevent leftover temporary files caused by abnormal exits.
+    rm -f ${TMPLOG}
+}
+trap cleanup EXIT
 
 echo "===================="
 echo "bpftrace --info:"
 echo "===================="
 "${BPFTRACE_RUNTIME_TEST_EXECUTABLE}" --info;
 
-find "$TESTS_DIR"/self -type f -a -name \*.bt -print0 | xargs -0L1 "${BPFTRACE_RUNTIME_TEST_EXECUTABLE}" --test;
-exit $?
+filter_matched_count=0
+filter_args=()
+[[ -n ${FILTER} ]] && filter_args=( --probe-filter "${FILTER}" )
+
+while IFS= read -r -d '' script; do
+    TMPLOG=$(mktemp)
+
+    "${BPFTRACE_RUNTIME_TEST_EXECUTABLE}" --test "${filter_args[@]}" "${script}" | tee "${TMPLOG}"
+
+    status=${PIPESTATUS[0]}
+    if [[ ${status} -ne 0 ]]; then
+        if [[ -n ${FILTER} ]]; then
+            if grep -q "^No probes to attach$" "${TMPLOG}"; then
+                rm -f "${TMPLOG}"
+                continue
+            fi
+        fi
+
+        rm -f "${TMPLOG}"
+        exit "${status}"
+    fi
+
+    filter_matched_count=$(( filter_matched_count + 1 ))
+    rm -f "${TMPLOG}"
+done < <(find "$TESTS_DIR"/self -type f -a -name \*.bt -print0)
+
+if [[ ${filter_matched_count} -eq 0 ]] && [[ -n ${FILTER} ]]; then
+   echo >&2 "ERROR: Not match any probe with filter '${FILTER}'"
+   exit 2 # ENOENT
+fi
