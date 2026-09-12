ecca8a082b91e05f99e0b71ee9beec47f2527096
Jordan Rome <jordan@jordanrome.com>
2026-09-12 12:57:23 -0400

    Remove check for bpf_map_lookup_percpu_elem helper (#5342)
    This was added in kernel 5.19 which is below our min kernel version. We can
    remove the check for it.
    
    
    
    stack-info: PR: https://github.com/bpftrace/bpftrace/pull/5342, branch: user/jordalgo/rm_has_helper_map_lookup_percpu_elem/1
    
    Signed-off-by: Jordan Rome <jordalgo@meta.com>

--- src/bpffeature.h
+++ src/bpffeature.h
@@ -66,7 +66,6 @@ public:
 
   DEFINE_HELPER_TEST(ktime_get_tai_ns, BPF_PROG_TYPE_KPROBE);
   DEFINE_HELPER_TEST(get_func_ip, BPF_PROG_TYPE_KPROBE);
-  DEFINE_HELPER_TEST(map_lookup_percpu_elem, BPF_PROG_TYPE_KPROBE);
   DEFINE_HELPER_TEST(loop, BPF_PROG_TYPE_KPROBE); // Added in 5.17.
 
   bool has_kfunc(std::string kfunc);
