34b505b12a5d16bc86937ef3181ed91afa3134fa
Ivan A. Melnikov <iv@altlinux.org>
2026-09-18 14:16:09 +0400

    tests/runtime: Fix get_tai_ns feature detection
    It should look for `get_tai_ns` in the bpftrace output,
    `get_ktime_ns` never was there.
    
    Fixes: 5ffa91ea739d679e684e1911fd3787ef3fb488b7
    Signed-off-by: Ivan A. Melnikov <iv@altlinux.org>

--- tests/runtime/engine/runner.py
+++ tests/runtime/engine/runner.py
@@ -160,7 +160,7 @@ class Runner(object):
         bpffeature["uprobe_multi"] = output.find("uprobe_multi: yes") != -1
         bpffeature["aot"] = cmake_vars.LIBBCC_BPF_CONTAINS_RUNTIME
         bpffeature["skboutput"] = output.find("skboutput: yes") != -1
-        bpffeature["get_tai_ns"] = output.find("get_ktime_ns: yes") != -1
+        bpffeature["get_tai_ns"] = output.find("get_tai_ns: yes") != -1
         bpffeature["get_func_ip"] = output.find("get_func_ip: yes") != -1
         bpffeature["jiffies64"] = output.find("jiffies64: yes") != -1
         bpffeature["dwunwind"] = output.find("dwunwind (DWARF stack unwinding): yes") != -1
